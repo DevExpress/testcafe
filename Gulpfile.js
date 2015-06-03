d8311fa9fb653617328e2ba58a87bf95c8f6bc0b
dikareva <yelena_dikareva@rambler.ru>
2015-06-03 16:23:50 +0300

    Revert "hammerhead nodeunit tests"
    This reverts commit 94a6f554d51caa2a1f997a300975f254f745ca65.

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    nodeunit = require('gulp-nodeunit');
-
-gulp.task('Nodeunit', function () {
-    return gulp
-        .src('test/nodeunit/**/*_test.js')
-        .pipe(nodeunit());
-});
