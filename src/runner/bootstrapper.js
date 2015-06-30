ca32befa46e438b7ffb26114d16a4415b3085df5
Ivan Nikulin <ifaaan@gmail.com>
2015-06-30 16:23:19 +0300

    Start implementing runner

---
+++ src/runner/bootstrapper.js
@@ -0,0 +1,13 @@
+export default class RunnerBootstrapper {
+    constructor () {
+        this.src             = [];
+        this.browsers        = [];
+        this.filter          = null;
+        this.reporter        = null;
+        this.reportOutStream = null;
+    }
+
+    async createRunnableConfiguration () {
+        // TODO
+    }
+}
