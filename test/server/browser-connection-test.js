d77c590a9efcf4fc6c3c786dfe9c152f30d72b20
Ivan Nikulin <ifaaan@gmail.com>
2015-07-13 15:54:53 +0300

    Restructure project. Move assets to the appropriate dirs. Prepare unit testing environment.

--- test/server/browser-connection-test.js
+++ test/server/browser-connection-test.js
@@ -1,4 +1,21 @@
+//var TestCafe = require('../../lib/');
+
 // Test ready event
 // Test heartbeat timeout
 // Test idle page rendering
 // Test status request
+
+/*
+describe('Browser connection', function () {
+    var testCafe = null;
+
+    before(function () {
+        testCafe = new TestCafe(1335, 1336);
+    });
+
+    after(function () {
+        testCafe.close();
+    });
+
+});
+*/
