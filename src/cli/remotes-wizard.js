16b88454181712133fdaa3e138342a8ac7bf6c67
Margarita <margarita.loseva@devexpress.com>
2016-01-28 13:13:30 +0300

    Wizard text changed

--- src/cli/remotes-wizard.js
+++ src/cli/remotes-wizard.js
@@ -2,6 +2,7 @@ import qrcode from 'qrcode-terminal';
 import chalk from 'chalk';
 import log from './log';
 import promisifyEvent from 'promisify-event';
+import dedent from 'dedent';
 
 export default async function (testCafe, remoteCount, showQRCode) {
     var connections = [];
@@ -9,15 +10,23 @@ export default async function (testCafe, remoteCount, showQRCode) {
     if (remoteCount) {
         log.hideSpinner();
 
+        var description = dedent(`
+            Connecting ${remoteCount} remote browser(s)...
+            Navigate to the appropriate URL from each of the remote browsers.
+        `);
+
+        log.write(description);
+
+        if (showQRCode)
+            log.write('You can either enter the URL or scan the QR-code.');
+
         for (var i = 0; i < remoteCount; i++) {
             var browserConnection = testCafe.createBrowserConnection();
 
-            log.write(`To connect a remote browser #${i + 1}, use it to open ${chalk.underline.blue(browserConnection.url)}`);
+            log.write(`Browser #${i + 1}: ${chalk.underline.blue(browserConnection.url)}`);
 
-            if (showQRCode) {
-                log.write('or scan this QR-code:\n');
+            if (showQRCode)
                 qrcode.generate(browserConnection.url);
-            }
 
             await promisifyEvent(browserConnection, 'ready');
 
