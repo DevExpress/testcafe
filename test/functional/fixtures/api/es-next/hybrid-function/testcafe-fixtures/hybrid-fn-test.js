b541cff64f655d6b9c5e01ee537e5554ce1b94d0
inikulin <ifaaan@gmail.com>
2016-04-20 14:55:58 +0300

    Implemented Hybrid function initialization errors (#241)

--- test/functional/fixtures/api/es-next/hybrid-function/testcafe-fixtures/hybrid-fn-test.js
+++ test/functional/fixtures/api/es-next/hybrid-function/testcafe-fixtures/hybrid-fn-test.js
@@ -24,3 +24,35 @@ test('Hammerhead code instrumentation', async () => {
 
     expect(location).eql('http://localhost:3000/api/es-next/hybrid-function/pages/index.html');
 });
+
+test('Hybrid fn is not a function', async () => {
+    await Hybrid(123)();
+});
+
+test('Hybrid fn test run is unresolvable', async () => {
+    var fs = require('fs');
+
+    var hybrid = Hybrid(() => 123);
+
+    return new Promise((resolve, reject) => {
+        fs.readFile('not/exists', () => {
+            try {
+                hybrid();
+                resolve();
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    });
+});
+
+test('Async syntax in Hybrid', async () => {
+    Hybrid(async () => Promise.resolve());
+});
+
+test('Generator in Hybrid', async () => {
+    Hybrid(function*() {
+        yield 1;
+    });
+});
