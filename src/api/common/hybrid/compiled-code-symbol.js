ee256734cd5e19e2c92c32c630d570c6b013b644
inikulin <ifaaan@gmail.com>
2016-05-06 13:59:40 +0300

    Fix typo in comment

--- src/api/common/hybrid/compiled-code-symbol.js
+++ src/api/common/hybrid/compiled-code-symbol.js
@@ -1,3 +1,3 @@
 // NOTE: This symbol used for storing hybrid function's compiled code for it's
-// later use as dependency. Also, it's used a hybrid function marker.
+// later use as dependency. Also, it's used as a hybrid function marker.
 export default Symbol('compiledCode');
