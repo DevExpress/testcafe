eba388a5132e47a80de0132096249d42011be1f8
miherlosev@mail.ru <Ein7Onai>
2016-05-27 17:59:59 +0300

    44444444444444444444444444444444444444444444

--- test/functional/fixtures/api/es-next/take-screenshot/testcafe-fixtures/take-screenshot.js
+++ test/functional/fixtures/api/es-next/take-screenshot/testcafe-fixtures/take-screenshot.js
@@ -17,24 +17,6 @@ test('Take a screenshot', async t => {
 test('Take a screenshot with a custom path', async t => {
     const ua       = await getUserAgent();
     const parsedUA = parse(ua);
-<<<<<<< HEAD
-<<<<<<< f50d7d4b70565973bbb14668756ae946101e0b80
 
-<<<<<<< 1c90f95a121f2fda659a8449048ad28b21020f96
-<<<<<<< e8cb9c35813fdf7814793df0504da89b0e0966ab
-    await t.takeScreenshot('../../../../../../../___test-screenshots___/custom/' + parsedUA.family);
-=======
     await t.takeScreenshot('../../../../screenshots/custom/' + parsedUA.family);
->>>>>>> TakeScreenshot, TakeScreenshotOnFail commands (part of #441, part of #240) (#552)
-=======
-    await t.takeScreenshot('../../../../../../../___test-screenshots___/custom/' + parsedUA.family);
->>>>>>> fix problems after merge TakeScreenshotCommand PR (#558)
-=======
-    
-    await t.takeScreenshot('../../../../screenshots/custom/' + parsedUA.family);
->>>>>>> TakeScreenshot, TakeScreenshotOnFail commands (part of #441, part of #240) (#552)
-=======
-    
-    await t.takeScreenshot('../../../../screenshots/custom/' + parsedUA.family);
->>>>>>> 9089b3f8cf2f815abd863913636403286f43aac2
 });
