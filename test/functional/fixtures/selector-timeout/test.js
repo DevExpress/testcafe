243c029acf69238eb02f9f0992179bce64ef2909
Ivan Nikulin <ifaaan@gmail.com>
2016-06-23 20:54:08 +0300

    Fix linting error

--- test/functional/fixtures/selector-timeout/test.js
+++ test/functional/fixtures/selector-timeout/test.js
@@ -1,12 +1,12 @@
 var expect = require('chai').expect;
 
 describe('Selector timeout', function () {
-    it("Should pass if selector timeout exceeds time required for the element to appear", function () {
+    it('Should pass if selector timeout exceeds time required for the element to appear', function () {
         return runTests('testcafe-fixtures/selector-timeout.js', 'Wait for element with timeout enough for it to appear',
             { selectorTimeout: 2500 });
     });
 
-    it("Should fail if selector timeout is less than time required for the element to appear", function () {
+    it('Should fail if selector timeout is less than time required for the element to appear', function () {
         return runTests('testcafe-fixtures/selector-timeout.js', 'Wait for element with insufficient timeout',
             { shouldFail: true, selectorTimeout: 500 })
             .catch(function (errs) {
@@ -14,7 +14,7 @@ describe('Selector timeout', function () {
             });
     });
 
-    it("Should fail if selector timeout is less than the time required for the element to appear (legacy)", function () {
+    it('Should fail if selector timeout is less than the time required for the element to appear (legacy)', function () {
         return runTests('testcafe-fixtures/selector-timeout.test.js', 'Wait for element with insufficient timeout',
             { shouldFail: true, selectorTimeout: 500 })
             .catch(function (errs) {
