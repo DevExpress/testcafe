helen-dikareva/testcafe-phoenix

First part of native dialogs handling implemented (part of #451)

src/test-run/commands/actions.js
@@ -1,7 +1,9 @@
 import TYPE from './type';
 import SelectorFactory from '../../client-functions/selector-factory';
 import Assignable from '../../utils/assignable';
-import { ClickOptions, MouseOptions, TypeOptions } from './options';
+import ExtendedDialogCommand from './extended-dialog-command';
+
+import { ClickOptions, MouseOptions, TypeOptions, HandleDialogOptions } from './options';
 
 import {
     actionOptions,
@@ -9,7 +11,9 @@ import {
     positiveIntegerArgument,
     nonEmptyStringArgument,
     urlArgument,
-    stringOrStringArrayArgument
+    booleanArgument,
+    stringOrStringArrayArgument,
+    stringOrNull
 } from './validations/argument';
 
 import { ActionSelectorError } from '../../errors/test-run';
@@ -42,8 +46,12 @@ function initTypeOptions (name, val) {
     return new TypeOptions(val, true);
 }
 
+function initHandleDialogOption (name, val) {
+    return new HandleDialogOptions(val, true);
+}
+
 // Commands
-export class ClickCommand extends Assignable {
+export class ClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -62,7 +70,7 @@ export class ClickCommand extends Assignable {
     }
 }
 
-export class RightClickCommand extends Assignable {
+export class RightClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -81,7 +89,7 @@ export class RightClickCommand extends Assignable {
     }
 }
 
-export class DoubleClickCommand extends Assignable {
+export class DoubleClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -100,7 +108,7 @@ export class DoubleClickCommand extends Assignable {
     }
 }
 
-export class HoverCommand extends Assignable {
+export class HoverCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -119,7 +127,7 @@ export class HoverCommand extends Assignable {
     }
 }
 
-export class TypeTextCommand extends Assignable {
+export class TypeTextCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -140,7 +148,7 @@ export class TypeTextCommand extends Assignable {
     }
 }
 
-export class DragCommand extends Assignable {
+export class DragCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -163,7 +171,7 @@ export class DragCommand extends Assignable {
     }
 }
 
-export class DragToElementCommand extends Assignable {
+export class DragToElementCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -185,7 +193,7 @@ export class DragToElementCommand extends Assignable {
     }
 }
 
-export class SelectTextCommand extends Assignable {
+export class SelectTextCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -206,7 +214,7 @@ export class SelectTextCommand extends Assignable {
     }
 }
 
-export class SelectEditableContentCommand extends Assignable {
+export class SelectEditableContentCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -225,7 +233,7 @@ export class SelectEditableContentCommand extends Assignable {
     }
 }
 
-export class SelectTextAreaContentCommand extends Assignable {
+export class SelectTextAreaContentCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -250,7 +258,7 @@ export class SelectTextAreaContentCommand extends Assignable {
     }
 }
 
-export class PressKeyCommand extends Assignable {
+export class PressKeyCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -267,7 +275,7 @@ export class PressKeyCommand extends Assignable {
     }
 }
 
-export class NavigateToCommand extends Assignable {
+export class NavigateToCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -284,7 +292,7 @@ export class NavigateToCommand extends Assignable {
     }
 }
 
-export class SetFilesToUploadCommand extends Assignable {
+export class SetFilesToUploadCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -304,7 +312,7 @@ export class SetFilesToUploadCommand extends Assignable {
     }
 }
 
-export class ClearUploadCommand extends Assignable {
+export class ClearUploadCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -322,7 +330,7 @@ export class ClearUploadCommand extends Assignable {
     }
 }
 
-export class SwitchToIframeCommand extends Assignable {
+export class SwitchToIframeCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -338,8 +346,76 @@ export class SwitchToIframeCommand extends Assignable {
     }
 }
 
-export class SwitchToMainWindowCommand {
+export class SwitchToMainWindowCommand extends ExtendedDialogCommand {
     constructor () {
+        super();
+
         this.type = TYPE.switchToMainWindow;
+    }
+}
+
+class HandleDialogCommand extends Assignable {
+    constructor (obj) {
+        super(obj);
+
+        this.options = null;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return [
+            { name: 'options', type: actionOptions, init: initHandleDialogOption, required: true }
+        ];
+    }
+}
+
+export class HandleAlertCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type = TYPE.handleAlert;
+    }
+}
+
+export class HandleConfirmCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type   = TYPE.handleConfirm;
+        this.result = false;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return super._getAssignableProperties().concat([
+            { name: 'result', type: booleanArgument }
+        ]);
+    }
+}
+
+export class HandlePromptCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type   = TYPE.handlePrompt;
+        this.result = null;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return super._getAssignableProperties().concat([
+            { name: 'result', type: stringOrNull }
+        ]);
+    }
+}
+
+export class HandleBeforeUnloadCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type = TYPE.handleBeforeUnload;
     }
 }

test/functional/fixtures/native-dialogs-handling/es-next/testcafe-fixtures/resize-test.js
@@ -1,0 +1,63 @@
+import { parse } from 'useragent';
+import { ClientFunction } from 'testcafe';
+
+const getWindowWidth             = ClientFunction(() => window.innerWidth);
+const getWindowHeight            = ClientFunction(() => window.innerHeight);
+const getUserAgent               = ClientFunction(() => navigator.userAgent.toString());
+const setWindowOnresizeHandler   = ClientFunction(() => {
+    window.onresize = function () {
+        window.onresize = function () {
+        };
+
+        /* eslint-disable no-alert*/
+        alert('Alert!');
+        /* eslint-enable no-alert*/
+    };
+});
+const resetWindowOnresizeHandler = ClientFunction(() => window.onresize = function () {
+});
+
+var initialWindowSize = {};
+
+fixture `Native dialogs during resizeWindow action`
+    .page `http://localhost:3000/fixtures/native-dialogs-handling/es-next/pages/index.html`
+    .beforeEach(async () => {
+        var ua       = await getUserAgent();
+        var parsedUA = parse(ua);
+
+        initialWindowSize[parsedUA.family] = {
+            width:  await getWindowWidth(),
+            height: await getWindowHeight()
+        };
+    })
+    .afterEach(async t => {
+        var ua       = await getUserAgent();
+        var parsedUA = parse(ua);
+        var size     = initialWindowSize[parsedUA.family];
+
+        await resetWindowOnresizeHandler();
+
+        await t
+            .resizeWindow(size.width, size.height);
+    });
+
+//resizeWindow
+test('Dialog appears during resizeWindow action', async t => {
+    await setWindowOnresizeHandler();
+
+    await t.resizeWindow(500, 500)
+        .handleAlert();
+});
+
+test('No expected alert during resizeWindow action', async t => {
+    await t
+        .resizeWindow(500, 500)
+        .handleAlert({ timeout: 200 });
+});
+
+test('Unexpected alert during resizeWindow action', async t => {
+    await setWindowOnresizeHandler();
+
+    await t
+        .resizeWindow(500, 500);
+});

test/functional/fixtures/native-dialogs-handling/raw/test.js
@@ -1,0 +1,59 @@
+var errorInEachBrowserContains            = require('../../../assertion-helper.js').errorInEachBrowserContains;
+var getExpectedDialogNotAppearedErrorText = require('../error-text.js').getExpectedDialogNotAppearedErrorText;
+var getUnexpectedDialogErrorText          = require('../error-text.js').getUnexpectedDialogErrorText;
+var DIALOG_TYPE                           = require('../../../../../lib/test-run/browser-dialogs.js').TYPE;
+
+
+describe('[RAW] Native dialogs handling', function () {
+    it('Should pass if the expected confirm dialog appears after an action', function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'Expected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, 'Confirm dialog with result: true', 0);
+            });
+    });
+
+    it("Should fail if the expected confirm dialog doesn't appear after an action", function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'No expected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getExpectedDialogNotAppearedErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[No expected confirm after an action callsite]]', 0);
+            });
+    });
+
+    it('Should fail when an unexpected confirm dialog appears after an action', function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'Unexpected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getUnexpectedDialogErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[Unexpected confirm after an action callsite]]', 0);
+            });
+    });
+
+    it('Should pass if the expected confirm dialog appears after page load', function () {
+        return runTests('./testcafe-fixtures/page-load.testcafe', 'Expected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, 'Confirm dialog with result: true', 0);
+            });
+    });
+
+    it("Should fail if the expected confirm dialog doesn't appear after page load", function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'No expected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getExpectedDialogNotAppearedErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[No expected confirm after page load callsite]]', 0);
+            });
+    });
+
+    it('Should fail when an unexpected confirm dialog appears after page load', function () {
+        return runTests('./testcafe-fixtures/page-load.testcafe', 'Unexpected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getUnexpectedDialogErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[Unexpected confirm after page load callsite]]', 0);
+            });
+    });
+});

DevExpress/testcafe

src/test-run/commands/actions.js
@@ -1,7 +1,9 @@
 import TYPE from './type';
 import SelectorFactory from '../../client-functions/selector-factory';
 import Assignable from '../../utils/assignable';
-import { ClickOptions, MouseOptions, TypeOptions } from './options';
+import ExtendedDialogCommand from './extended-dialog-command';
+
+import { ClickOptions, MouseOptions, TypeOptions, HandleDialogOptions } from './options';
 
 import {
     actionOptions,
@@ -9,7 +11,9 @@ import {
     positiveIntegerArgument,
     nonEmptyStringArgument,
     urlArgument,
-    stringOrStringArrayArgument
+    booleanArgument,
+    stringOrStringArrayArgument,
+    stringOrNull
 } from './validations/argument';
 
 import { ActionSelectorError } from '../../errors/test-run';
@@ -42,8 +46,12 @@ function initTypeOptions (name, val) {
     return new TypeOptions(val, true);
 }
 
+function initHandleDialogOption (name, val) {
+    return new HandleDialogOptions(val, true);
+}
+
 // Commands
-export class ClickCommand extends Assignable {
+export class ClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -62,7 +70,7 @@ export class ClickCommand extends Assignable {
     }
 }
 
-export class RightClickCommand extends Assignable {
+export class RightClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -81,7 +89,7 @@ export class RightClickCommand extends Assignable {
     }
 }
 
-export class DoubleClickCommand extends Assignable {
+export class DoubleClickCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -100,7 +108,7 @@ export class DoubleClickCommand extends Assignable {
     }
 }
 
-export class HoverCommand extends Assignable {
+export class HoverCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -119,7 +127,7 @@ export class HoverCommand extends Assignable {
     }
 }
 
-export class TypeTextCommand extends Assignable {
+export class TypeTextCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -140,7 +148,7 @@ export class TypeTextCommand extends Assignable {
     }
 }
 
-export class DragCommand extends Assignable {
+export class DragCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -163,7 +171,7 @@ export class DragCommand extends Assignable {
     }
 }
 
-export class DragToElementCommand extends Assignable {
+export class DragToElementCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -185,7 +193,7 @@ export class DragToElementCommand extends Assignable {
     }
 }
 
-export class SelectTextCommand extends Assignable {
+export class SelectTextCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -206,7 +214,7 @@ export class SelectTextCommand extends Assignable {
     }
 }
 
-export class SelectEditableContentCommand extends Assignable {
+export class SelectEditableContentCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -225,7 +233,7 @@ export class SelectEditableContentCommand extends Assignable {
     }
 }
 
-export class SelectTextAreaContentCommand extends Assignable {
+export class SelectTextAreaContentCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -250,7 +258,7 @@ export class SelectTextAreaContentCommand extends Assignable {
     }
 }
 
-export class PressKeyCommand extends Assignable {
+export class PressKeyCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -267,7 +275,7 @@ export class PressKeyCommand extends Assignable {
     }
 }
 
-export class NavigateToCommand extends Assignable {
+export class NavigateToCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -284,7 +292,7 @@ export class NavigateToCommand extends Assignable {
     }
 }
 
-export class SetFilesToUploadCommand extends Assignable {
+export class SetFilesToUploadCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -304,7 +312,7 @@ export class SetFilesToUploadCommand extends Assignable {
     }
 }
 
-export class ClearUploadCommand extends Assignable {
+export class ClearUploadCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -322,7 +330,7 @@ export class ClearUploadCommand extends Assignable {
     }
 }
 
-export class SwitchToIframeCommand extends Assignable {
+export class SwitchToIframeCommand extends ExtendedDialogCommand {
     constructor (obj) {
         super(obj);
 
@@ -338,8 +346,76 @@ export class SwitchToIframeCommand extends Assignable {
     }
 }
 
-export class SwitchToMainWindowCommand {
+export class SwitchToMainWindowCommand extends ExtendedDialogCommand {
     constructor () {
+        super();
+
         this.type = TYPE.switchToMainWindow;
+    }
+}
+
+class HandleDialogCommand extends Assignable {
+    constructor (obj) {
+        super(obj);
+
+        this.options = null;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return [
+            { name: 'options', type: actionOptions, init: initHandleDialogOption, required: true }
+        ];
+    }
+}
+
+export class HandleAlertDialogCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type = TYPE.handleAlertDialog;
+    }
+}
+
+export class HandleConfirmDialogCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type        = TYPE.handleConfirmDialog;
+        this.returnValue = false;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return super._getAssignableProperties().concat([
+            { name: 'returnValue', type: booleanArgument }
+        ]);
+    }
+}
+
+export class HandlePromptDialogCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type        = TYPE.handlePromptDialog;
+        this.returnValue = null;
+
+        this._assignFrom(obj, true);
+    }
+
+    _getAssignableProperties () {
+        return super._getAssignableProperties().concat([
+            { name: 'returnValue', type: stringOrNull }
+        ]);
+    }
+}
+
+export class HandleBeforeUnloadDialogCommand extends HandleDialogCommand {
+    constructor (obj) {
+        super(obj);
+
+        this.type = TYPE.handleBeforeUnloadDialog;
     }
 }

test/functional/fixtures/native-dialogs-handling/es-next/testcafe-fixtures/resize-test.js
@@ -1,0 +1,63 @@
+import { parse } from 'useragent';
+import { ClientFunction } from 'testcafe';
+
+const getWindowWidth             = ClientFunction(() => window.innerWidth);
+const getWindowHeight            = ClientFunction(() => window.innerHeight);
+const getUserAgent               = ClientFunction(() => navigator.userAgent.toString());
+const setWindowOnresizeHandler   = ClientFunction(() => {
+    window.onresize = function () {
+        window.onresize = function () {
+        };
+
+        /* eslint-disable no-alert*/
+        alert('Alert!');
+        /* eslint-enable no-alert*/
+    };
+});
+const resetWindowOnresizeHandler = ClientFunction(() => window.onresize = function () {
+});
+
+var initialWindowSize = {};
+
+fixture `Native dialogs during resizeWindow action`
+    .page `http://localhost:3000/fixtures/native-dialogs-handling/es-next/pages/index.html`
+    .beforeEach(async () => {
+        var ua       = await getUserAgent();
+        var parsedUA = parse(ua);
+
+        initialWindowSize[parsedUA.family] = {
+            width:  await getWindowWidth(),
+            height: await getWindowHeight()
+        };
+    })
+    .afterEach(async t => {
+        var ua       = await getUserAgent();
+        var parsedUA = parse(ua);
+        var size     = initialWindowSize[parsedUA.family];
+
+        await resetWindowOnresizeHandler();
+
+        await t
+            .resizeWindow(size.width, size.height);
+    });
+
+//resizeWindow
+test('Dialog appears during resizeWindow action', async t => {
+    await setWindowOnresizeHandler();
+
+    await t.resizeWindow(500, 500)
+        .handleAlertDialog();
+});
+
+test('No expected alert during resizeWindow action', async t => {
+    await t
+        .resizeWindow(500, 500)
+        .handleAlertDialog({ timeout: 200 });
+});
+
+test('Unexpected alert during resizeWindow action', async t => {
+    await setWindowOnresizeHandler();
+
+    await t
+        .resizeWindow(500, 500);
+});

test/functional/fixtures/native-dialogs-handling/raw/test.js
@@ -1,0 +1,59 @@
+var errorInEachBrowserContains            = require('../../../assertion-helper.js').errorInEachBrowserContains;
+var getExpectedDialogNotAppearedErrorText = require('../error-text.js').getExpectedDialogNotAppearedErrorText;
+var getUnexpectedDialogErrorText          = require('../error-text.js').getUnexpectedDialogErrorText;
+var DIALOG_TYPE                           = require('../../../../../lib/test-run/browser-dialogs.js').DIALOG_TYPE;
+
+
+describe('[RAW] Native dialogs handling', function () {
+    it('Should pass if the expected confirm dialog appears after an action', function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'Expected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, 'Confirm dialog with result: true', 0);
+            });
+    });
+
+    it("Should fail if the expected confirm dialog doesn't appear after an action", function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'No expected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getExpectedDialogNotAppearedErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[No expected confirm after an action callsite]]', 0);
+            });
+    });
+
+    it('Should fail when an unexpected confirm dialog appears after an action', function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'Unexpected confirm after an action',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getUnexpectedDialogErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[Unexpected confirm after an action callsite]]', 0);
+            });
+    });
+
+    it('Should pass if the expected confirm dialog appears after page load', function () {
+        return runTests('./testcafe-fixtures/page-load.testcafe', 'Expected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, 'Confirm dialog with result: true', 0);
+            });
+    });
+
+    it("Should fail if the expected confirm dialog doesn't appear after page load", function () {
+        return runTests('./testcafe-fixtures/native-dialogs.testcafe', 'No expected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getExpectedDialogNotAppearedErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[No expected confirm after page load callsite]]', 0);
+            });
+    });
+
+    it('Should fail when an unexpected confirm dialog appears after page load', function () {
+        return runTests('./testcafe-fixtures/page-load.testcafe', 'Unexpected confirm after page load',
+            { shouldFail: true })
+            .catch(function (errs) {
+                errorInEachBrowserContains(errs, getUnexpectedDialogErrorText(DIALOG_TYPE.confirm), 0);
+                errorInEachBrowserContains(errs, '[[Unexpected confirm after page load callsite]]', 0);
+            });
+    });
+});