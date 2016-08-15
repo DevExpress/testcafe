be202cd769fccfc39557ecfadded345e7d4675a5
Andrey Belym <belym.a.2105@gmail.com>
2016-08-15 13:25:32 +0300

    renderTemplate fix (closes #718)

--- src/utils/render-template.js
+++ src/utils/render-template.js
@@ -1,5 +1,8 @@
 export default function renderTemplate (template, ...args) {
+    if (!args.length)
+        return template;
+
     var counter = 0;
 
-    return template.replace(/{.+?}/g, () => args[counter++]);
+    return template.replace(/{.+?}/g, match => counter < args.length ? args[counter++] : match);
 }
