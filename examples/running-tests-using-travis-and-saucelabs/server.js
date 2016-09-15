72ebef384e76a8029fa19acbfe9ace8557130806
Margarita <margarita.loseva@devexpress.com>
2016-09-15 13:44:02 +0300

    [docs] files modified

--- examples/running-tests-using-travis-and-saucelabs/server.js
+++ examples/running-tests-using-travis-and-saucelabs/server.js
@@ -1,4 +1,4 @@
-const connect = require('connect');
+const connect     = require('connect');
 const serveStatic = require('serve-static');
 
 connect().use(serveStatic(__dirname)).listen(9090, () => {
