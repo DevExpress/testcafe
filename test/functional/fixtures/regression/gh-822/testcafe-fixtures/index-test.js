f390b3ce33355800ebce75754cdb04943d707a3d
inikulin <ifaaan@gmail.com>
2016-09-20 14:14:46 +0300

    Fix lint error

--- test/functional/fixtures/regression/gh-822/testcafe-fixtures/index-test.js
+++ test/functional/fixtures/regression/gh-822/testcafe-fixtures/index-test.js
@@ -4,7 +4,7 @@ import { Selector } from 'testcafe';
 fixture `gh822`
     .page `http://localhost:3000/fixtures/regression/gh-822/pages/index.html`;
 
-test('Selectors with escapable symbols', async t => {
+test('Selectors with escapable symbols', async () => {
     const div = await Selector('#\\35 -2')();
 
     expect(div.innerText).eql('1');
