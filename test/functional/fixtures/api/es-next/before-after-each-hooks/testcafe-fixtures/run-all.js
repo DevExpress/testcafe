bf1f91ab2cab0a649fb177c6d75255e292c2a042
inikulin <ifaaan@gmail.com>
2016-09-29 16:15:08 +0300

    Make linter happy

--- test/functional/fixtures/api/es-next/before-after-each-hooks/testcafe-fixtures/run-all.js
+++ test/functional/fixtures/api/es-next/before-after-each-hooks/testcafe-fixtures/run-all.js
@@ -1,11 +1,18 @@
 fixture `Run all`
     .page `http://localhost:3000/fixtures/api/es-next/before-after-each-hooks/pages/index.html`
-    .beforeEach(async t => { await t.click('#beforeEach') })
+    .beforeEach(async t => {
+        await t.click('#beforeEach');
+    })
     .afterEach(async t => {
         await t
             .click('#afterEach')
             .click('#failAndReport');
     });
 
-test('Test1', async t => { await t.click('#test') });
-test('Test2', async t => { await t.click('#test') });
+test('Test1', async t => {
+    await t.click('#test');
+});
+
+test('Test2', async t => {
+    await t.click('#test');
+});
