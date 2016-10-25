23b45d962602df38babbe4dafe4631fe6c62efca
Alex Mos <alexander.moskovkin@devexpress.com>
2016-10-25 20:01:45 +0300

    Fix server tests after babel submodules update

--- test/server/data/client-fn-compilation/basic/expected-node10.js
+++ test/server/data/client-fn-compilation/basic/expected-node10.js
@@ -1,7 +1,7 @@
 (function () {
     return (function () {
-        var _window$location = __get$(window, "location");
-        var hostname         = __get$(_window$location, "hostname"),
+        var _window$location = __get$(window, "location"),
+            hostname         = __get$(_window$location, "hostname"),
             port             = __get$(_window$location, "port");
         return hostname + ':' + port;
     });
