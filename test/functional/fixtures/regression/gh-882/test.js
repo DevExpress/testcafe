37ee966c360dfb3d0769479a99d0a2218ecd231d
Andrey Belym <belym.a.2105@gmail.com>
2016-10-28 17:33:45 +0300

    Bring back functional tests (#929)

--- test/functional/fixtures/regression/gh-882/test.js
+++ test/functional/fixtures/regression/gh-882/test.js
@@ -1,4 +1,4 @@
-describe.only('[Regression](GH-882) Test should not hang when content is scaled or in Hi-DPI mode', function () {
+describe('[Regression](GH-882) Test should not hang when content is scaled or in Hi-DPI mode', function () {
     // NOTE: Test must run on a zoomed page or in Hi-DPI mode
     it('gh-882', function () {
         return runTests('testcafe-fixtures/index-test.js', 'gh-882');
