6cf0dbb900b58602093142b1f2458a0fb1764b2f
helen-dikareva <yelena.dikareva@yandex.ru>
2016-11-08 17:45:02 +0300

    test fixed

--- test/functional/fixtures/regression/gh-889/test.js
+++ test/functional/fixtures/regression/gh-889/test.js
@@ -1,5 +1,9 @@
 describe('[Regression](GH-889)', function () {
     it('Should not call blur for table', function () {
-        return runTests('testcafe-fixtures/index.test.js', 'Click on children of table');
+        return runTests('testcafe-fixtures/index.test.js', 'Click on children of table', { skip: 'ie' });
+    });
+
+    it('Should not call blur for table (for IE)', function () {
+        return runTests('testcafe-fixtures/index.test.js', 'Click on children of table (for IE)', { only: 'ie' });
     });
 });
