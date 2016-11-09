bd006b0a5542bf69a6c40896f5eff564c5145cb2
helen-dikareva <helen-dikareva@users.noreply.github.com>
2016-11-09 19:54:54 +0300

    Functional test 'Click on children of table' fixed (#961)
    * test fixed
    
    * hammerhead updated
    
    * typo fixed

--- test/functional/fixtures/regression/gh-889/testcafe-fixtures/index.test.js
+++ test/functional/fixtures/regression/gh-889/testcafe-fixtures/index.test.js
@@ -5,14 +5,28 @@ import { expect } from 'chai';
 fixture `GH-889`
     .page `http://localhost:3000/fixtures/regression/gh-889/pages/index.html`;
 
-test('Click on children of table', async t => {
-    var getTableFocusEventCount = ClientFunction(() => window.tableFocusEventCount);
-    var getTableBlurEventCount  = ClientFunction(() => window.tableBlurEventCount);
+var getTableFocusEventCount = ClientFunction(() => window.tableFocusEventCount);
+var getTableBlurEventCount  = ClientFunction(() => window.tableBlurEventCount);
 
+test('Click on children of table', async t => {
     await t
-        .click('#cell')
+        .click('#td1')
         .click('#div');
 
     expect(await getTableFocusEventCount()).equals(1);
     expect(await getTableBlurEventCount()).equals(0);
+});
+
+test('Click on children of table (for IE)', async t => {
+    var getFirstTableDataFocusEventCount  = ClientFunction(() => window.firstTableDataFocusEventCount);
+    var getSecondTableDataFocusEventCount = ClientFunction(() => window.secondTableDataFocusEventCount);
+
+    await t
+        .click('#td1')
+        .click('#div');
+
+    expect(await getTableFocusEventCount()).equals(0);
+    expect(await getTableBlurEventCount()).equals(0);
+    expect(await getFirstTableDataFocusEventCount()).equals(1);
+    expect(await getSecondTableDataFocusEventCount()).equals(1);
 });
