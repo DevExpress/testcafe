191824f8cd886489e97ba3da978ad64af9caa89c
Georgiy Abbasov <georgiy.abbasov@devexpress.com>
2016-12-02 16:11:07 +0300

    Tests refactored

--- test/functional/fixtures/regression/gh-993/test.js
+++ test/functional/fixtures/regression/gh-993/test.js
@@ -1,5 +1,5 @@
 describe('[Regression](GH-993)', function () {
     it("Key events should have 'key' or 'keyIdentifier' properties when t.pressKey('enter') performed", function () {
-        return runTests('testcafe-fixtures/index.test.js', "Press the 'enter' key on the input");
+        return runTests('testcafe-fixtures/index.test.js', 'Press the "enter" key on the input');
     });
 });
--- test/functional/fixtures/regression/gh-993/testcafe-fixtures/index.test.js
+++ test/functional/fixtures/regression/gh-993/testcafe-fixtures/index.test.js
@@ -1,10 +1,9 @@
 import { ClientFunction } from 'testcafe';
-import { expect } from 'chai';
 
 fixture `t.pressKey on angular2 element`
     .page('http://localhost:3000/fixtures/regression/gh-993/pages/index.html');
 
-test("Press the 'enter' key on the input", async t => {
+test('Press the "enter" key on the input', async t => {
     await t
         .click('#target')
         .pressKey('enter');
@@ -15,6 +14,7 @@ test("Press the 'enter' key on the input", async t => {
 
     const { keydown, keyup } = await getKeySequences();
 
-    expect(keydown).equal('Enter');
-    expect(keyup).equal('Enter');
+    await t
+        .expect(keydown).eql('Enter')
+        .expect(keyup).eql('Enter');
 });
