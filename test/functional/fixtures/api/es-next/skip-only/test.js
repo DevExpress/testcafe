acc4799de9bb0b084c8d969d940c0d66e8b25fea
inikulin <ifaaan@gmail.com>
2017-01-03 01:36:50 +0300

    Add tests for test.skip

--- test/functional/fixtures/api/es-next/skip-only/test.js
+++ test/functional/fixtures/api/es-next/skip-only/test.js
@@ -11,6 +11,23 @@ describe('[API] .skip/.only', function () {
                 expect(errs[2]).contains('Fixture3Test1');
                 expect(errs[3]).contains('Fixture5Test1');
                 expect(errs[4]).contains('Fixture5Test3');
+                expect(testReport.passed).eql(1);
+                expect(testReport.total).eql(6);
+            });
+    });
+
+    it('Should filter out tests without "skip" directive', function () {
+        return runTests('./testcafe-fixtures/skip-test.js', null, { shouldFail: true, only: 'chrome' })
+            .catch(function (errs) {
+                expect(errs.length).eql(3);
+                expect(errs[0]).contains('Fixture4Test1');
+                expect(errs[1]).contains('Fixture5Test2');
+                expect(errs[2]).contains('Fixture5Test4');
+                expect(testReport.passed).eql(1);
+                expect(testReport.total).eql(4);
+                expect(testReport.skipped).eql(5);
+                expect(testReport.fixtures[4].tests[0].skipped).to.be.true;
+                expect(testReport.fixtures[4].tests[1].skipped).to.be.false;
             });
     });
 });
--- test/functional/fixtures/api/es-next/skip-only/testcafe-fixtures/skip-test.js
+++ test/functional/fixtures/api/es-next/skip-only/testcafe-fixtures/skip-test.js
@@ -1,16 +1,16 @@
-fixture.only `Fixture1`;
+fixture.skip `Fixture1`;
 
 test('Fixture1Test1', () => {
     throw new Error('Fixture1Test1');
 });
 
-fixture.only('Fixture2');
+fixture.skip('Fixture2');
 
 test('Fixture2Test1', () => {
     throw new Error('Fixture2Test1');
 });
 
-fixture `Fixture3`.only;
+fixture `Fixture3`.skip;
 
 test('Fixture3Test1', () => {
     throw new Error('Fixture3Test1');
@@ -24,7 +24,7 @@ test('Fixture4Test1', () => {
 
 fixture `Fixture5`;
 
-test.only('Fixture5Test1', () => {
+test.skip('Fixture5Test1', () => {
     throw new Error('Fixture5Test1');
 });
 
@@ -34,11 +34,11 @@ test('Fixture5Test2', () => {
 
 test('Fixture5Test3', () => {
     throw new Error('Fixture5Test3');
-}).only;
+}).skip;
 
 test('Fixture5Test4', () => {
     throw new Error('Fixture5Test4');
 });
 
 test('Fixture5Test5', () => {
-}).only;
+});
