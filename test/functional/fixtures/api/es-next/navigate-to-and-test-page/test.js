271fa2b69226dffb879cf12285542247425e5d8c
inikulin <ifaaan@gmail.com>
2017-01-03 14:34:00 +0300

    Add test for test.page

--- test/functional/fixtures/api/es-next/navigate-to-and-test-page/test.js
+++ test/functional/fixtures/api/es-next/navigate-to-and-test-page/test.js
@@ -10,7 +10,13 @@ describe('[API] t.navigateTo', function () {
         return runTests('./testcafe-fixtures/navigate-to-test.js', 'Incorrect protocol', { shouldFail: 'true' })
             .catch(function (errs) {
                 expect(errs[0]).contains('The "url" argument specifies a URL that uses an unsupported ftp:// protocol. Only HTTP and HTTPS are supported, as well as protocol-relative and relative URLs.');
-                expect(errs[0]).contains('> 10 |    await t.navigateTo(\'ftp://localhost:3000/fixtures/api/es-next/navigate-to/pages/index.html\');');
+                expect(errs[0]).contains('> 10 |    await t.navigateTo(\'ftp://localhost:3000/fixtures/api/es-next/navigate-to-and-test-page/pages/index.html\');');
             });
+    });
+});
+
+describe('[API] test.page', function () {
+    it('Should navigate to the specified url on test start', function () {
+        return runTests('./testcafe-fixtures/navigate-to-test.js', 'Page directive', { only: 'chrome'});
     });
 });
