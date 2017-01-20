a183432ba3742a540164a971daac6f8014cb0649
Andrey Belym <belym.a.2105@gmail.com>
2017-01-20 13:54:20 +0300

    Revert overflow:hidden for ShadowUI root (closes #1156)

--- test/functional/fixtures/regression/gh-1138/test.js
+++ test/functional/fixtures/regression/gh-1138/test.js
@@ -1,4 +1,4 @@
-describe('[Regression](GH-1138)', function () {
+describe.skip('[Regression](GH-1138)', function () {
     it('Test should click on target that has moved during the action', function () {
         return runTests('testcafe-fixtures/index.test.js', 'Click on element bound to the right bottom corner');
     });
