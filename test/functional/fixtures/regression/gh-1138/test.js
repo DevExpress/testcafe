a1cfd053fc8e45da8e9937c11253843948991205
Andrey Belym <belym.a.2105@gmail.com>
2017-01-20 17:28:01 +0300

    Fix cursor

--- test/functional/fixtures/regression/gh-1138/test.js
+++ test/functional/fixtures/regression/gh-1138/test.js
@@ -1,4 +1,4 @@
-describe.skip('[Regression](GH-1138)', function () {
+describe('[Regression](GH-1138)', function () {
     it('Test should click on target that has moved during the action', function () {
         return runTests('testcafe-fixtures/index.test.js', 'Click on element bound to the right bottom corner');
     });
