0b49b4b6c6e67913870d75a440dc564a1eac85c9
Andrey Belym <belym.a.2105@gmail.com>
2017-02-02 11:22:14 +0300

    Disable regenerator for node>=4

--- src/compiler/es-next/load-babel-libs.js
+++ src/compiler/es-next/load-babel-libs.js
@@ -1,3 +1,17 @@
+import NODE_VER from '../../utils/node-version';
+
+function getOptsForPresetEnv () {
+    var opts = { targets: { node: 'current' }, loose: true };
+
+    // NOTE: Disable transforming generators into state-machine for node>=4, because this versions have native generators.
+    // Also this versions have native template literals, regenerator do not work with them:
+    // https://github.com/facebook/regenerator/issues/276
+    if (NODE_VER >= 4)
+        opts.exclude = ['transform-regenerator'];
+
+    return opts;
+}
+
 // NOTE: lazy load heavy dependencies
 export default function loadBabelLibs () {
     return {
@@ -5,6 +19,6 @@ export default function loadBabelLibs () {
         presetStage2:     require('babel-preset-stage-2'),
         transformRuntime: require('babel-plugin-transform-runtime'),
         presetFallback:   require('babel-preset-env').default(null, { loose: true }),
-        presetEnv:        require('babel-preset-env').default(null, { targets: { node: 'current' }, loose: true })
+        presetEnv:        require('babel-preset-env').default(null, getOptsForPresetEnv())
     };
 }
