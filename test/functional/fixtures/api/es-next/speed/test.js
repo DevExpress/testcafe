9160e4f69ff2d1e003168b93860bcb6cb5732d37
Alex Mos <alexander.moskovkin@devexpress.com>
2017-02-16 19:36:56 +0300

    Fix an unstable test (closes #1253)

--- test/functional/fixtures/api/es-next/speed/test.js
+++ test/functional/fixtures/api/es-next/speed/test.js
@@ -4,6 +4,10 @@ describe('[API] Test Speed', function () {
     });
 
     it('Should run test with different speed in iframe', function () {
-        return runTests('./testcafe-fixtures/speed-test.js', 'Speed in iframe', { only: 'chrome', speed: 0.5 });
+        return runTests('./testcafe-fixtures/speed-test.js', 'Speed in iframe', {
+            only:            'chrome',
+            speed:           0.5,
+            selectorTimeout: 10000
+        });
     });
 });
