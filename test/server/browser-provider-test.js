d5b2fd2ec9b66948bfa08ad5a3c0e1d936224086
Andrey Belym <belym.a.2105@gmail.com>
2017-03-02 11:01:25 +0300

    Catch provider init errors (closes #1282)

--- test/server/browser-provider-test.js
+++ test/server/browser-provider-test.js
@@ -4,76 +4,134 @@ var testcafeBrowserTools = require('testcafe-browser-tools');
 var browserProviderPool  = require('../../lib/browser/provider/pool');
 
 describe('Browser provider', function () {
-    var processedBrowserInfo                 = null;
-    var originalBrowserToolsGetBrowserInfo   = null;
-    var originalBrowserToolsOpen             = null;
-    var originalBrowserToolsGetInstallations = null;
+    describe('Path and arguments handling', function () {
+        var processedBrowserInfo                 = null;
+        var originalBrowserToolsGetBrowserInfo   = null;
+        var originalBrowserToolsOpen             = null;
+        var originalBrowserToolsGetInstallations = null;
 
-    function getBrowserInfo (arg) {
-        return browserProviderPool
-            .getBrowserInfo(arg)
-            .then(function (browserInfo) {
-                return browserInfo.provider.openBrowser('id', 'test-url', browserInfo.browserName);
-            })
-            .catch(function (error) {
-                expect(error.message).to.contain('STOP');
-                return processedBrowserInfo;
-            });
-    }
+        function getBrowserInfo (arg) {
+            return browserProviderPool
+                .getBrowserInfo(arg)
+                .then(function (browserInfo) {
+                    return browserInfo.provider.openBrowser('id', 'test-url', browserInfo.browserName);
+                })
+                .catch(function (error) {
+                    expect(error.message).to.contain('STOP');
+                    return processedBrowserInfo;
+                });
+        }
 
-    before(function () {
-        originalBrowserToolsGetBrowserInfo   = testcafeBrowserTools.getBrowserInfo;
-        originalBrowserToolsOpen             = testcafeBrowserTools.open;
-        originalBrowserToolsGetInstallations = testcafeBrowserTools.getInstallations;
+        before(function () {
+            originalBrowserToolsGetBrowserInfo   = testcafeBrowserTools.getBrowserInfo;
+            originalBrowserToolsOpen             = testcafeBrowserTools.open;
+            originalBrowserToolsGetInstallations = testcafeBrowserTools.getInstallations;
 
-        testcafeBrowserTools.getBrowserInfo = function (path) {
-            return {
-                path: path,
-                cmd:  '--internal-arg'
+            testcafeBrowserTools.getBrowserInfo = function (path) {
+                return {
+                    path: path,
+                    cmd:  '--internal-arg'
+                };
             };
+
+            testcafeBrowserTools.open = function (browserInfo) {
+                processedBrowserInfo = browserInfo;
+
+                throw new Error('STOP');
+            };
+
+            testcafeBrowserTools.getInstallations = function () {
+                return new Promise(function (resolve) {
+                    resolve({ chrome: {} });
+                });
+            };
+        });
+
+        after(function () {
+            testcafeBrowserTools.getBrowserInfo   = originalBrowserToolsGetBrowserInfo;
+            testcafeBrowserTools.open             = originalBrowserToolsOpen;
+            testcafeBrowserTools.getInstallations = originalBrowserToolsGetInstallations;
+        });
+
+        it('Should parse browser parameters with arguments', function () {
+            return getBrowserInfo('path:/usr/bin/chrome --arg1 --arg2')
+                .then(function (browserInfo) {
+                    expect(browserInfo.path).to.be.equal('/usr/bin/chrome');
+                    expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
+                });
+        });
+
+        it('Should parse browser parameters with arguments if there are spaces in a file path', function () {
+            return getBrowserInfo('path:`/opt/Google Chrome/chrome` --arg1 --arg2')
+                .then(function (browserInfo) {
+                    expect(browserInfo.path).to.be.equal('/opt/Google Chrome/chrome');
+                    expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
+                });
+        });
+
+        it('Should parse alias with arguments', function () {
+            return getBrowserInfo('chrome --arg1 --arg2')
+                .then(function (browserInfo) {
+                    expect(browserInfo.path).to.be.equal('chrome');
+                    expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
+                });
+        });
+    });
+
+    describe('Init/dispose error handling', function () {
+        var initShouldSuccess = false;
+
+        var dummyProvider = {
+            init: function () {
+                if (initShouldSuccess)
+                    return Promise.resolve();
+
+                return Promise.reject(new Error('Initialization error'));
+            },
+
+            dispose: function () {
+                return Promise.reject(new Error('Dispose error'));
+            }
         };
 
-        testcafeBrowserTools.open = function (browserInfo) {
-            processedBrowserInfo = browserInfo;
+        before(function () {
+            browserProviderPool.addProvider('dummy', dummyProvider);
+        });
 
-            throw new Error('STOP');
-        };
+        after(function () {
+            browserProviderPool.removeProvider('dummy');
+        });
 
-        testcafeBrowserTools.getInstallations = function () {
-            return new Promise(function (resolve) {
-                resolve({ chrome: {} });
-            });
-        };
-    });
+        beforeEach(function () {
+            initShouldSuccess = false;
+        });
 
-    after(function () {
-        testcafeBrowserTools.getBrowserInfo   = originalBrowserToolsGetBrowserInfo;
-        testcafeBrowserTools.open             = originalBrowserToolsOpen;
-        testcafeBrowserTools.getInstallations = originalBrowserToolsGetInstallations;
-    });
+        it('Should catch initialization error', function () {
+            return browserProviderPool
+                .getProvider('dummy')
+                .then(function () {
+                    throw new Error('Promise rejection expected');
+                })
+                .catch(function (error) {
+                    expect(error.message).to.contain('Initialization error');
+                });
+        });
 
-    it('Should parse browser parameters with arguments', function () {
-        return getBrowserInfo('path:/usr/bin/chrome --arg1 --arg2')
-            .then(function (browserInfo) {
-                expect(browserInfo.path).to.be.equal('/usr/bin/chrome');
-                expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
-            });
-    });
+        it('Should catch dispose error', function () {
+            initShouldSuccess = true;
 
-    it('Should parse browser parameters with arguments if there are spaces in a file path', function () {
-        return getBrowserInfo('path:`/opt/Google Chrome/chrome` --arg1 --arg2')
-            .then(function (browserInfo) {
-                expect(browserInfo.path).to.be.equal('/opt/Google Chrome/chrome');
-                expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
-            });
-    });
-
-    it('Should parse alias with arguments', function () {
-        return getBrowserInfo('chrome --arg1 --arg2')
-            .then(function (browserInfo) {
-                expect(browserInfo.path).to.be.equal('chrome');
-                expect(browserInfo.cmd).to.be.equal('--internal-arg --arg1 --arg2');
-            });
+            return browserProviderPool
+                .getProvider('dummy')
+                .then(function () {
+                    return browserProviderPool.dispose();
+                })
+                .then(function () {
+                    throw new Error('Promise rejection expected');
+                })
+                .catch(function (error) {
+                    expect(error.message).to.contain('Dispose error');
+                });
+        });
     });
 });
 
