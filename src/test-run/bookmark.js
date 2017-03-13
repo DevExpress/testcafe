3cb5598addd2a38e95801b96774f6254900c3bcd
inikulin <ifaaan@gmail.com>
2017-03-13 16:31:25 +0300

    Implement test run bookmark

---
+++ src/test-run/bookmark.js
@@ -0,0 +1,82 @@
+import ClientFunctionBuilder from '../client-functions/client-function-builder';
+import {
+    SwitchToMainWindowCommand,
+    SwitchToIframeCommand,
+    SetNativeDialogHandlerCommand,
+    SetTestSpeedCommand,
+    NavigateToCommand
+} from './commands/actions';
+
+class TestRunBookmark {
+    constructor (testRun) {
+        this.testRun = testRun;
+
+        this.url            = 'about:blank';
+        this.dialogHandler  = testRun.activeDialogHandler;
+        this.iframeSelector = testRun.activeIframeSelector;
+        this.speed          = testRun.speed;
+        this.ctx            = testRun.ctx;
+        this.fixtureCtx     = testRun.fixtureCtx;
+    }
+
+    async _restoreDialogHandler () {
+        if (this.testRun.activeDialogHandler !== this.dialogHandler) {
+            var restoreDialogCommand = new SetNativeDialogHandlerCommand({ dialogHandler: this.dialogHandler });
+
+            await this.testRun.executeCommand(restoreDialogCommand);
+        }
+    }
+
+    async _restoreSpeed () {
+        if (this.testRun.speed !== this.speed) {
+            var restoreSpeedCommand = new SetTestSpeedCommand({ speed: this.speed });
+
+            await this.testRun.executeCommand(restoreSpeedCommand);
+        }
+    }
+
+    async _restoreWorkingFrame () {
+        if (this.testRun.activeIframeSelector !== this.iframeSelector) {
+            var switchWorkingFrameCommand = this.iframeSelector ?
+                                            new SwitchToIframeCommand({ selector: this.iframeSelector }) :
+                                            new SwitchToMainWindowCommand();
+
+            await this.testRun.executeCommand(switchWorkingFrameCommand);
+        }
+    }
+
+    async _restorePage () {
+        var navigateCommand = new NavigateToCommand({ url: this.url });
+
+        await this.testRun.executeCommand(navigateCommand);
+    }
+
+    async restore () {
+        this.testRun.ctx        = this.ctx;
+        this.testRun.fixtureCtx = this.fixtureCtx;
+
+        await this._restoreDialogHandler();
+        await this._restoreSpeed();
+        await this._restorePage();
+        await this._restoreWorkingFrame();
+    }
+}
+
+export default async function createBookmark (testRun) {
+    var bookmark = new TestRunBookmark(testRun);
+
+    if (testRun.activeIframeSelector)
+        await testRun.execCommand(new SwitchToMainWindowCommand());
+
+    var builder = new ClientFunctionBuilder(() => {
+        /* eslint-disable no-undef */
+        return window.location.href;
+        /* eslint-enable no-undef */
+    }, { boundTestRun: testRun });
+
+    var getLocation = builder.getFunction();
+
+    bookmark.url = await getLocation();
+
+    return bookmark;
+}
