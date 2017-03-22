9c57b540fa0dae86fa27c1bdf4113d82450ca284
Georgiy Abbasov <georgiy.abbasov@devexpress.com>
2017-03-22 13:07:07 +0300

    fix iframe test

--- test/functional/fixtures/api/es-next/hooks/onEachPage/test.js
+++ test/functional/fixtures/api/es-next/hooks/onEachPage/test.js
@@ -30,7 +30,13 @@ describe('API fixture.onEachPage hook', function () {
     });
 
     it('Should switch hook context window with test', function () {
-        return runTests('./testcafe-fixtures/on-each-page.test.js', 'Run hook in the current window context', { only: 'chrome' });
+        // NOTE: we set selectorTimeout to a large value to wait for an iframe to load
+        // on the farm (it is fast locally but can take some time on the farm)
+
+        return runTests('./testcafe-fixtures/on-each-page.test.js', 'Run hook in the current window context', {
+            only:            'chrome',
+            selectorTimeout: 10000
+        });
     });
 
     it('Should restart hook if page reloaded during its execution', function () {
