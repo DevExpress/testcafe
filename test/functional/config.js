f61ff0d7f663531307817ef67a4e5ad4c5473483
Georgiy Abbasov <georgiy.abbasov@devexpress.com>
2017-04-09 22:37:45 +0300

    Make linter happy

--- test/functional/config.js
+++ test/functional/config.js
@@ -75,11 +75,11 @@ testingEnvironments[testingEnvironmentNames.saucelabsMobileBrowsers] = {
             alias:     'android'
         },
         {
-            os:         'ios',
+            os:        'ios',
             osVersion: '9.3',
-            browser:    'Mobile Safari',
-            device:     'iPad Mini 4',
-            alias:      'ipad'
+            browser:   'Mobile Safari',
+            device:    'iPad Mini 4',
+            alias:     'ipad'
         },
         {
             os:        'ios',
