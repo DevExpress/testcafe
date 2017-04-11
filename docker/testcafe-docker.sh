e8235a8653ac0d2c1e00131b58dd3c834e930e96
Andrey Belym <belym.a.2105@gmail.com>
2017-04-11 16:49:48 +0300

    Implement builds of Docker images (closes #1141) (#1197)
    * Implement builds of Docker images (closes #1141)
    
    * Detect Docker environment

---
+++ docker/testcafe-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+XVFB_SCREEN_WIDTH=${SCREEN_WIDTH-1280}
+XVFB_SCREEN_HEIGHT=${SCREEN_HEIGHT-720}
+
+dbus-daemon --session --fork
+Xvfb :1 -screen 0 "${XVFB_SCREEN_WIDTH}x${XVFB_SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
+export DISPLAY=:1.0
+fluxbox >/dev/null 2>&1 &
+node /opt/testcafe/bin/testcafe.js "$@"
