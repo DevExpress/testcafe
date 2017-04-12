38ba8bd22fe486079ed8a43afe1c256675f42be7
Georgiy Abbasov <georgiy.abbasov@devexpress.com>
2017-04-12 23:12:47 +0300

    only gh-1311

--- test/functional/fixtures/regression/gh-1311/test.js
+++ test/functional/fixtures/regression/gh-1311/test.js
@@ -3,7 +3,7 @@ describe('[Regression](GH-1311)', function () {
         return runTests('testcafe-fixtures/index.test.js', 'Click on select option (Non IE)', { skip: ['ie', 'ie 9', 'ie 10', 'edge'] });
     });
 
-    it('Should not raise "input" event in IE while changing value in select input', function () {
+    it.only('Should not raise "input" event in IE while changing value in select input', function () {
         return runTests('testcafe-fixtures/index.test.js', 'Click on select option (IE)', { only: ['ie', 'ie 9', 'ie 10', 'edge'] });
     });
 });
