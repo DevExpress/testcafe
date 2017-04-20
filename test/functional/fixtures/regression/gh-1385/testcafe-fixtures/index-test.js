c2f2c4471e58264555a102debd5ee190c57fd577
AlexanderMoskovkin <alexander.moskovkin@devexpress.com>
2017-04-20 10:41:51 +0300

    Fix linting

--- test/functional/fixtures/regression/gh-1385/testcafe-fixtures/index-test.js
+++ test/functional/fixtures/regression/gh-1385/testcafe-fixtures/index-test.js
@@ -1,16 +1,10 @@
+import { ClientFunction } from 'testcafe';
+
 fixture `gh-1385`
     .page `http://localhost:3000/fixtures/regression/gh-1385/pages/index.html`;
 
 test('Check "...fromPoint" functions', async t => {
-    await t.click('#div');
-
-    const {
-              caretPositionFromPointSupported,
-              caretRangeFromPointSupported,
-              elementFromPointId,
-              caretPositionFromPointData,
-              caretRangeFromPointData
-          } = await t.eval(() => {
+    const getDataFromClient = ClientFunction(() => {
         return {
             caretPositionFromPointSupported: !!document.caretPositionFromPoint,
             caretRangeFromPointSupported:    !!document.caretRangeFromPoint,
@@ -19,6 +13,16 @@ test('Check "...fromPoint" functions', async t => {
             caretRangeFromPointData:         window.caretRangeFromPointData
         };
     });
+
+    await t.click('#div');
+
+    const {
+              caretPositionFromPointSupported,
+              caretRangeFromPointSupported,
+              elementFromPointId,
+              caretPositionFromPointData,
+              caretRangeFromPointData
+          } = await getDataFromClient();
 
     await t.expect(elementFromPointId).eql('div');
 
