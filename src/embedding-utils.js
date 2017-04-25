3bc1d12044006db480fe648f6e2134b4d9611309
Andrey Churkin <andrey.churkin@devexpress.com>
2017-04-25 12:11:52 +0300

    Add few classes to embedding utils

--- src/embedding-utils.js
+++ src/embedding-utils.js
@@ -2,11 +2,15 @@ import ReporterPluginHost from './reporter/plugin-host';
 import TestRunErrorFormattableAdapter from './errors/test-run/formattable-adapter';
 import * as testRunErrors from './errors/test-run';
 import TestRun from './test-run';
+import COMMAND_TYPE from './test-run/commands/type';
+import Assignable from './utils/assignable';
 
 export default {
     TestRunErrorFormattableAdapter,
     TestRun,
     testRunErrors,
+    COMMAND_TYPE,
+    Assignable,
 
     buildReporterPlugin (pluginFactory, outStream) {
         var plugin = pluginFactory();
