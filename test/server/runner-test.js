394e769e0d426e9867195a9dcf13de905c80e98e
Casey Webb <notcaseywebb@gmail.com>
2017-07-17 14:10:47 -0500

    Fix reporter fallback test

--- test/server/runner-test.js
+++ test/server/runner-test.js
@@ -159,11 +159,11 @@ describe('Runner', function () {
         });
 
         it('Should fallback to the default reporter if reporter was not set', function () {
-            runner._runTask = function (reporterPlugin) {
-                expect(reporterPlugin.reportFixtureStart).to.be.a('function');
-                expect(reporterPlugin.reportTestDone).to.be.a('function');
-                expect(reporterPlugin.reportTaskStart).to.be.a('function');
-                expect(reporterPlugin.reportTaskDone).to.be.a('function');
+            runner._runTask = function (reporterPlugins) {
+                expect(reporterPlugins[0].reportFixtureStart).to.be.a('function');
+                expect(reporterPlugins[0].reportTestDone).to.be.a('function');
+                expect(reporterPlugins[0].reportTaskStart).to.be.a('function');
+                expect(reporterPlugins[0].reportTaskDone).to.be.a('function');
 
                 return Promise.resolve({});
             };
