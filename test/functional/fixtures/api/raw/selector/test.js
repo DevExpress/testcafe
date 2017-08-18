bf90cced2b6def4ea26828cca1b0ba7ae432af8e
AlexanderMoskovkin <alexander.moskovkin@devexpress.com>
2017-08-18 13:33:11 +0300

    Fix a typo

--- test/functional/fixtures/api/raw/selector/test.js
+++ test/functional/fixtures/api/raw/selector/test.js
@@ -2,7 +2,7 @@ var errorInEachBrowserContains = require('../../../../assertion-helper.js').erro
 
 // NOTE: the whole Selectors API is tested in the es-next tests. The
 // goal of this tests - check different types of Selector API constructions
-// (like deriative selectors, ClientFunctions, RegExp usage etc..)
+// (like derivative selectors, ClientFunctions, RegExp usage etc..)
 describe('[Raw API] Selector', function () {
     it('Should work with different type of selectors', function () {
         return runTests('./testcafe-fixtures/test.testcafe', 'Different types of selectors', { only: 'chrome' });
