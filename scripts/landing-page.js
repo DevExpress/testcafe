9df4f1f709d5a23cd08eb1d838dbdd3b227e03bf
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2017-10-10 19:47:09 +0300

    [docs] Update the website for v0.18.0 (#1863)
    * Update the website for v0.18.0
    
    * Fix the publication list

---
+++ scripts/landing-page.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#page-object-popover").dxPopover({
+        target: "#page-object-link",
+        showEvent: "dxclick",
+        position: "top",
+        width: 300
+    });
+})
