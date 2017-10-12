3cf61e822bba0819fd682c19453590702ce5efa9
VasilyStrelyaev <bazilstrel@gmail.com>
2017-10-12 14:30:35 +0300

    Some more fixes for the website

--- scripts/landing-page.js
+++ scripts/landing-page.js
@@ -5,4 +5,18 @@ $(function(){
         position: "top",
         width: 300
     });
+
+    
+    
+    var GET_STARTED_CLICKED_FLAG = 'get-started-clicked';
+
+    $('.get-started-button').on('click', function () {
+        if(!window.localStorage.getItem(GET_STARTED_CLICKED_FLAG)) {
+            ga('send', 'event', 'landingPage', 'getStartedClicked');
+            window.localStorage.setItem(GET_STARTED_CLICKED_FLAG, true);
+        }
+    });
+
+    
 })
+
