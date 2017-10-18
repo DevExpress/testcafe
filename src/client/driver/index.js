a8817cbd3ba8bd84e5271837e354f3decefb34cb
Andrey Churkin <andrey.churkin@devexpress.com>
2017-10-18 17:46:20 +0300

    Publish client 'ElementSnapshot' and 'NodeSnapshot' classes ( close #1885)

--- src/client/driver/index.js
+++ src/client/driver/index.js
@@ -2,6 +2,7 @@ import hammerhead from './deps/hammerhead';
 import Driver from './driver';
 import IframeDriver from './iframe-driver';
 import ScriptExecutionBarrier from './script-execution-barrier';
+import embeddingUtils from './embedding-utils';
 
 
 hammerhead.nativeMethods.objectDefineProperty.call(window, window, '%testCafeDriver%', {
@@ -23,6 +24,13 @@ hammerhead.nativeMethods.objectDefineProperty.call(window, window, '%ScriptExecu
     configurable: false,
     writable:     false,
     value:        ScriptExecutionBarrier
+});
+
+hammerhead.nativeMethods.objectDefineProperty.call(window, window, '%testCafeEmbeddingUtils%', {
+    enumerable:   false,
+    configurable: false,
+    writable:     false,
+    value:        embeddingUtils
 });
 
 /* eslint-disable no-undef */
