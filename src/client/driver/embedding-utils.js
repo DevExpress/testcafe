70446cceab1624adbdf6825269d2215595987f22
Andrey Churkin <alam17@mail.ru>
2017-10-18 19:01:45 +0300

    Publish client 'ElementSnapshot' and 'NodeSnapshot' classes ( close #1885) (#1893)

---
+++ src/client/driver/embedding-utils.js
@@ -0,0 +1,6 @@
+import { ElementSnapshot, NodeSnapshot } from './command-executors/client-functions/selector-executor/node-snapshots';
+
+export default {
+    NodeSnapshot,
+    ElementSnapshot
+};
