ad9480455f25ea3872ace937afa6e2f12d4673f9
Andrey Belym <belym.a.2105@gmail.com>
2018-02-21 11:14:43 +0300

    Make screenshot mark black'n'white (closes #2145)

---
+++ src/screenshots/constants.js
@@ -0,0 +1,6 @@
+// -------------------------------------------------------------
+// WARNING: this file is used by both the client and the server.
+// Do not use any browser or node-specific API!
+// -------------------------------------------------------------
+export const MARK_LENGTH       = 32;
+export const MARK_RIGHT_MARGIN = 10;
