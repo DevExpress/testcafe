e6eb287d24272c8edbe735aef6a0625b32bbf492
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-02-22 14:23:44 +0300

    exclude mobile browsers

--- test/functional/fixtures/regression/gh-2153/test.js
+++ test/functional/fixtures/regression/gh-2153/test.js
@@ -1,5 +1,5 @@
 describe('[Regression](GH-2153)', function () {
     it("Shouldn't call document and window handlers extra times", function () {
-        return runTests('testcafe-fixtures/index.js');
+        return runTests('testcafe-fixtures/index.js', null, { only: 'chrome,ie,firefox' });
     });
 });
