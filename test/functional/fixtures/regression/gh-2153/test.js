442a3caf460f66e812777051450bbe2211667646
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-02-26 11:21:21 +0300

    rewrite test in a better way

--- test/functional/fixtures/regression/gh-2153/test.js
+++ test/functional/fixtures/regression/gh-2153/test.js
@@ -1,5 +1,5 @@
 describe('[Regression](GH-2153)', function () {
-    it("Shouldn't call document and window handlers extra times", function () {
-        return runTests('testcafe-fixtures/index.js', null, { only: 'chrome,ie,firefox' });
+    it("Shadow element should not appear in user event handler", function () {
+        return runTests('testcafe-fixtures/index.js');
     });
 });
--- test/functional/fixtures/regression/gh-2153/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-2153/testcafe-fixtures/index.js
@@ -1,15 +1,13 @@
 import { Selector } from 'testcafe';
 
-fixture `GH-2153 - Shouldn't call document and window handlers extra times`
+fixture `GH-2153 - Shadow element should not appear in user event handler`
     .page `http://localhost:3000/fixtures/regression/gh-2153/pages/index.html`;
 
-test(`Shouldn't call document and window handlers extra times`, async t => {
+test(`Shadow element should not appear in user event handler`, async t => {
     const select  = Selector('#select');
     const option  = select.find('option').withText('Two');
-    const counter = Selector('#logger');
 
     await t
         .click(select)
-        .click(option)
-        .expect(counter.innerText).eql('window|document|window|document|');
+        .click(option);
 });
