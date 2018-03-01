426012f423f326b0b4674a5d7ccf73ee9b564e38
Andrey Belym <belym.a.2105@gmail.com>
2018-03-01 14:09:53 +0300

    Finally fix stack-chain issue (closes #2166) (#2178)
    * Fix stack-chain issue
    
    * Fix remarks
    
    * Get rid of stack-chain

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,189 +1,20 @@
-import fs from 'fs';
-import chalk from 'chalk';
 import resolveCwd from 'resolve-cwd';
-import browserProviderPool from '../browser/provider/pool';
-import { GeneralError, APIError } from '../errors/runtime';
-import MESSAGE from '../errors/runtime/message';
-import CliArgumentParser from './argument-parser';
 import log from './log';
-import remotesWizard from './remotes-wizard';
-import createTestCafe from '../';
 
 
-const TERMINATION_TYPES = {
-    sigint:   'sigint',
-    sigbreak: 'sigbreak',
-    shutdown: 'shutdown'
-};
-
-var showMessageOnExit = true;
-var exitMessageShown  = false;
-var exiting           = false;
-
-var handledSignalsCount = {
-    [TERMINATION_TYPES.sigint]:   0,
-    [TERMINATION_TYPES.sigbreak]: 0,
-    [TERMINATION_TYPES.shutdown]: 0
-};
-
-function exitHandler (terminationType) {
-    handledSignalsCount[terminationType]++;
-
-    if (showMessageOnExit && !exitMessageShown) {
-        exitMessageShown = true;
-
-        log.hideSpinner();
-        log.write('Stopping TestCafe...');
-        log.showSpinner();
-
-        process.on('exit', () => log.hideSpinner(true));
-    }
-
-    if (exiting || handledSignalsCount[terminationType] < 2)
-        return;
-
-    exiting = true;
-
-    exit(0);
-}
-
-function setupExitHandler () {
-    process.on('SIGINT', () => exitHandler(TERMINATION_TYPES.sigint));
-    process.on('SIGBREAK', () => exitHandler(TERMINATION_TYPES.sigbreak));
-
-    process.on('message', message => message === 'shutdown' && exitHandler(TERMINATION_TYPES.shutdown));
-}
-
-function exit (code) {
-    log.hideSpinner(true);
-
-    // NOTE: give a process time to flush the output.
-    // It's necessary in some environments.
-    setTimeout(() => process.exit(code), 0);
-}
-
-function error (err) {
-    log.hideSpinner();
-
-    var message = null;
-
-    // HACK: workaround for the `instanceof` problem
-    // (see: http://stackoverflow.com/questions/33870684/why-doesnt-instanceof-work-on-instances-of-error-subclasses-under-babel-node)
-    if (err.constructor === GeneralError)
-        message = err.message;
-
-    else if (err.constructor === APIError)
-        message = err.coloredStack;
-
-    else
-        message = err.stack;
-
-    log.write(chalk.red('ERROR ') + message + '\n');
-    log.write(chalk.gray('Type "testcafe -h" for help.'));
-
-    exit(1);
-}
-
-async function runTests (argParser) {
-    var opts              = argParser.opts;
-    var port1             = opts.ports && opts.ports[0];
-    var port2             = opts.ports && opts.ports[1];
-    var externalProxyHost = opts.proxy;
-
-    log.showSpinner();
-
-    var testCafe       = await createTestCafe(opts.hostname, port1, port2);
-    var concurrency    = argParser.concurrency || 1;
-    var remoteBrowsers = await remotesWizard(testCafe, argParser.remoteCount, opts.qrCode);
-    var browsers       = argParser.browsers.concat(remoteBrowsers);
-    var runner         = testCafe.createRunner();
-    var failed         = 0;
-    var reporters      = argParser.opts.reporters.map(r => {
-        return {
-            name:      r.name,
-            outStream: r.outFile ? fs.createWriteStream(r.outFile) : void 0
-        };
-    });
-
-    reporters.forEach(r => runner.reporter(r.name, r.outStream));
-
-    runner
-        .useProxy(externalProxyHost)
-        .src(argParser.src)
-        .browsers(browsers)
-        .concurrency(concurrency)
-        .filter(argParser.filter)
-        .screenshots(opts.screenshots, opts.screenshotsOnFails)
-        .startApp(opts.app, opts.appInitDelay);
-
-    runner.once('done-bootstrapping', () => log.hideSpinner());
-
-    try {
-        failed = await runner.run(opts);
-    }
-
-    finally {
-        showMessageOnExit = false;
-        await testCafe.close();
-    }
-
-    exit(failed);
-}
-
-async function listBrowsers (providerName = 'locally-installed') {
-    var provider = await browserProviderPool.getProvider(providerName);
-
-    if (!provider)
-        throw new GeneralError(MESSAGE.browserProviderNotFound, providerName);
-
-    if (provider.isMultiBrowser) {
-        var browserNames = await provider.getBrowserList();
-
-        await browserProviderPool.dispose();
-
-        if (providerName === 'locally-installed')
-            console.log(browserNames.join('\n'));
-        else
-            console.log(browserNames.map(browserName => `"${providerName}:${browserName}"`).join('\n'));
-    }
-    else
-        console.log(`"${providerName}"`);
-
-    exit(0);
-}
-
-function useLocalInstallation () {
-    var local = resolveCwd('testcafe/lib/cli');
+function getLocalInstallation () {
+    const local = resolveCwd('testcafe/lib/cli');
 
     if (local && local !== __filename) {
         log.write('Using locally installed version of TestCafe.');
-        require(local);
-        return true;
+        return local;
     }
 
-    return false;
+    return '';
 }
 
+(function loader () {
+    const cliPath = getLocalInstallation() || require.resolve('./cli');
 
-(async function cli () {
-    if (useLocalInstallation())
-        return;
-
-    setupExitHandler(exitHandler);
-
-    try {
-        var argParser = new CliArgumentParser();
-
-        await argParser.parse(process.argv);
-
-        if (argParser.opts.listBrowsers)
-            await listBrowsers(argParser.opts.providerName);
-        else
-            await runTests(argParser);
-    }
-    catch (err) {
-        showMessageOnExit = false;
-        error(err);
-    }
+    require(cliPath);
 })();
-
--- src/errors/stack-cleaning-hook.js
+++ src/errors/stack-cleaning-hook.js
@@ -1,18 +1,26 @@
+import errorStackParser from 'error-stack-parser';
 import createStackFilter from './create-stack-filter';
+
 
 const ORIGINAL_STACK_TRACE_LIMIT = Error.stackTraceLimit;
 const STACK_TRACE_LIMIT          = 200;
+const TOP_ANONYMOUS_FRAME_RE     = /\s+at\s<anonymous>$/;
 
-// NOTE: 'stack-chain' can't be imported directly, because only one version of it can be imported in a process.
-// It causes the process to crash, when older globally installed TestCafe version loads newer locally installed version.
-// See the 'stack-chain' code at https://github.com/AndreasMadsen/stack-chain/blob/001f69e35ecd070c68209d13c4325fe5d23fc136/index.js#L10
-let stackTrace = null;
 
 export default {
     isEnabled: false,
 
-    _hook (err, frames) {
-        return frames.filter(createStackFilter(ORIGINAL_STACK_TRACE_LIMIT));
+    _getFrames (error) {
+        try {
+            return errorStackParser.parse(error);
+        }
+        catch (e) {
+            return [];
+        }
+    },
+
+    _renderFrameInfo (frames) {
+        return frames.map(frame => frame.getSource()).join('\n');
     },
 
     get enabled () {
@@ -25,18 +33,28 @@ export default {
 
         this.isEnabled = val;
 
-        if (!stackTrace)
-            stackTrace = require('stack-chain');
-
-        if (this.isEnabled) {
-            // NOTE: Babel errors may have really deep stacks,
-            // so we increase stack trace capacity
+        // NOTE: Babel errors may have really deep stacks,
+        // so we increase stack trace capacity
+        if (this.isEnabled)
             Error.stackTraceLimit = STACK_TRACE_LIMIT;
-            stackTrace.filter.attach(this._hook);
-        }
-        else {
+        else
             Error.stackTraceLimit = ORIGINAL_STACK_TRACE_LIMIT;
-            stackTrace.filter.deattach(this._hook);
-        }
+    },
+
+    cleanError (error) {
+        error.stack = error.stack.replace(TOP_ANONYMOUS_FRAME_RE, '');
+
+        let frames = this._getFrames(error);
+
+        if (!frames.length)
+            return error;
+
+        error.stack = error.stack.replace(this._renderFrameInfo(frames), '');
+
+        frames = frames.filter(createStackFilter(ORIGINAL_STACK_TRACE_LIMIT));
+
+        error.stack += this._renderFrameInfo(frames);
+
+        return error;
     }
 };
