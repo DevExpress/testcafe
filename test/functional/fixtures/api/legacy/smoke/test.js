82986ea8cb88810b8f512ce18c1b75e65403ca11
Andrey Belym <belym.a.2105@gmail.com>
2018-03-13 13:07:11 +0300

    Skip legacy smoke tests for iPad and iPhone

--- test/functional/fixtures/api/legacy/smoke/test.js
+++ test/functional/fixtures/api/legacy/smoke/test.js
@@ -5,11 +5,11 @@ var path   = require('path');
 
 describe('[Legacy] Smoke tests', function () {
     it('Should run basic tests', function () {
-        return runTests(globby(path.join(__dirname, './testcafe-fixtures/basic/*test.js')), null);
+        return runTests(globby(path.join(__dirname, './testcafe-fixtures/basic/*test.js')), null, { skip: 'iphone,ipad' });
     });
 
     it('Should fail on errors', function () {
-        return runTests('./testcafe-fixtures/errors.test.js', null, { shouldFail: true })
+        return runTests('./testcafe-fixtures/errors.test.js', null, { shouldFail: true, skip: 'iphone,ipad' })
             .catch(function (errs) {
                 expect(errs[0]).contains('A target element of the click action has not been found in the DOM tree.');
             });
