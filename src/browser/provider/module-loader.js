afb791536293883d51e8921c2c59287c55b602ab
Bradley Smith <bsmithb2@gmail.com>
2018-03-21 10:01:40 +1100

    Updated to accept command line args of @private/package rather than the full package name as per request in PR #2229

--- src/browser/provider/module-loader.js
+++ src/browser/provider/module-loader.js
@@ -1,11 +1,12 @@
-const BROWSER_PROVIDER_SCOPED_PACKAGE_RE = /^@\S+\/testcafe-browser-provider\S/;
+const BROWSER_PROVIDER_SCOPED_PACKAGE_RE = /^@\S+\/\S/;
+const BROWSER_PROVIDER_PARSER_RE = /@([\w-]+)\/(.*)/g;
 
 export default class BrowserProviderModuleLoader {
     loadModule (providerName) {
         var providerObject = null;
 
         if (this.isPrivateModule(providerName))
-            providerObject = require(`${providerName}`);
+            providerObject = require(`${this.fillPrivateModuleValue(providerName)}`);
         else
             providerObject = require(`testcafe-browser-provider-${providerName}`);
         return providerObject;
@@ -15,4 +16,9 @@ export default class BrowserProviderModuleLoader {
         return BROWSER_PROVIDER_SCOPED_PACKAGE_RE.exec(providerName);
     }
 
+    fillPrivateModuleValue (providerName) {
+        var parsedProviderName = BROWSER_PROVIDER_PARSER_RE.exec(providerName);
+
+        return `@${parsedProviderName[1]}/testcafe-browser-provider-${parsedProviderName[2]}`;
+    }
 }
--- test/server/browser-provider-module-loader-test.js
+++ test/server/browser-provider-module-loader-test.js
@@ -7,10 +7,21 @@ describe('Browser provider module loader', function () {
             expect(new BrowserProviderModuleLoader().isPrivateModule('@private/testcafe-browser-provider-package')).to.be.not.null;
         });
         it('Should not find @private/private-package', function () {
-            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/private-package')).to.be.null;
+            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/private-package')).to.be.not.null;
+        });
+        it('Should not find @private/package', function () {
+            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/package')).to.be.not.null;
         });
         it('Should not find browserprovider', function () {
             expect(new BrowserProviderModuleLoader().isPrivateModule('browserprovider')).to.be.null;
+        });
+    });
+    describe('private module loading package name loading', function () {
+        it('Should return @private/testcafe-browser-provider-package for @private/package', function () {
+            var result = new BrowserProviderModuleLoader().fillPrivateModuleValue('@private/package');
+
+            expect(result).to.be.not.null;
+            expect(result).to.equal('@private/testcafe-browser-provider-package');
         });
     });
 });
