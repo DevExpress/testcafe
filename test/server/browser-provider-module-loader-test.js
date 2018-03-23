4ee30964c54223c38b938bea6cb9da59074a7632
Bradley Smith <bsmithb2@gmail.com>
2018-03-23 11:57:08 +1100

    Update tests to match code review renaming

--- test/server/browser-provider-module-loader-test.js
+++ test/server/browser-provider-module-loader-test.js
@@ -4,21 +4,21 @@ var expect = require('chai').expect;
 describe('Browser provider module loader', function () {
     describe('module loading regex', function () {
         it('Should find @private/testcafe-browser-provider-package', function () {
-            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/testcafe-browser-provider-package')).to.be.not.null;
+            expect(new BrowserProviderModuleLoader().isScopedProvider('@private/testcafe-browser-provider-package')).to.be.not.null;
         });
         it('Should not find @private/private-package', function () {
-            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/private-package')).to.be.not.null;
+            expect(new BrowserProviderModuleLoader().isScopedProvider('@private/private-package')).to.be.not.null;
         });
         it('Should not find @private/package', function () {
-            expect(new BrowserProviderModuleLoader().isPrivateModule('@private/package')).to.be.not.null;
+            expect(new BrowserProviderModuleLoader().isScopedProvider('@private/package')).to.be.not.null;
         });
         it('Should not find browserprovider', function () {
-            expect(new BrowserProviderModuleLoader().isPrivateModule('browserprovider')).to.be.null;
+            expect(new BrowserProviderModuleLoader().isScopedProvider('browserprovider')).to.be.null;
         });
     });
     describe('private module loading package name loading', function () {
         it('Should return @private/testcafe-browser-provider-package for @private/package', function () {
-            var result = new BrowserProviderModuleLoader().fillPrivateModuleValue('@private/package');
+            var result = new BrowserProviderModuleLoader().getScopedProviderModuleName('@private/package');
 
             expect(result).to.be.not.null;
             expect(result).to.equal('@private/testcafe-browser-provider-package');
