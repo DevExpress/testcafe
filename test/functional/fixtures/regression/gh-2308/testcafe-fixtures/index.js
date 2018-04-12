20ee77ff9e20acfc90d265040568b51347283f7c
LavrovArtem <artjomlav@gmail.com>
2018-04-12 18:45:15 +0300

    add test

---
+++ test/functional/fixtures/regression/gh-2308/testcafe-fixtures/index.js
@@ -0,0 +1,13 @@
+fixture `GH-2308 - Use a location port for service urls`
+    .page `http://localhost:3000/fixtures/regression/gh-2308/pages/index.html`;
+
+test('Load a top window on the cross-domain port', async t => {
+    await t
+        .switchToIframe('iframe')
+        .click('[type="submit"]')
+        .switchToMainWindow()
+        .click('body');
+});
+
+test('Second test', async t => {
+});
