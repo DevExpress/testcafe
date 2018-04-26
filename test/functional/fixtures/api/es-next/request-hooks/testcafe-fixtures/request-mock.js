dd6a2de6fd7ad626aec67e781fd8245fa66573af
miherlosev@mail.ru <Ein7Onai>
2018-04-26 12:30:06 +0300

    fix 'RequestMock does not send response' (close #2336)

--- test/functional/fixtures/api/es-next/request-hooks/testcafe-fixtures/request-mock.js
+++ test/functional/fixtures/api/es-next/request-hooks/testcafe-fixtures/request-mock.js
@@ -1,8 +1,31 @@
 import { Selector, RequestMock } from 'testcafe';
 
+const testPageMarkup = `
+    <html>
+        <body>
+            <h1>Mocked page</h1>
+            <h2></h2>
+            <button onclick="sendRequest()"></button>
+            <script>
+                function sendRequest() {
+                    fetch('http://dummy-url.com/get')
+                        .then(res => {
+                            return res.text();
+                        })
+                        .then(text => {
+                            document.querySelector('h2').textContent = text;                            
+                        });                    
+                }
+            </script>
+        </body>
+    </html>
+`;
+
 const requestMock = RequestMock()
     .onRequestTo('http://dummy-url.com')
-    .respond('<html><body><h1>Mocked page</h1></body></html>')
+    .respond(testPageMarkup)
+    .onRequestTo('http://dummy-url.com/get')
+    .respond('Data from mocked fetch request')
     .onRequestTo('https://another-dummy-url.com')
     .respond();
 
@@ -14,6 +37,8 @@ test
         await t
             .navigateTo('http://dummy-url.com')
             .expect(Selector('h1').textContent).eql('Mocked page')
+            .click('button')
+            .expect(Selector('h2').textContent).eql('Data from mocked fetch request')
             .navigateTo('https://another-dummy-url.com')
             .expect(Selector('body').exists).ok();
     });
