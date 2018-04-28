a42ee6df4d8104cd85d52adde38350982a0e06c2
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-04-28 09:43:50 +0300

    remove functional test

--- test/functional/fixtures/regression/gh-2325/test.js
+++
@@ -1,5 +0,0 @@
-describe('[Regression](GH-2325)', function () {
-    it('Simulate screenX and screenY properties of event', function () {
-        return runTests('testcafe-fixtures/index.js', null, { skip: ['iphone', 'ipad', 'android'] });
-    });
-});
