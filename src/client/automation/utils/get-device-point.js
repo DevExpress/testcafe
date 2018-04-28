b2754a4b43c714b49dd735ee129504ae598abeee
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-04-28 08:51:46 +0300

    fix obj is null problem

--- src/client/automation/utils/get-device-point.js
+++ src/client/automation/utils/get-device-point.js
@@ -1,6 +1,11 @@
 export default function getDevicePoint (clientPoint) {
-    const x = (window.screenLeft || window.screenX) + clientPoint.x;
-    const y = (window.screenTop || window.screenY) + clientPoint.y;
+    if (!clientPoint)
+        return null;
+
+    const screenLeft = window.screenLeft || window.screenX;
+    const screenTop  = window.screenTop || window.screenY;
+    const x          = screenLeft + clientPoint.x;
+    const y          = screenTop + clientPoint.y;
 
     return { x, y };
 }
