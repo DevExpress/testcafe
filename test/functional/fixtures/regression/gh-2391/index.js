bad5bc3a71aef3914c49db6fd08a9963022f56a8
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-05-23 14:11:16 +0300

    skip edge because of #2450

--- test/functional/fixtures/regression/gh-2391/index.js
+++
@@ -1,5 +0,0 @@
-describe('[Regression](GH-2391)', function () {
-    it('should scroll to element if it is hidden by fixed', function () {
-        return runTests('testcafe-fixtures/index.js');
-    });
-});
