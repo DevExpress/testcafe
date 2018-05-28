5bf65b0fd93d7a083fa8515f2408ec9540ca7c0d
Andrey Belym <belym.a.2105@gmail.com>
2018-05-28 16:18:26 +0300

    Fix mistypes

--- src/utils/moment-loader.js
+++ src/utils/moment-loader.js
@@ -16,32 +16,32 @@ function getSideMomentModulePath (sidePath) {
     }
 }
 
-funciton getCachedAndCleanModules (modulePath) {
+function getCachedAndCleanModules (modulePath) {
     const cachedModule = require.cache[modulePath];
-    
+
     delete require.cache[modulePath];
-    
+
     require(modulePath);
-    
+
     return { cachedModule, cleanModule: require.cache[modulePath] };
 }
 
 function getModulesPaths () {
     const durationFormatModulePath = require.resolve('moment-duration-format');
-    
+
     return {
         durationFormatModulePath,
-        
+
         mainMomentModulePath: require.resolve('moment'),
         sideMomentModulePath: getSideMomentModulePath(durationFormatModulePath)
-    };  
+    };
 }
 
 function getMomentModules ({ mainMomentModulePath, sideMomentModulePath }) {
-    return { 
-        sideModule: require.cache[sideMomentModulePath], 
-        
-        ...getCachedAndCleanModules(mainMomentModulePath) 
+    return {
+        sideModule: require.cache[sideMomentModulePath],
+
+        ...getCachedAndCleanModules(mainMomentModulePath)
     };
 }
 
@@ -49,11 +49,11 @@ function getMomentModuleWithDurationFormatPatch () {
     const modulesPaths  = getModulesPaths();
     const momentModules = getMomentModules(modulesPaths);
 
-    if (modulesPaths.sideMomentModulePath && (modulesPaths.sideMomentModulePath !== modulesPaths.mainMomentModulePath) {
+    if (modulesPaths.sideMomentModulePath && modulesPaths.sideMomentModulePath !== modulesPaths.mainMomentModulePath) {
         require.cache[modulesPaths.sideMomentModulePath] = momentModules.mainModule;
-        
+
         require(modulesPaths.durationFormatModulePath);
-    
+
         restoreInitialCacheState(momentModules.sideModule, modulesPaths.sideMomentModulePath);
     }
     else {
