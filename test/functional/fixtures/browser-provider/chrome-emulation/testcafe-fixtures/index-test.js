5ca95522d5d8fc7dd8c626170a740a5637dc1e52
Andrey Belym <belym.a.2105@gmail.com>
2018-06-05 18:11:22 +0300

    Add a test

---
+++ test/functional/fixtures/browser-provider/chrome-emulation/testcafe-fixtures/index-test.js
@@ -0,0 +1,14 @@
+import { ClientFunction } from 'testcafe';
+
+fixture `Touch events`
+    .page `example.com`;
+
+const EVENT_HANDLERS = ['ontouchstart', 'ontouchend', 'ontouchmove', 'ontouchcancel'];
+
+const hasTouchEventHandlers = ClientFunction(() => EVENT_HANDLERS.every(handler => handler in window), {
+    dependencies: { EVENT_HANDLERS }
+});
+
+test('Check presence of touch event handlers', async t => {
+    await t.expect(hasTouchEventHandlers()).ok();
+});
