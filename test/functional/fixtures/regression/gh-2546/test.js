df044e8bd0c7011f754f168755f11bb58f959ca9
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-07-11 18:30:57 +0300

    functional test for 'uncaught error'

--- test/functional/fixtures/regression/gh-2546/test.js
+++ test/functional/fixtures/regression/gh-2546/test.js
@@ -1,6 +1,10 @@
-var expect = require('chai').expect;
+const path     = require('path');
+const expect   = require('chai').expect;
+const { exec } = require('child_process');
 
 describe('[Regression](GH-2546)', function () {
+    this.timeout(60000);
+
     it('Should fail on uncaught promise rejection when ignoreUncaughtErrors is false', function () {
         return runTests('./testcafe-fixtures/index.js', 'Unhandled promise rejection', { shouldFail: true })
             .catch(function (errs) {
@@ -24,7 +28,7 @@ describe('[Regression](GH-2546)', function () {
             });
     });
 
-    it('Should not fail on uncaught promise rejection when ignoreUncaughtErrors is true', function () {
+    it('Should not fail on uncaught exception when ignoreUncaughtErrors is true', function () {
         let unhandledRejectionRaiseCount = 0;
 
         const listener = err => {
@@ -41,5 +45,38 @@ describe('[Regression](GH-2546)', function () {
 
                 expect(unhandledRejectionRaiseCount).gte(1);
             });
+    });
+
+    it('Should fail on uncaught exception when ignoreUncaughtErrors is false', function () {
+        var testcafePath = path.resolve('bin/testcafe');
+        var testFilePath = path.resolve('test/functional/fixtures/regression/gh-2546/testcafe-fixtures/uncaughtException.js');
+        var browsers     = '"chrome:headless --no-sandbox"';
+        var command  = `node ${testcafePath} ${browsers} ${testFilePath}`;
+
+        return new Promise(resolve => {
+            exec(command, (error, stdout) => {
+                resolve({ error, stdout });
+            });
+        }).then(value => {
+            expect(value.stdout).contains('Uncaught exception');
+            expect(value.stdout).contains('unhandled');
+            expect(value.error).is.not.null;
+        });
+    });
+
+    it('Should not fail on uncaught promise rejection when ignoreUncaughtErrors is true', function () {
+        var testcafePath = path.resolve('bin/testcafe');
+        var testFilePath = path.resolve('test/functional/fixtures/regression/gh-2546/testcafe-fixtures/uncaughtException.js');
+        var browsers     = '"chrome:headless --no-sandbox"';
+        var args         = '--ignore-uncaught-errors';
+        var command      = `node ${testcafePath} ${browsers} ${testFilePath} ${args}`;
+
+        return new Promise(resolve => {
+            exec(command, (error, stdout) => {
+                resolve({ error, stdout });
+            });
+        }).then(value => {
+            expect(value.error).is.null;
+        });
     });
 });
