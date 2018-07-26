4b5368fa4ad713f6d5aa3af89f9a5e50c9ca1877
Rb3As <arseniy.rubtsov@gmail.com>
2018-07-26 11:11:06 +0300

    SelectorExecutor class has been added into client driver's embedding utils

--- src/client/driver/embedding-utils.js
+++ src/client/driver/embedding-utils.js
@@ -1,6 +1,8 @@
 import { ElementSnapshot, NodeSnapshot } from './command-executors/client-functions/selector-executor/node-snapshots';
+import SelectorExecutor from './command-executors/client-functions/selector-executor';
 
 export default {
     NodeSnapshot,
-    ElementSnapshot
+    ElementSnapshot,
+    SelectorExecutor
 };
