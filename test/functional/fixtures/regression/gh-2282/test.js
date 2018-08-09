cbb2c2ac8f688da905277d6d0625b2946e3b6ddc
Andrey Belym <belym.a.2105@gmail.com>
2018-08-09 12:04:22 +0300

    Tests workaround

--- test/functional/fixtures/regression/gh-2282/test.js
+++ test/functional/fixtures/regression/gh-2282/test.js
@@ -1,4 +1,4 @@
-describe.only('[Regression](GH-2282)', function () {
+describe('[Regression](GH-2282)', function () {
     it('Cookies should be restored correctly when User Roles with the preserveUrl option are used', function () {
         return runTests('testcafe-fixtures/index.js');
     });
