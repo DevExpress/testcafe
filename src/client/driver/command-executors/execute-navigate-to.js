cafca589878a580de0a3d12008317db8be7830bf
Andrey Belym <belym.a.2105@gmail.com>
2018-08-10 15:12:01 +0300

    Tests workaround

--- src/client/driver/command-executors/execute-navigate-to.js
+++ src/client/driver/command-executors/execute-navigate-to.js
@@ -2,14 +2,47 @@ import hammerhead from '../deps/hammerhead';
 import  { RequestBarrier, pageUnloadBarrier, browser } from '../deps/testcafe-core';
 import DriverStatus from '../status';
 
-const { Promise, createNativeXHR, utils } = hammerhead;
+const { Promise, createNativeXHR, nativeMethods, utils } = hammerhead;
+
+const STORAGE_PREFIX = 'hammerhead|storage-wrapper';
+
+function findStorageKey (storage) {
+    for(let i = 0; i < storage.length; i++) {
+        const key = storage.key(i);
+
+        if (key.indexOf(STORAGE_PREFIX) === 0)
+            return key;
+    }
+
+    return '';
+}
+
+function restoreStorage (storageGetter, savedStorage) {
+    const storage = storageGetter.call(window);
+    const storageKey = findStorageKey(storage);
+
+    if (!storageKey)
+        return;
+
+    storage.setItem(storageKey, savedStorage);
+}
 
 export default function executeNavigateTo (command) {
-    debugger;
-
     const proxyUrl = utils.url.getProxyUrl(command.url);
 
-    const ensurePagePromise = command.stateSnapshot ? Promise.resolve() : browser
+    if (command.stateSnapshot) {
+        let stateSnapshot = JSON.parse(command.stateSnapshot);
+
+        if (!stateSnapshot)
+            stateSnapshot = { storages: { localStorage: '[[],[]]', sessionStorage: '[[],[]]' } };
+
+        hammerhead.sandbox.storageSandbox.lock();
+
+        restoreStorage(nativeMethods.winSessionStorageGetter, stateSnapshot.storages.sessionStorage);
+        restoreStorage(nativeMethods.winLocalStorageGetter, stateSnapshot.storages.localStorage);
+    }
+
+    const ensurePagePromise = browser
             .sendXHR(proxyUrl, createNativeXHR, { parseResponse: false, addAcceptHeader: true })
             .catch(() => (new Promise(r => setTimeout(r, 300))).then(() => browser.sendXHR(proxyUrl, createNativeXHR, { parseResponse: false, addAcceptHeader: true })))
             .catch(() => (new Promise(r => setTimeout(r, 300))).then(() => browser.sendXHR(proxyUrl, createNativeXHR, { parseResponse: false, addAcceptHeader: true })))
