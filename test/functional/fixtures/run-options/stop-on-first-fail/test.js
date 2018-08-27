23d5c2cce2c6952f35b5bfeb37cbfa0ed83ac958
miherlosev@mail.ru <Ein7Onai>
2018-08-27 15:21:17 +0300

    fix tests again

--- test/functional/fixtures/run-options/stop-on-first-fail/test.js
+++ test/functional/fixtures/run-options/stop-on-first-fail/test.js
@@ -21,28 +21,29 @@ describe('Stop test task on first failed test', () => {
             shouldFail:      true,
             stopOnFirstFail: true,
             only:            'chrome'
-        })
-            .catch(() => {
-                expect(getTestRunCount()).eql(2);
-                expect(testReport.failedCount).eql(1);
-            });
+        }).catch(() => {
+            expect(getTestRunCount()).eql(2);
+            expect(testReport.failedCount).eql(1);
+        });
     });
 
     it('Reporting', () => {
         const stream = createTestStream();
 
         return runTests('./testcafe-fixtures/stop-on-first-fail-test.js', void 0, {
+            shouldFail:      true,
             stopOnFirstFail: true,
             reporters:       [{
                 reporter:  'spec',
                 outStream: stream
             }]
-        }).then(() => {
-            const pluginHost = new ReporterPluginHost({ noColors: true });
+        }).catch(() => {
+            const pluginHost  = new ReporterPluginHost({ noColors: true });
+            const { ok, err } = pluginHost.symbols;
 
-            expect(stream.data).contains(`${pluginHost.symbols.ok} test1`);
-            expect(stream.data).contains(`${pluginHost.symbols.err} test2`);
-            expect(stream.data).to.not.contains(`${pluginHost.symbols.ok} test3`);
+            expect(stream.data).contains(`${ok} test1`);
+            expect(stream.data).contains(`${err} test2`);
+            expect(stream.data).to.not.contains(`${ok} test3`);
             expect(stream.data).contains('2/3 failed');
         });
     });
