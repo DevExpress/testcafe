8f85b19749427900a4d9eaab0561db1abca745ff
Nicolas Cisco <ncis20@gmail.com>
2018-09-10 12:27:04 -0300

    Cli: adding `meta` option and filtering tests by meta

--- src/cli/argument-parser.js
+++ src/cli/argument-parser.js
@@ -8,6 +8,7 @@ import MESSAGE from '../errors/runtime/message';
 import { assertType, is } from '../errors/runtime/type-assertions';
 import getViewPortWidth from '../utils/get-viewport-width';
 import { wordWrap, splitQuotedText } from '../utils/string';
+import { isMatch } from 'lodash';
 import parseSslOptions from './parse-ssl-options';
 
 const REMOTE_ALIAS_RE = /^remote(?::(\d*))?$/;
@@ -89,6 +90,7 @@ export default class CLIArgumentParser {
             .option('-F, --fixture-grep <pattern>', 'run only fixtures matching the specified pattern')
             .option('-a, --app <command>', 'launch the tested app using the specified command before running tests')
             .option('-c, --concurrency <number>', 'run tests concurrently')
+            .option('-m, --meta <json>', 'run only tests matching the specified meta', JSON.parse)
             .option('--debug-on-fail', 'pause the test if it fails')
             .option('--app-init-delay <ms>', 'specify how much time it takes for the tested app to initialize')
             .option('--selector-timeout <ms>', 'set the amount of time within which selectors make attempts to obtain a node to be returned')
@@ -125,7 +127,7 @@ export default class CLIArgumentParser {
         this.opts.testGrep    = CLIArgumentParser._optionValueToRegExp('--test-grep', this.opts.testGrep);
         this.opts.fixtureGrep = CLIArgumentParser._optionValueToRegExp('--fixture-grep', this.opts.fixtureGrep);
 
-        this.filter = (testName, fixtureName) => {
+        this.filter = (testName, fixtureName, _, meta) => {
 
             if (this.opts.test && testName !== this.opts.test)
                 return false;
@@ -137,6 +139,9 @@ export default class CLIArgumentParser {
                 return false;
 
             if (this.opts.fixtureGrep && !this.opts.fixtureGrep.test(fixtureName))
+                return false;
+
+            if (this.opts.meta && !isMatch(meta, this.opts.meta))
                 return false;
 
             return true;
