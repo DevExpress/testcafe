588c0d2803d6663e9945b8f061783e9c0e0f621a
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-10-09 12:54:04 +0300

    changes

--- test/functional/fixtures/regression/gh-2861/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-2861/testcafe-fixtures/index.js
@@ -8,10 +8,11 @@ const btn2   = Selector('#btn2');
 const result = Selector('#result');
 
 test('Should not hang on custom element click', async (t) => {
-    await t.click(btn1);
-    await t.expect(result.innerText).eql('');
-    await t.click(btn1, { offsetX: 0, offsetY: 0 });
-    await t.expect(result.innerText).eql('clicked');
-    await t.click(btn2);
-    await t.expect(result.innerText).eql('clickedclicked');
+    await t
+        .click(btn1)
+        .expect(result.innerText).eql('')
+        .click(btn1, { offsetX: 1, offsetY: 1 })
+        .expect(result.innerText).eql('clicked')
+        .click(btn2)
+        .expect(result.innerText).eql('clickedclicked');
 });
