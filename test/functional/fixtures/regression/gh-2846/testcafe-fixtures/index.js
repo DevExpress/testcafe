78482e787c01995736483b2705772fe3e7d039c3
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-10-22 14:34:21 +0300

    fix

--- test/functional/fixtures/regression/gh-2846/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-2846/testcafe-fixtures/index.js
@@ -1,5 +1,5 @@
-fixture `GH-1994 - The element that matches the specified selector is not visible`
-    .page `http://localhost:3000/fixtures/regression/gh-2546/pages/index.html`;
+fixture `GH-2846`
+    .page `http://localhost:3000/fixtures/regression/gh-2846/pages/index.html`;
 
 test(`Debug`, async t => {
     await t.debug();
