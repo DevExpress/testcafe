f49d336b35b0cc8c17e387daa218b4befa245e71
Andrey Belym <belym.a.2105@gmail.com>
2018-10-24 16:10:34 +0300

    Extract Marionette commands to a separate file

---
+++ src/browser/provider/built-in/firefox/marionette-client/commands.js
@@ -0,0 +1,8 @@
+export default {
+    newSession:     'WebDriver:NewSession',
+    executeScript:  'WebDriver:ExecuteScript',
+    takeScreenshot: 'WebDriver:TakeScreenshot',
+    getWindowRect:  'WebDriver:GetWindowRect',
+    setWindowRect:  'WebDriver:SetWindowRect',
+    quit:           'Marionette:Quit'
+};
