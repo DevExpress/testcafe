2d6811b853b8dca29f28c95773331cfc80a91aee
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-10-24 12:43:07 +0300

    fix test

--- test/functional/fixtures/regression/gh-2074/test.js
+++ test/functional/fixtures/regression/gh-2074/test.js
@@ -4,9 +4,13 @@ describe('[Regression](GH-2074)', function () {
     it('Should execute test located in external module', function () {
         return runTests('testcafe-fixtures/index.js', null, { shouldFail: true, disableTestSyntaxValidation: true })
             .catch(errors => {
-                Object.values(errors).forEach(err => {
-                    expect(err[0]).contains('test is executed');
-                });
+                if (Array.isArray(errors))
+                    expect(errors[0]).contains('test is executed');
+                else {
+                    Object.values(errors).forEach(err => {
+                        expect(err[0]).contains('test is executed');
+                    });
+                }
             });
     });
 });
