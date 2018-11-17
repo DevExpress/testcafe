357e48c2fa8dd5fc7f792377fac786f952924d84
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-11-17 18:49:19 +0300

    test

--- test/functional/fixtures/regression/gh-3070/test.js
+++
@@ -1,7 +0,0 @@
-describe('[Regression](GH-3070) - Should not hang after \'wait\' command', function () {
-    it('Wait for 15 seconds', function () {
-        return runTests('testcafe-fixtures/index.js');
-    });
-});
-
-
--- test/functional/fixtures/regression/gh-3070/testcafe-fixtures/index.js
+++
@@ -1,7 +0,0 @@
-fixture `GH-3070 - Should not hang after 'wait' command`
-    .page `http://localhost:3000/fixtures/regression/gh-3070/pages/index.html`;
-
-test(`Wait for 15 seconds`, async t => {
-    await t.wait(15000);
-    await t.click('h1');
-});
