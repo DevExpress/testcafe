4e91444df1042e1e21b80cf3aefc6800a1da583d
alexey.kamaev <alexey.kamaev@devexpress.com>
2018-12-04 13:24:03 +0300

    use time-limit-promise

--- test/functional/fixtures/regression/gh-3021/test.js
+++ test/functional/fixtures/regression/gh-3021/test.js
@@ -1,25 +1,10 @@
-const Promise = require('pinkie');
+const getTimeLimitedPromise = require('time-limit-promise');
 
 describe('[Regression](GH-3021) - Should not wait for selector timeout', function () {
     it('Should not wait for selector timeout', function () {
-        let isTimeoutExceeded = false;
-        let timeout           = null;
-
         const runTestsPromise = runTests('testcafe-fixtures/index.js', null, { selectorTimeout: 20000 });
 
-        const timeoutPromise = new Promise(resolve => {
-            timeout = setTimeout(() => {
-                isTimeoutExceeded = true;
-
-                resolve();
-            }, 10000);
-        });
-
-        return Promise.race([runTestsPromise, timeoutPromise]).then(() => {
-            if (isTimeoutExceeded)
-                throw new Error('We should not wait for selector timeout in this test');
-            else
-                clearTimeout(timeout);
-        });
+        return getTimeLimitedPromise(runTestsPromise, 10000, { rejectWith: new Error('We should not wait for selector timeout in this test') });
     });
 });
+
