3da6fb0abbf51f94789f0d71db9567cb7b111a7f
Artem Lavrov <artjomlav@gmail.com>
2019-01-29 10:19:52 +0300

    Fixed the error in an iframe after the "document.open" call in IE and Edge (close #3343) (#3346)

---
+++ test/functional/fixtures/regression/gh-3343/test.js
@@ -0,0 +1,3 @@
+describe('[Regression](GH-3343) - Should not raise an error when an iframe is rewritten', function () {
+    it('Rewrite an iframe', () => runTests('testcafe-fixtures/index.js'));
+});
