d43984ed952c1110f2df8a4477f3a770f05e2d88
miherlosev@mail.ru <Ein7Onai>
2019-02-22 16:46:31 +0300

    updated the test name

--- test/functional/fixtures/video-recording/test.js
+++ test/functional/fixtures/video-recording/test.js
@@ -100,7 +100,7 @@ if (config.useLocalBrowsers) {
                 });
         });
 
-        it('Should display the warning if no suitable placeholder for the "pathPattern" option was specified', () => {
+        it('Should display the warning if there is the not suitable placeholder for the "pathPattern" option was specified', () => {
             return runTests('./testcafe-fixtures/index-test.js', '', {
                 only:         'chrome',
                 shouldFail:   true,
