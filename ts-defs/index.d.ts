eafffd31da4e8f142798026e661893d2c41d234c
Helen Dikareva <elena.dikareva@devexpress.com>
2019-02-25 16:33:15 +0300

    StatusPanel's buttons renaming

--- ts-defs/index.d.ts
+++ ts-defs/index.d.ts
@@ -1693,7 +1693,7 @@ interface RunOptions {
      */
     quarantineMode: boolean;
     /**
-     * Specifies if tests run in the debug mode. If this option is enabled, test execution is paused before the first action or assertion allowing you to invoke the developer tools and debug. In the debug mode, you can execute the test step-by-step to reproduce its incorrect behavior. You can also use the Unlock page switch in the footer to unlock the tested page and interact with its elements.
+     * Specifies if tests run in the debug mode. If this option is enabled, test execution is paused before the first action or assertion allowing you to invoke the developer tools and debug. In the debug mode, you can execute the test step-by-step to reproduce its incorrect behavior. You can also use the Unlock Page switch in the footer to unlock the tested page and interact with its elements.
      */
     debugMode: boolean;
     /**
