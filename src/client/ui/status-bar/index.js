5c480648594ba443b981caa384e1ba219d21f356
Andrey Belym <belym.a.2105@gmail.com>
2019-04-02 17:20:16 +0300

    Fix status text position

--- src/client/ui/status-bar/index.js
+++ src/client/ui/status-bar/index.js
@@ -183,7 +183,7 @@ export default class StatusBar extends serviceUtils.EventEmitter {
         shadowUI.addClass(this.statusDiv, STATUS_DIV_CLASS);
 
         if (browserUtils.isMacPlatform)
-            this.statusDiv.style.marginTop = '9px';
+            this.statusDiv.style.marginTop = '11px';
 
         statusContainer.appendChild(this.statusDiv);
 
