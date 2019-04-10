f96914e50255c5a85a701aaacf08913ffc22d237
Alex Kamaev <alexikam@gmail.com>
2019-04-10 12:29:51 +0300

    try fix test

--- test/functional/fixtures/regression/gh-3456/test.js
+++ test/functional/fixtures/regression/gh-3456/test.js
@@ -1,3 +1,4 @@
+const createTestCafe = require('../../../../../lib');
 const config          = require('../../../config');
 const path            = require('path');
 const { expect }      = require('chai');
@@ -6,21 +7,27 @@ const assertionHelper = require('../../../assertion-helper.js');
 const SCREENSHOTS_PATH = path.resolve(assertionHelper.SCREENSHOTS_PATH);
 
 if (config.useLocalBrowsers) {
-    describe('--window-size arg in Headless mode (GH-3456)', function () {
+    describe('[Regression](GH-3456) Should process --window-size arg in Headless mode ', function () {
         it(':headless', () => {
             const browsers = [
                 'chrome:headless --window-size=501,602',
                 'chrome --headless --window-size=501,602'
             ];
 
-            const runner      = testCafe.createRunner();
-            const fixturePath = path.join(__dirname, '/testcafe-fixtures/index.js');
+            return createTestCafe('127.0.0.1', 1335, 1336)
+                .then(tc => {
+                    testCafe = tc;
+                })
+                .then(() => {
+                    const runner      = testCafe.createRunner();
+                    const fixturePath = path.join(__dirname, '/testcafe-fixtures/index.js');
 
-            return runner
-                .src(fixturePath)
-                .browsers(browsers)
-                .screenshots(SCREENSHOTS_PATH)
-                .run()
+                    return runner
+                        .src(fixturePath)
+                        .browsers(browsers)
+                        .screenshots(SCREENSHOTS_PATH)
+                        .run();
+                })
                 .then(failedCount => {
                     expect(failedCount).eql(0);
 
@@ -30,6 +37,9 @@ if (config.useLocalBrowsers) {
                     expect(dimensionsCorrect).eql(true);
 
                     return assertionHelper.removeScreenshotDir();
+                })
+                .then(() => {
+                    testCafe.close();
                 });
         });
     });
