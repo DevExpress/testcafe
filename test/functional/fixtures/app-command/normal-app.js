7380c8d1ce94901a5be9794155efb3d93951712b
Andrey Belym <belym.a.2105@gmail.com>
2019-05-06 13:51:26 +0300

    Fix lint

--- test/functional/fixtures/app-command/normal-app.js
+++ test/functional/fixtures/app-command/normal-app.js
@@ -6,5 +6,5 @@ const path = require('path');
 const pageSource = fs.readFileSync(path.join(__dirname, 'static/index.html'));
 
 http
-   .createServer((req, res) => res.end(pageSource))
-   .listen(3026);
+    .createServer((req, res) => res.end(pageSource))
+    .listen(3026);
