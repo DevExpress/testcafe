0d352e99d490a7d42e384031114fa274a4771fb6
Andrey Belym <belym.a.2105@gmail.com>
2019-05-22 16:17:54 +0300

    Fix initial promo messages display rate

--- test/server/marketing-test.js
+++ test/server/marketing-test.js
@@ -1,50 +1,93 @@
-const { expect }     = require('chai');
-const marketingTest  = require('../../lib/marketing');
-const consoleWrapper = require('./helpers/console-wrapper');
-const Promise        = require('pinkie');
-const { EOL }        = require('os');
+const { EOL }           = require('os');
+const { expect }        = require('chai');
+const proxyquire        = require('proxyquire');
+const consoleWrapper    = require('./helpers/console-wrapper');
 
-it('Display an appropriate promo message', () => {
-    const createShowMessagePromise = (runNumber) => {
-        runNumber = runNumber || marketingTest.NUMBER_RUNS_BETWEEN_SHOW_MESSAGE;
 
-        let chain = Promise.resolve();
+describe('Promo messages', () => {
+    before(() => {
+        consoleWrapper.init();
+        consoleWrapper.wrap();
+    });
 
-        for (let i = 0; i < runNumber; i++) {
-            chain = chain.then(() => {
-                return marketingTest.showMessageWithLinkToTestCafeStudio();
-            });
+    beforeEach(() => {
+        consoleWrapper.messages.clear();
+    });
+
+    after(() => {
+        consoleWrapper.unwrap();
+    });
+
+    it('Should display an appropriate promo message', async () => {
+        const marketingTest = require('../../lib/marketing');
+
+        const runsCount = 50;
+
+        const runNumberHistory = [];
+        let messageLog         = '';
+
+        await marketingTest._dataFile._remove();
+
+        for (let i = 1; i <= runsCount; i++) {
+            await marketingTest.showMessageWithLinkToTestCafeStudio();
+
+            if (consoleWrapper.messages.log) {
+                messageLog += consoleWrapper.messages.log;
+
+                runNumberHistory.push(i);
+                consoleWrapper.messages.clear();
+            }
         }
 
-        return chain;
-    };
+        expect(runNumberHistory).deep.equal([1, 10, 20, 30, 40, 50]);
 
-    return marketingTest._dataFile._remove()
-        .then(() => {
-            consoleWrapper.init();
-            consoleWrapper.wrap();
+        expect(messageLog).contains(
+            EOL + 'You can stop writing and start recording. Check out our commercial testing platform – now with a fully integrated visual test recorder: https://devexpress.com/testcafe-studio' +
+            EOL + 'Support open source – Help us spread the word about TestCafe Studio – IDE with a fully integrated visual test recorder: https://devexpress.com/test-cafe-studio' +
+            EOL + 'Does your QA team hate writing test scripts? Check out TestCafe Studio – now with an easy-to-use visual test recorder: https://devexpress.com/testcafe/studio' +
+            EOL + 'You can stop writing and start recording. Check out our commercial testing platform – now with a fully integrated visual test recorder: https://devexpress.com/testcafe-studio'
+        );
+    });
 
-            return createShowMessagePromise(1);
-        })
-        .then(() => {
-            expect(consoleWrapper.messages.log).contains(EOL + 'You can stop writing and start recording. Check out');
+    it('Should have uniform distribution of the first displayed message', async () => {
+        const marketingTest = proxyquire('../../lib/marketing', {
+            './data-file': class {
+                async load () {
+                    return {
+                        runCount:              0,
+                        displayedMessageIndex: void 0
+                    };
+                }
 
-            return createShowMessagePromise();
-        })
-        .then(() => {
-            expect(consoleWrapper.messages.log).contains(EOL + 'Support open source – Help us spread the word');
+                async save () {
 
-            return createShowMessagePromise();
-        })
-        .then(() => {
-            expect(consoleWrapper.messages.log).contains(EOL + 'Does your QA team hate writing test scripts?');
-
-            return createShowMessagePromise();
-        })
-        .then(() => {
-            expect(consoleWrapper.messages.log).contains(EOL + 'You can stop writing and start recording. Check out');
-
-            consoleWrapper.unwrap();
-            consoleWrapper.messages.clear();
+                }
+            }
         });
+
+        const attemptsCount = 100000;
+        const accuracy      = 100;
+
+        const messageStats = {};
+
+        for (let i = 0; i < attemptsCount; i++) {
+            await marketingTest.showMessageWithLinkToTestCafeStudio();
+
+            const message = consoleWrapper.messages.log;
+
+            if (!messageStats[message])
+                messageStats[message] = 0;
+
+            messageStats[message]++;
+        }
+
+        Object.keys(messageStats).forEach(message => {
+            messageStats[message] = Math.floor(messageStats[message] / attemptsCount * accuracy);
+        });
+
+        const messageRates = Object.values(messageStats);
+
+        expect(messageRates).to.deep.equal([33, 33, 33]);
+    });
 });
+
