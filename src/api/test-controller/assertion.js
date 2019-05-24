1ecc439ab51392d5b1224ded3193c6cb86a9ad4b
aleks-pro <prokhor.aleks@gmail.com>
2019-05-24 13:16:24 +0300

    Assertion types object created

--- src/api/test-controller/assertion.js
+++ src/api/test-controller/assertion.js
@@ -1,4 +1,5 @@
 import AssertionCommand from '../../test-run/commands/assertion';
+import ASSERTION_TYPE from '../../assertions/type';
 import { AssertionWithoutMethodCallError } from '../../errors/test-run';
 
 export default class Assertion {
@@ -32,67 +33,67 @@ export default class Assertion {
     }
 
     eql (expected, message, opts) {
-        return this._enqueueAssertion('eql', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.eql, { expected, message, opts });
     }
 
     notEql (expected, message, opts) {
-        return this._enqueueAssertion('notEql', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notEql, { expected, message, opts });
     }
 
     ok (message, opts) {
-        return this._enqueueAssertion('ok', { message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.ok, { message, opts });
     }
 
     notOk (message, opts) {
-        return this._enqueueAssertion('notOk', { message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notOk, { message, opts });
     }
 
     contains (expected, message, opts) {
-        return this._enqueueAssertion('contains', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.contains, { expected, message, opts });
     }
 
     notContains (expected, message, opts) {
-        return this._enqueueAssertion('notContains', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notContains, { expected, message, opts });
     }
 
     typeOf (expected, message, opts) {
-        return this._enqueueAssertion('typeOf', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.typeOf, { expected, message, opts });
     }
 
     notTypeOf (expected, message, opts) {
-        return this._enqueueAssertion('notTypeOf', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notTypeOf, { expected, message, opts });
     }
 
     gt (expected, message, opts) {
-        return this._enqueueAssertion('gt', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.gt, { expected, message, opts });
     }
 
     gte (expected, message, opts) {
-        return this._enqueueAssertion('gte', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.gte, { expected, message, opts });
     }
 
     lt (expected, message, opts) {
-        return this._enqueueAssertion('lt', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.lt, { expected, message, opts });
     }
 
     lte (expected, message, opts) {
-        return this._enqueueAssertion('lte', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.lte, { expected, message, opts });
     }
 
     within (start, finish, message, opts) {
         // NOTE: `within` is not available in Chai `assert` interface.
-        return this._enqueueAssertion('within', { expected: start, expected2: finish, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.within, { expected: start, expected2: finish, message, opts });
     }
 
     notWithin (start, finish, message, opts) {
-        return this._enqueueAssertion('notWithin', { expected: start, expected2: finish, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notWithin, { expected: start, expected2: finish, message, opts });
     }
 
     match (expected, message, opts) {
-        return this._enqueueAssertion('match', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.match, { expected, message, opts });
     }
 
     notMatch (expected, message, opts) {
-        return this._enqueueAssertion('notMatch', { expected, message, opts });
+        return this._enqueueAssertion(ASSERTION_TYPE.notMatch, { expected, message, opts });
     }
 }
