5ff5b5576ba189ebf36ed28d50dce882dc8636d7
Andrey Belym <belym.a.2105@gmail.com>
2019-08-01 12:10:24 +0300

    Disable the GH-1999 functional test

--- test/functional/fixtures/regression/gh-1999/test.js
+++ test/functional/fixtures/regression/gh-1999/test.js
@@ -1,4 +1,5 @@
-describe('[Regression](GH-1999)', function () {
+// TODO: Need to fix https://github.com/DevExpress/testcafe/issues/4094
+describe.skip('[Regression](GH-1999)', function () {
     it("Shouldn't raise an error if an iframe has html in src", function () {
         return runTests('testcafe-fixtures/index.js', null, { only: 'chrome' });
     });
