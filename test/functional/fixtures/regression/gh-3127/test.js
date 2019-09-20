db08ae0fe1a2bebd108fe80fc5b5c3d532c1979a
Alex Kamaev <alexikam@gmail.com>
2019-09-20 15:34:59 +0300

    pdf in chrome

--- test/functional/fixtures/regression/gh-3127/test.js
+++ test/functional/fixtures/regression/gh-3127/test.js
@@ -8,6 +8,6 @@ describe('[Regression](GH-3127, GH-2741) Should download files', function () {
     });
 
     it('Download PDF', function () {
-        return runTests('testcafe-fixtures/index-test.js', 'PDF', { only: 'firefox' });
+        return runTests('testcafe-fixtures/index-test.js', 'PDF', { only: ['firefox', 'chrome'] });
     });
 });
