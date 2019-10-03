0007f9ea6e2488093966f2f0b9801454d991b66e
Farfurix <vladimir.airich@gmail.com>
2019-10-03 15:09:02 +0300

    update test

--- test/functional/fixtures/api/es-next/browser-info/testcafe-fixtures/browser-info-test.js
+++ test/functional/fixtures/api/es-next/browser-info/testcafe-fixtures/browser-info-test.js
@@ -1,6 +1,17 @@
 fixture `getBrowserConsoleMessages`;
 
 
+function isNotValidAlias (alias) {
+    return alias.indexOf('chromium') === -1 &&
+           alias.indexOf('chrome') === -1 &&
+           alias.indexOf('chrome-canary') === -1 &&
+           alias.indexOf('ie') === -1 &&
+           alias.indexOf('edge') === -1 &&
+           alias.indexOf('firefox') === -1 &&
+           alias.indexOf('opera') === -1 &&
+           alias.indexOf('safari') === -1;
+}
+
 test
     .page `http://localhost:3000/fixtures/api/es-next/browser-info/pages/index.html`
 ('t.getBrowserInfo', async t => {
@@ -9,6 +20,8 @@ test
     // [WIP] remove it
     console.log(browserInfo); // eslint-disable-line no-console
 
-    if (browserInfo.alias !== 'chrome' && browserInfo.alias !== 'firefox')
+    if (isNotValidAlias(browserInfo.alias))
         await t.expect(false).ok();
+    else
+        await t.expect(true).ok();
 });
