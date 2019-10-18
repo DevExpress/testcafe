7e97a596d1277ad510d836160ac077afccc32c08
Mikhail Losev <mlosev86@gmail.com>
2019-10-18 12:58:53 +0300

    Rewrite browser provider with TypeScript (#4394)

---
+++ @types/testcafe-browser-tools/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'testcafe-browser-tools' {
+    export function close(windowDescriptor: string | any): Promise<void>;
+    export function findWindow(pageTitle: string): any;
+    export function generateThumbnail(sourcePath: string, thumbnailPath: string, width: number, height: number): Promise<void>;
+    export function isMaximized(windowDescriptor?: string | any): Promise<boolean>;
+    export function resize(windowDescriptor: string | any, currentWidth: number, currentHeight: number, width: number, height: number): Promise<void>;
+    export function maximize(windowDescriptor: string | any): Promise<void>;
+    export function screenshot(windowDescriptor: string | any, screenshotPath: string): Promise<void>;
+
+}
