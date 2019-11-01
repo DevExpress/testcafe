e8b9bb07e79a507246630bab3c4af7138a18e953
Alex Kamaev <alexikam@gmail.com>
2019-11-01 11:53:19 +0300

    skip browsers

--- test/functional/fixtures/regression/gh-4360/test.js
+++ test/functional/fixtures/regression/gh-4360/test.js
@@ -1,6 +1,6 @@
 describe('[Regression](GH-4360) - Should not throw \'contextStorage is undefined\' error', function () {
     it('Submit form in iframe immediately after load', function () {
-        return runTests('testcafe-fixtures/index.js');
+        return runTests('testcafe-fixtures/index.js', null, { skip: ['chrome-osx', 'firefox-osx', 'ipad', 'iphone'] });
     });
 });
 
