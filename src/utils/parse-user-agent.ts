36b144a97992f45bf445dc03aabccd4ba9d83958
Vladimir Airikh <vladimir.airich@gmail.com>
2019-11-14 15:12:17 +0300

    Requrested changes, fix headless "skip" value

--- src/utils/parse-user-agent.ts
+++ src/utils/parse-user-agent.ts
@@ -20,51 +20,44 @@ interface ParsedUserAgent {
     userAgent: string;
 }
 
-export default function parseUserAgent (userAgent: string = ''): ParsedUserAgent {
-    let parsedUserAgent;
-
-    if (!userAgent) {
-        parsedUserAgent = EMPTY_PARSED_USER_AGENT;
-
-        parsedUserAgent.browser.name    = DEFAULT_NAME;
-        parsedUserAgent.browser.version = DEFAULT_VERSION;
-        parsedUserAgent.os.name         = DEFAULT_NAME;
-        parsedUserAgent.os.version      = DEFAULT_VERSION;
-        parsedUserAgent.platform.type   = DEFAULT_PLATFORM_TYPE;
-        parsedUserAgent.engine.name     = DEFAULT_NAME;
-        parsedUserAgent.engine.version  = DEFAULT_VERSION;
-    }
-    else
-        parsedUserAgent = Bowser.parse(userAgent);
-
-    const browserName    = parsedUserAgent.browser.name || DEFAULT_NAME;
-    const browserVersion = parsedUserAgent.browser.version || DEFAULT_VERSION;
-
-    const os: ParsedComponent = { name: DEFAULT_NAME, version: DEFAULT_VERSION };
-
-    if (parsedUserAgent.os.name) {
-        os.name = parsedUserAgent.os.name;
-
-        // NOTE: a 'versionName' property value is more readable in the case of Windows (GH-481):
-        // Windows 8.1: os.version: "NT 6.3", os.versionName: "8.1".
-        if (parsedUserAgent.os.name.toLowerCase() === 'windows') {
-            if (parsedUserAgent.os.versionName)
-                os.version = parsedUserAgent.os.versionName;
-        }
-        else if (parsedUserAgent.os.version)
-            os.version = parsedUserAgent.os.version;
-    }
-
-    const engine: ParsedComponent = {
-        name:    parsedUserAgent.engine.name || DEFAULT_NAME,
-        version: parsedUserAgent.engine.version || DEFAULT_VERSION
+function calculateSimpleComponent (bowserDetails: Bowser.Parser.Details): ParsedComponent {
+    return {
+        name:    bowserDetails.name || DEFAULT_NAME,
+        version: bowserDetails.version || DEFAULT_VERSION
     };
+}
 
-    const prettyUserAgent = `${browserName} ${browserVersion} / ${os.name} ${os.version}`;
+function calculateOsComponent (parsedOsDetails: Bowser.Parser.OSDetails): ParsedComponent {
+    const name = parsedOsDetails.name || DEFAULT_NAME;
+
+    let version = DEFAULT_VERSION;
+
+    // NOTE: a 'versionName' property value is more readable in the case of Windows (GH-481):
+    // Windows 8.1: os.version: "NT 6.3", os.versionName: "8.1".
+    if (name.toLowerCase() === 'windows') {
+        if (parsedOsDetails.versionName)
+            version = parsedOsDetails.versionName;
+    }
+    else if (parsedOsDetails.version)
+        version = parsedOsDetails.version;
+
+    return { name, version };
+}
+
+function composePrettyUserAgent (browser: ParsedComponent, os: ParsedComponent): string {
+    return `${browser.name} ${browser.version} / ${os.name} ${os.version}`;
+}
+
+export default function parseUserAgent (userAgent: string = ''): ParsedUserAgent {
+    const parsedUserAgent = userAgent ? Bowser.parse(userAgent) : EMPTY_PARSED_USER_AGENT;
+    const browser         = calculateSimpleComponent(parsedUserAgent.browser);
+    const os              = calculateOsComponent(parsedUserAgent.os);
+    const engine          = calculateSimpleComponent(parsedUserAgent.engine);
+    const prettyUserAgent = composePrettyUserAgent(browser, os);
 
     return {
-        name:            browserName,
-        version:         browserVersion,
+        name:            browser.name,
+        version:         browser.version,
         platform:        parsedUserAgent.platform.type || DEFAULT_PLATFORM_TYPE,
         os,
         engine,
