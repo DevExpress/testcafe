aacdf645adad1a63f4a2b27f04bf130b77b18946
CORP\ostashev.dmitry <dmitry.ostashev@devexpress.com>
2019-11-20 18:09:23 +0300

    try make test stable

--- test/functional/fixtures/regression/gh-4360/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-4360/testcafe-fixtures/index.js
@@ -4,6 +4,8 @@ fixture `GH-4360 - Should not throw 'contextStorage is undefined' error`
     .page `http://localhost:3000/fixtures/regression/gh-4360/pages/index.html`;
 
 test(`Submit form in iframe immediately after load`, async t => {
-    await t.switchToIframe('iframe');
+    await t
+        .wait(500)
+        .switchToIframe('iframe');
     await t.expect(Selector('#target').innerText).eql('OK');
 });
