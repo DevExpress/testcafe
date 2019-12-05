a7c2f89d4c3389a561dab5e221475b8685490c9e
Andrey Belym <belym.a.2105@gmail.com>
2019-12-05 16:31:59 +0300

    Fix tabs

--- test/server/ipc-packet-test.js
+++ test/server/ipc-packet-test.js
@@ -59,7 +59,7 @@ describe('IPC Packet', () => {
         smallPacket.MAX_PAYLOAD_SIZE = 1;
         smallPacket.MAX_PACKET_SIZE  = smallPacket.HEADER_SIZE + smallPacket.MAX_PAYLOAD_SIZE;
 
-	        expect(() => smallPacket.serialize(Buffer.from('XX'))).throw('The specified payload is too large to form an IPC packet.');
+        expect(() => smallPacket.serialize(Buffer.from('XX'))).throw('The specified payload is too large to form an IPC packet.');
 
         expect(() => smallPacket.parse(bigPacket.serialize(Buffer.from('XX')))).throw('The specified payload is too large to form an IPC packet.');
     });
