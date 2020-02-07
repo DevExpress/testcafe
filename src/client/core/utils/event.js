ad404080ecd4d504c408008525225344ee36191e
Vladimir Airikh <vladimir.airich@gmail.com>
2020-02-07 16:02:33 +0300

    Fix EventTarget native methods calling (#4614)

--- src/client/core/utils/event.js
+++ src/client/core/utils/event.js
@@ -6,6 +6,7 @@ import * as domUtils from './dom';
 const Promise       = hammerhead.Promise;
 const nativeMethods = hammerhead.nativeMethods;
 const listeners     = hammerhead.eventSandbox.listeners;
+const browserUtils  = hammerhead.utils.browser;
 
 // Imported form the hammerhead
 export const BUTTON            = hammerhead.utils.event.BUTTON;
@@ -16,14 +17,14 @@ export const WHICH_PARAMETER   = hammerhead.utils.event.WHICH_PARAMETER;
 export const preventDefault = hammerhead.utils.event.preventDefault;
 
 export function bind (el, event, handler, useCapture) {
-    if (domUtils.isWindow(el))
+    if (browserUtils.isIE11 && domUtils.isWindow(el))
         nativeMethods.windowAddEventListener.call(el, event, handler, useCapture);
     else
         nativeMethods.addEventListener.call(el, event, handler, useCapture);
 }
 
 export function unbind (el, event, handler, useCapture) {
-    if (domUtils.isWindow(el))
+    if (browserUtils.isIE11 && domUtils.isWindow(el))
         nativeMethods.windowRemoveEventListener.call(el, event, handler, useCapture);
     else
         nativeMethods.removeEventListener.call(el, event, handler, useCapture);
