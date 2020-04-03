19da8b40ead012c9ffbbbfe077d67dd119378ec4
AlexKamaev <alexikam@gmail.com>
2020-04-03 16:39:29 +0300

    modify diff calculation

--- src/utils/diff.ts
+++ src/utils/diff.ts
@@ -1,8 +1,5 @@
-import { set, isEmpty, isEqual, isNil as isNullOrUndefined } from 'lodash';
+import { set, isPlainObject } from 'lodash';
 import { Dictionary } from '../configuration/interfaces';
-
-const OBJECTS_EMPTY_ERROR                = 'Objects should be not empty';
-const OBJECTS_DIFFERENT_PROPERTIES_ERROR = 'Objects should contain the same properties';
 
 function getFullPropertyPath (property: string, parentProperty: string): string {
     if (parentProperty)
@@ -11,25 +8,18 @@ function getFullPropertyPath (property: string, parentProperty: string): string 
     return property;
 }
 
-function validate (source: Dictionary<object>, modified: Dictionary<object>): void {
-    if (isEmpty(source) || isEmpty(modified))
-        throw new TypeError(OBJECTS_EMPTY_ERROR);
-
-    if (!isEqual(Object.keys(source), Object.keys(modified)))
-        throw new TypeError(OBJECTS_DIFFERENT_PROPERTIES_ERROR);
-}
-
 function diff (source: Dictionary<object>, modified: Dictionary<object>, result: Dictionary<object>, parentProperty: string = ''): void {
-    validate(source, modified);
-
     for (const property in source) {
         const fullPropertyPath = getFullPropertyPath(property, parentProperty);
+
+        if (!modified.hasOwnProperty(property))
+            continue;
 
         const sourceValue   = source[property] as Dictionary<object>;
         const modifiedValue = modified[property] as Dictionary<object>;
 
-        if (!isNullOrUndefined(modifiedValue) && sourceValue !== modifiedValue) {
-            if (typeof sourceValue === 'object' && typeof modifiedValue === 'object')
+        if (sourceValue !== modifiedValue) {
+            if (isPlainObject(sourceValue) && isPlainObject(modifiedValue))
                 diff(sourceValue, modifiedValue, result, fullPropertyPath);
             else
                 set(result, fullPropertyPath, modifiedValue);
@@ -40,15 +30,9 @@ function diff (source: Dictionary<object>, modified: Dictionary<object>, result:
 export default (source: Dictionary<object>, modified: Dictionary<object>) => {
     const result = {};
 
-    try {
+    if (isPlainObject(source) && isPlainObject(modified))
         diff(source, modified, result);
-    }
-    catch {
-        return null;
-    }
-
-    if (isEmpty(result))
-        return null;
 
     return result;
+
 };
--- test/server/util-test.js
+++ test/server/util-test.js
@@ -42,19 +42,19 @@ describe('Utils', () => {
     });
 
     it('Diff', () => {
-        expect(diff(null, null)).is.null;
-        expect(diff(void 0, void 0)).is.null;
-        expect(diff({ a: void 0 }, { b: void 0 })).is.null;
-        expect(diff({ a: null }, { b: null })).is.null;
-        expect(diff({ a: 1 }, { a: 1 })).is.null;
-        expect(diff({ a: 1 }, { a: 1 })).is.null;
-        expect(diff({ a: 1 }, { a: void 0 })).is.null;
-        expect(diff({ a: 1 }, { a: null })).is.null;
-        expect(diff({ a: 1, b: 1 }, { a: 1, b: 1 })).is.null;
-        expect(diff({ a: 1, b: {} }, { a: 1, b: {} })).is.null;
-        expect(diff({ a: 1, b: { c: 3 } }, { a: 1, b: { c: 3 } })).is.null;
-        expect(diff({ a: 1, b: { c: { d: 4 } } }, { a: 1, b: { c: { d: 4 } } })).is.null;
-
+        expect(diff(null, null)).eql({});
+        expect(diff(void 0, void 0)).eql({});
+        expect(diff(1, 2)).eql({});
+        expect(diff({ a: void 0 }, { b: void 0 })).eql({});
+        expect(diff({ a: null }, { b: null })).eql({});
+        expect(diff({ a: null }, { a: 1 })).eql({ a: 1 });
+        expect(diff({ a: 1 }, { a: 1 })).eql({});
+        expect(diff({ a: 1 }, { a: void 0 })).eql({ a: void 0 });
+        expect(diff({ a: 1 }, { a: null })).eql({ a: null });
+        expect(diff({ a: 1, b: 1 }, { a: 1, b: 1 })).eql({});
+        expect(diff({ a: 1, b: {} }, { a: 1, b: {} })).eql({});
+        expect(diff({ a: 1, b: { c: 3 } }, { a: 1, b: { c: 3 } })).eql({});
+        expect(diff({ a: 1, b: { c: { d: 4 } } }, { a: 1, b: { c: { d: 4 } } })).eql({});
         expect(diff({ a: 0 }, { a: 1 })).eql({ a: 1 });
         expect(diff({ a: 1 }, { a: 0 })).eql({ a: 0 });
         expect(diff({ a: 1 }, { a: 2 })).eql({ a: 2 });
