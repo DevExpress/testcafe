cb39e4a7eabd05353456e59eb06e5bdc818d75e0
AlexKamaev <alexikam@gmail.com>
2020-04-20 16:52:54 +0300

    content iframe did not load test for #3904

---
+++ test/functional/fixtures/regression/gh-3904/test.js
@@ -0,0 +1,5 @@
+describe('GH-3904 - Should correctly switch to iframe loaded from form', function () {
+    it('Should correctly switch to iframe loaded from form', function () {
+        return runTests('testcafe-fixtures/index.js');
+    });
+});
