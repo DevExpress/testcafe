bd1c49cca4d84ec7dc6488ad8ae921f4a550b355
Andrey Belym <belym.a.2105@gmail.com>
2020-05-14 15:52:01 +0300

    Allow to use ClientScript.module in TS (closes #5071)

--- ts-defs-src/test-api/client-script.d.ts
+++ ts-defs-src/test-api/client-script.d.ts
@@ -1,6 +1,18 @@
 
-interface ClientScript {
-    content?: string;
-    path?: string;
+interface ClientScriptCommon {
     page?: any;
 }
+
+interface ClientScriptContent extends ClientScriptCommon {
+    content?: string;
+}
+
+interface ClientScriptModule extends ClientScriptCommon {
+    module?: string;
+}
+
+interface ClientScriptPath extends ClientScriptCommon {
+    path?: string;
+}
+
+type ClientScript = ClientScriptContent | ClientScriptModule | ClientScriptPath;
