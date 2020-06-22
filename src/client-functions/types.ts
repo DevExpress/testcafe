cdf09f21b18015eee9b6d682f2f2aedd2d9153e3
Dmitrii <1@1.1>
2020-06-22 10:21:39 +0300

    Refactored type checkers

--- src/client-functions/types.ts
+++ src/client-functions/types.ts
@@ -3,15 +3,10 @@ import ClientFunctionBuilder from './client-function-builder';
 import SelectorBuilder from './selectors/selector-builder';
 
 export function isClientFunction (obj: any): boolean {
-    if (typeof obj === 'function')
-        return obj[builderSymbol] && obj[builderSymbol] instanceof ClientFunctionBuilder && !(obj[builderSymbol] instanceof SelectorBuilder);
-
-    return false;
+    return obj && obj[builderSymbol] && obj[builderSymbol] instanceof ClientFunctionBuilder &&
+        !(obj[builderSymbol] instanceof SelectorBuilder);
 }
 
 export function isSelector (obj: any): boolean {
-    if (typeof obj === 'function')
-        return obj[builderSymbol] && obj[builderSymbol] instanceof SelectorBuilder;
-
-    return false;
+    return obj && obj[builderSymbol] && obj[builderSymbol] instanceof SelectorBuilder;
 }
