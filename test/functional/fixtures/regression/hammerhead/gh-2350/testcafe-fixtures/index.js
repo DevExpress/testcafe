a10c1211383e35e386d819a38b58b3fbaceab4c8
Vladimir Airikh <vladimir.airich@gmail.com>
2020-07-06 18:42:55 +0300

    Add the "document.title changing" test case (#5279)

--- test/functional/fixtures/regression/hammerhead/gh-2350/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/hammerhead/gh-2350/testcafe-fixtures/index.js
@@ -15,6 +15,13 @@ const EMPTY_VALUE_EXPECTED_LOG = [
     'body: '
 ];
 
+const CHANGE_VALUE_EXPECTED_LOG = [
+    'head:after-first-title: Test page title 1',
+    'head:after-title-update: Test page title 2',
+    'body: Test page title 2',
+    'body:after-title-update: Test page title 3'
+];
+
 test
     .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/initial-value.html')
     ('initial value', async t => {
@@ -25,4 +32,10 @@ test
     .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/empty-value.html')
     ('empty value', async t => {
         await t.expect(getLog()).eql(EMPTY_VALUE_EXPECTED_LOG);
+    });
+
+test
+    .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/change-value.html')
+    ('change value', async t => {
+        await t.expect(getLog()).eql(CHANGE_VALUE_EXPECTED_LOG);
     });
