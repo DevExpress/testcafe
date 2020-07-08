6c3ac21d0aba35023df201da3560ecf9e5417b1a
miherlosev <miherlosev@mail.ru>
2020-07-08 12:03:31 +0300

    tests for hammerhead/#2374

--- test/functional/fixtures/regression/hammerhead/gh-2350/test.js
+++ test/functional/fixtures/regression/hammerhead/gh-2350/test.js
@@ -10,4 +10,8 @@ describe("Should provide a valid value for the 'document.title' property", () =>
     it('Change value', () => {
         return runTests('./testcafe-fixtures/index.js', 'change value');
     });
+
+    it('Text property getters of the title element', () => {
+        return runTests('./testcafe-fixtures/index.js', 'text property getters of the title element');
+    });
 });
