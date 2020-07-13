643cf7d64654dcd60db13a3cf2444ec9b26cca11
Mikhail Losev <mlosev86@gmail.com>
2020-07-13 11:28:41 +0300

    Update hammerhead; Tests for hammerhead/#2374 (#5299)
    * tests for hammerhead/#2374
    
    * update xx

--- test/functional/fixtures/regression/hammerhead/gh-2350/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/hammerhead/gh-2350/testcafe-fixtures/index.js
@@ -22,6 +22,12 @@ const CHANGE_VALUE_EXPECTED_LOG = [
     'body:after-title-update: Test page title 3'
 ];
 
+const TEXT_PROPERTY_GETTERS_OF_TITLE_ELEMENT = [
+    'text: Test title',
+    'innerHTML: Test title',
+    'innerText: Test title'
+];
+
 test
     .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/initial-value.html')
     ('initial value', async t => {
@@ -38,4 +44,10 @@ test
     .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/change-value.html')
     ('change value', async t => {
         await t.expect(getLog()).eql(CHANGE_VALUE_EXPECTED_LOG);
+    });
+
+test
+    .page('http://localhost:3000/fixtures/regression/hammerhead/gh-2350/pages/text-property-getters-of-title-element.html')
+    ('text property getters of the title element', async t => {
+        await t.expect(getLog()).eql(TEXT_PROPERTY_GETTERS_OF_TITLE_ELEMENT);
     });
