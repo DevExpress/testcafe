26a71a91bf8e9763692c31ae85b09259ed87b348
AlexKamaev <alexikam@gmail.com>
2020-07-27 15:44:52 +0300

    `disableMultipleWindows` for specific test

--- test/functional/fixtures/run-options/disable-page-caching/test.js
+++ test/functional/fixtures/run-options/disable-page-caching/test.js
@@ -1,13 +1,13 @@
 describe('Disable page caching', () => {
     it('Test run', () => {
-        return runTests('testcafe-fixtures/test-run.js', null, { disablePageCaching: true });
+        return runTests('testcafe-fixtures/test-run.js', null, { disablePageCaching: true, disableMultipleWindows: true });
     });
 
     it('Fixture', () => {
-        return runTests('testcafe-fixtures/fixture.js');
+        return runTests('testcafe-fixtures/fixture.js', null, { disableMultipleWindows: true });
     });
 
     it('Single test', () => {
-        return runTests('testcafe-fixtures/single-test.js');
+        return runTests('testcafe-fixtures/single-test.js', null, { disableMultipleWindows: true });
     });
 });
