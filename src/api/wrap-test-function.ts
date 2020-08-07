14b05949e9110584b2dcf311087cf7ae0d2f2368
Dmitrii <ddm157@yandex.ru>
2020-08-07 15:05:23 +0300

    Refactored throwing errors/warnings in wrapTestFunction

--- src/api/wrap-test-function.ts
+++ src/api/wrap-test-function.ts
@@ -3,7 +3,7 @@ import testRunTracker from './test-run-tracker';
 import { TestRun } from './test-run-tracker.d';
 import TestCafeErrorList from '../errors/error-list';
 import { MissingAwaitError } from '../errors/test-run';
-import addWarning from '../notifications/add-rendered-warning';
+import addRenderedWarning from '../notifications/add-rendered-warning';
 import WARNING_MESSAGES from '../notifications/warning-message';
 
 export default function wrapTestFunction (fn: Function): Function {
@@ -11,6 +11,20 @@ export default function wrapTestFunction (fn: Function): Function {
         let result       = null;
         const errList    = new TestCafeErrorList();
         const markeredfn = testRunTracker.addTrackingMarkerToFunction(testRun.id, fn);
+
+        function addWarnings (callsiteSet: Set<Record<string, any>>, message: string): void {
+            callsiteSet.forEach(callsite => {
+                addRenderedWarning(testRun.warningLog, message, callsite);
+                callsiteSet.delete(callsite);
+            });
+        }
+
+        function addErrors (callsiteSet: Set<Record<string, any>>, ErrorClass: any): void {
+            callsiteSet.forEach(callsite => {
+                errList.addError(new ErrorClass(callsite));
+                callsiteSet.delete(callsite);
+            });
+        }
 
         testRun.controller = new TestController(testRun);
 
@@ -26,16 +40,8 @@ export default function wrapTestFunction (fn: Function): Function {
         }
 
         if (!errList.hasUncaughtErrorsInTestCode) {
-            // These look very similar, should I extract them into a separate function?
-            for (const callsite of testRun.observedCallsites.callsitesWithoutAwait) {
-                errList.addError(new MissingAwaitError(callsite));
-                testRun.observedCallsites.callsitesWithoutAwait.delete(callsite);
-            }
-
-            for (const callsite of testRun.observedCallsites.unawaitedSnapshotCallsites) {
-                addWarning(testRun.warningLog, WARNING_MESSAGES.missingAwaitOnSnapshotProperty, callsite);
-                testRun.observedCallsites.unawaitedSnapshotCallsites.delete(callsite);
-            }
+            addWarnings(testRun.observedCallsites.unawaitedSnapshotCallsites, WARNING_MESSAGES.missingAwaitOnSnapshotProperty);
+            addErrors(testRun.observedCallsites.callsitesWithoutAwait, MissingAwaitError);
         }
 
         if (errList.hasErrors)
