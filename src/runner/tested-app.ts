f163ccb1df880b912725f1f2529f58d130b3d23b
Matt Kubej <mkubej@gmail.com>
2020-08-19 03:21:45 -0400

    ability to log stdout and stderr of tested-app (closes #5423) (#5431)
    * ability to log stdout and stderr of tested-app
    
    * Print logs in real time
    
    * Fix lint
    
    * Fix tests, use execa
    
    Co-authored-by: Andrey Belym <belym.a.2105@gmail.com>

--- src/runner/tested-app.ts
+++ src/runner/tested-app.ts
@@ -1,11 +1,14 @@
-import { ChildProcess, exec } from 'child_process';
+import { ChildProcess } from 'child_process';
 import { delimiter as pathDelimiter } from 'path';
+import { command as spawnCommand } from 'execa';
+import { noop } from 'lodash';
 import kill from 'tree-kill';
 import OS from 'os-family';
 import delay from '../utils/delay';
 import { GeneralError } from '../errors/runtime';
 import { RUNTIME_ERRORS } from '../errors/types';
 import resolvePathRelativelyCwd from '../utils/resolve-path-relatively-cwd';
+import debugLogger from 'debug';
 
 const MODULES_BIN_DIR = resolvePathRelativelyCwd('./node_modules/.bin');
 
@@ -27,33 +30,53 @@ const ENV_PATH_KEY = (function () {
 
 export default class TestedApp {
     private _killed: boolean;
-    public errorPromise: null | Promise<void>;
     private _process: null | ChildProcess;
+    private _stdoutLogger: debug.Debugger;
+    private _stderrLogger: debug.Debugger;
+
+    public errorPromise: null | Promise<void>;
 
     public constructor () {
-        this._process     = null;
-        this.errorPromise = null;
-        this._killed      = false;
+        this._process          = null;
+        this._killed           = false;
+        this._stdoutLogger     = debugLogger('testcafe:tested-app:stdout');
+        this._stderrLogger     = debugLogger('testcafe:tested-app:stderr');
+
+        this.errorPromise      = null;
+    }
+
+    private async _run (command: string): Promise<void> {
+        const env       = Object.assign({}, process.env);
+        const path      = env[ENV_PATH_KEY] || '';
+        const pathParts = path.split(pathDelimiter);
+
+        pathParts.unshift(MODULES_BIN_DIR);
+
+        env[ENV_PATH_KEY] = pathParts.join(pathDelimiter);
+
+        this._process = spawnCommand(command, { shell: true, env });
+
+        this._process.stdout.on('data', data => this._stdoutLogger(String(data)));
+        this._process.stderr.on('data', data => this._stderrLogger(String(data)));
+
+        try {
+            await this._process;
+        }
+        catch (err) {
+            if (this._killed)
+                return;
+
+            const message = err.stack || String(err);
+
+            throw new GeneralError(RUNTIME_ERRORS.testedAppFailedWithError, message);
+        }
     }
 
     public async start (command: string, initDelay: number): Promise<void> {
-        this.errorPromise = new Promise((resolve, reject) => {
-            const env       = Object.assign({}, process.env);
-            const path      = env[ENV_PATH_KEY] || '';
-            const pathParts = path.split(pathDelimiter);
-
-            pathParts.unshift(MODULES_BIN_DIR);
-
-            env[ENV_PATH_KEY] = pathParts.join(pathDelimiter);
-
-            this._process = exec(command, { env }, err => {
-                if (!this._killed && err) {
-                    const message = err.stack || String(err);
-
-                    reject(new GeneralError(RUNTIME_ERRORS.testedAppFailedWithError, message));
-                }
-            });
-        });
+        // NOTE: We should not resolve it if no error was thrown
+        this.errorPromise = this
+            ._run(command)
+            .then(() => new Promise(noop));
 
         await Promise.race([
             delay(initDelay),
