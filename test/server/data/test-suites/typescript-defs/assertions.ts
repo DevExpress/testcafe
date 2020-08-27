94d0cdafd7f4b050d3999af31437f59673f5ee06
Dmitrii Peredreev <ddm157@yandex.ru>
2020-08-27 16:19:32 +0300

    Add type definition tests for typeOf and notTypeOf assertions (#5458)
    * Added type definition tests for .(not)typeOf() assertion
    
    * Fixed notTypeOf
    
    * Added functional tests, fixed a bug with regexp type (was regex)
    
    * Fixed callsites in tests
    
    * Fixed a line number

--- test/server/data/test-suites/typescript-defs/assertions.ts
+++ test/server/data/test-suites/typescript-defs/assertions.ts
@@ -3,20 +3,20 @@ import { t } from 'testcafe';
 fixture `ClientFunction`
     .page `http://localhost:3000/fixtures/api/es-next/client-function/pages/index.html`;
 
-test(".contains", async t => {
+test('.contains', async t => {
     await t
-        .expect("foobar").contains("foo")
-        .expect([42, "foo"] as (number | string)[]).contains("foo")
-        .expect([42, "foo"] as [number, string]).contains(42)
+        .expect('foobar').contains('foo')
+        .expect([42, 'foo'] as (number | string)[]).contains('foo')
+        .expect([42, 'foo'] as [number, string]).contains(42)
         .expect([42, 34] as number []).contains(34)
-        .expect({ ans: 42, foo: "bar" }).contains({ foo: "bar" })
+        .expect({ ans: 42, foo: 'bar' }).contains({ foo: 'bar' })
 });
 
-test(".notContains", async t => {
+test('.notContains', async t => {
     await t
-        .expect("foobar").notContains("foox")
-        .expect([42, "foo"] as (number | string)[]).notContains(12)
-        .expect([42, "foo"] as [number, string]).notContains("foox")
+        .expect('foobar').notContains('foox')
+        .expect([42, 'foo'] as (number | string)[]).notContains(12)
+        .expect([42, 'foo'] as [number, string]).notContains('foox')
         .expect([42, 34] as number []).notContains(13)
-        .expect({ ans: 42, foo: "bar" }).notContains({ foo: "baz" })
+        .expect({ ans: 42, foo: 'bar' }).notContains({ foo: 'baz' })
 });
