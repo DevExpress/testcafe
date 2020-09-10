9ff46910503f1e586cbbbec2293daebdac058c62
Mikhail Losev <mlosev86@gmail.com>
2020-09-10 16:01:33 +0300

    Rewrite request hooks to TypeScript (#5506)
    * initial
    
    * update xammerhead
    
    * update hammerhead again
    
    * fix test
    
    * Update src/api/request-hooks/hook.ts
    
    Co-authored-by: Andrey Belym <belym.a.2105@gmail.com>
    
    * Update src/api/request-hooks/hook.ts
    
    Co-authored-by: Andrey Belym <belym.a.2105@gmail.com>
    
    * Update src/api/request-hooks/interfaces.ts
    
    Co-authored-by: Andrey Belym <belym.a.2105@gmail.com>
    
    * Apply codestyle suggestions
    
    * Fix import
    
    * fix tests
    
    Co-authored-by: Andrey Belym <belym.a.2105@gmail.com>

---
+++ src/api/request-hooks/request-mock.ts
@@ -0,0 +1,67 @@
+import RequestHook from './hook';
+
+import {
+    ResponseMock,
+    RequestEvent,
+    ResponseEvent,
+    RequestFilterRule,
+    SAME_ORIGIN_CHECK_FAILED_STATUS_CODE
+} from 'testcafe-hammerhead';
+
+import { APIError } from '../../errors/runtime';
+import { RUNTIME_ERRORS } from '../../errors/types';
+import WARNING_MESSAGE from '../../notifications/warning-message';
+import WarningLog from '../../notifications/warning-log';
+import { RequestFilterRuleInit } from './interfaces';
+
+
+class RequestMock extends RequestHook {
+    private _pendingRequestFilterRuleInit: null | RequestFilterRuleInit;
+    private _mocks: Map<RequestFilterRule, ResponseMock>;
+
+    public constructor () {
+        super([]);
+
+        this._pendingRequestFilterRuleInit = null;
+        this._mocks                        = new Map();
+    }
+
+    public async onRequest (event: RequestEvent): Promise<void> {
+        const mock = this._mocks.get(event._requestFilterRule) as ResponseMock;
+
+        event.setMock(mock);
+    }
+
+    public async onResponse (event: ResponseEvent): Promise<void> {
+        if (event.statusCode === SAME_ORIGIN_CHECK_FAILED_STATUS_CODE)
+            (this.warningLog as WarningLog).addWarning(WARNING_MESSAGE.requestMockCORSValidationFailed, RequestMock.name, event._requestFilterRule);
+    }
+
+    // API
+    public onRequestTo (requestFilterRuleInit: RequestFilterRuleInit): RequestMock {
+        if (this._pendingRequestFilterRuleInit)
+            throw new APIError('onRequestTo', RUNTIME_ERRORS.requestHookConfigureAPIError, RequestMock.name, "The 'respond' method was not called after 'onRequestTo'. You must call the 'respond' method to provide the mocked response.");
+
+        this._pendingRequestFilterRuleInit = requestFilterRuleInit;
+
+        return this;
+    }
+
+    public respond (body: string | Function, statusCode?: number, headers?: object): RequestMock {
+        if (!this._pendingRequestFilterRuleInit)
+            throw new APIError('respond', RUNTIME_ERRORS.requestHookConfigureAPIError, RequestMock.name, "The 'onRequestTo' method was not called before 'respond'. You must call the 'onRequestTo' method to provide the URL requests to which are mocked.");
+
+        const mock = new ResponseMock(body, statusCode, headers);
+        const rule = new RequestFilterRule(this._pendingRequestFilterRuleInit);
+
+        this.requestFilterRules.push(rule);
+        this._mocks.set(rule, mock);
+        this._pendingRequestFilterRuleInit = null;
+
+        return this;
+    }
+}
+
+export default function createRequestMock (): RequestMock {
+    return new RequestMock();
+}
