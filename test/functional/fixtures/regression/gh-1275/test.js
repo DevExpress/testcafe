7a865ab95e269f42fa547cd4214a8f5a0cb1b39c
AlexKamaev <alexikam@gmail.com>
2020-09-29 18:12:15 +0300

    еуые

--- test/functional/fixtures/regression/gh-1275/test.js
+++ test/functional/fixtures/regression/gh-1275/test.js
@@ -1,7 +1,7 @@
 describe('[Regression](GH-1275)', function () {
     for (let i = 0; i < 1000; i++) {
         it.only(`test ${i}`, function () {
-            await t.wait(5000)
+            await t.wait(3600000)
             return runTests('testcafe-fixtures/index-test.js', 'Hide input on blur', { only: ['ie'] });
         });
     }
