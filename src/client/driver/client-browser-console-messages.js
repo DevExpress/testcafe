e17b0d70a0cd1527ef960b92d5650eba2f951a72
miherlosev <miherlosev@mail.ru>
2020-12-08 14:53:00 +0300

    fix lint

--- src/client/driver/client-browser-console-messages.js
+++ src/client/driver/client-browser-console-messages.js
@@ -1,8 +1,8 @@
 import BrowserConsoleMessages from '../../test-run/browser-console-messages';
 import { nativeMethods } from './deps/hammerhead';
 
-export default class ClientBrowserConsoleMessages extends BrowserConsoleMessages{
-    constructor(data) {
+export default class ClientBrowserConsoleMessages extends BrowserConsoleMessages {
+    constructor (data) {
         super(data, nativeMethods);
     }
 }
