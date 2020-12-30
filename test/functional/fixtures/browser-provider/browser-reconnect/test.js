ebdb303daf0a740a0c5f7377e8fc23d759eca7c4
AlexKamaev <alexikam@gmail.com>
2020-12-30 13:28:49 +0300

    fix env

--- test/functional/fixtures/browser-provider/browser-reconnect/test.js
+++ test/functional/fixtures/browser-provider/browser-reconnect/test.js
@@ -76,7 +76,7 @@ describe('Browser reconnect', function () {
             let errLog = '';
 
             return new Promise(resolve => {
-                const proc = spawn(`node ${path.join(__dirname, 'run-log-error-on-disconnect-test.js')}`, { shell: true, env: { DEBUG: 'hammerhead:*' } });
+                const proc = spawn(`node ${path.join(__dirname, 'run-log-error-on-disconnect-test.js')}`, { shell: true, env: { ...process.env, DEBUG: 'hammerhead:*' } });
 
                 proc.stderr.on('data', data => {
                     errLog += data.toString('utf-8');
