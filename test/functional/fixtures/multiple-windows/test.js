8949135eb9e6d4dd0c56df13e869f3fcbc47299c
AlexKamaev <alexikam@gmail.com>
2021-01-20 14:27:42 +0300

    add error message if the parent window cannot restore chid window links (closes #4760) (#5881)

--- test/functional/fixtures/multiple-windows/test.js
+++ test/functional/fixtures/multiple-windows/test.js
@@ -126,6 +126,13 @@ describe('Multiple windows', () => {
         return runTests('testcafe-fixtures/i5857.js');
     });
 
+    it('Should throw error if cannot restore child links', () => {
+        return runTests('testcafe-fixtures/i4760.js', null, { only: 'chrome', shouldFail: true })
+            .catch(errs => {
+                expect(errs[0]).to.contain('Failed to restore connection to window within the allocated timeout.');
+            });
+    });
+
     describe('API', () => {
         it('Open child window', () => {
             return runTests('testcafe-fixtures/api/api-test.js', 'Open child window', { only: 'chrome' });
