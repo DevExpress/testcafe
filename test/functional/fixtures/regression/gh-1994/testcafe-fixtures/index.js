c197edfce475388b0c4f83c1eea88dc44f1bce8e
Alex Kamaev <alexey.kamaev@devexpress.com>
2021-02-05 14:18:44 +0300

    tmp

--- test/functional/fixtures/regression/gh-1994/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-1994/testcafe-fixtures/index.js
@@ -2,8 +2,7 @@ fixture `GH-1994 - The element that matches the specified selector is not visibl
     .page `http://localhost:3000/fixtures/regression/gh-1994/pages/index.html`;
 
 test(`Recreate invisible element and click`, async t => {
-    await t
-        .click('#targetRecreate');
+    await t.click('#targetRecreate');
 });
 
 test(`Remove invisible element and click`, async t => {
