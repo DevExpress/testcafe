23b91a7218065176a961a2fa5f3ae2f4220f0123
miherlosev <miherlosev@mail.ru>
2021-03-05 11:24:53 +0300

    remove typings

--- @types/map-reverse/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'map-reverse' {
-    function mapReverse<T>(array: T[], callback: (item: T, index: number, currentArray: readonly T[]) => unknown): T[];
-
-    export = mapReverse;
-}
