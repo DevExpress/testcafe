679989b32b78dbfbd3fd7213d9fc58c0f1ddc5f2
Mikhail Losev <mlosev86@gmail.com>
2021-03-17 11:58:31 +0300

    Update src/cli/argument-parser.ts
    Co-authored-by: Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>

--- src/cli/argument-parser.ts
+++ src/cli/argument-parser.ts
@@ -150,7 +150,7 @@ export default class CLIArgumentParser {
             .option('--dev', 'enables mechanisms to log and diagnose errors')
             .option('--qr-code', 'outputs QR-code that repeats URLs used to connect the remote browsers')
             .option('--sf, --stop-on-first-fail', 'stop an entire test run if any test fails')
-            .option('--config-file <path>', 'use a different testcafe config to the default and specify its location')
+            .option('--config-file <path>', 'specify a custom path to the testcafe configuration file')
             .option('--ts-config-path <path>', 'use a custom TypeScript configuration file and specify its location')
             .option('--cs, --client-scripts <paths>', 'inject scripts into tested pages', this._parseList, [])
             .option('--disable-page-caching', 'disable page caching during test execution')
