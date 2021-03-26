7b25d477eac0bded44a14c8718c4cc319888c968
Alex Kamaev <alexey.kamaev@devexpress.com>
2021-03-26 15:45:33 +0300

    2

--- src/client/automation/playback/click/index.ts
+++ src/client/automation/playback/click/index.ts
@@ -6,70 +6,59 @@ import MouseUpAutomation from '../mouse-up';
 import { ClickOptions } from '../../../../test-run/commands/options';
 import { MouseDownStateController, MouseUpStateController } from '../automation-states';
 import { EnsureElementResult, EnsureElementResultArgs } from '../interfaces';
+import MouseBaseAutomation from '../mouse-base';
 
 const Promise = hammerhead.Promise;
 const extend  = hammerhead.utils.extend;
 const delay   = testCafeCore.delay;
 
 
-export default class ClickAutomation extends VisibleElementAutomation {
-    private modifiers: unknown;
+export default class ClickAutomation extends MouseBaseAutomation {
     private mouseDownAutomation: MouseDownAutomation | null;
     private mouseUpAutomation: MouseUpAutomation | null;
 
     public constructor (element: HTMLElement, clickOptions: ClickOptions) {
         super(element, clickOptions);
 
-        this.modifiers = clickOptions.modifiers;
-
         this.mouseDownAutomation = null;
         this.mouseUpAutomation = null;
     }
 
-    public get downState (): MouseDownStateController | null {
-        return this.mouseDownAutomation ? this.mouseDownAutomation.eventState : null;
+    // public get downState (): MouseDownStateController | null {
+    //     return this.mouseDownAutomation ? this.mouseDownAutomation.eventState : null;
+    // }
+    //
+    // public get upState (): MouseUpStateController | null {
+    //     return this.mouseUpAutomation ? this.mouseUpAutomation.upState : null;
+    // }
+
+    private _mousedown (): Promise<unknown> {
+        if (this.ensureElementResultArgs) {
+            this.mouseDownAutomation = new MouseDownAutomation(this.ensureElementResultArgs.element, this.options, this.ensureElementResultArgs);
+
+            return this.mouseDownAutomation.run(false);
+        }
+
+        return Promise.resolve();
     }
 
-    public get upState (): MouseUpStateController | null {
-        return this.mouseUpAutomation ? this.mouseUpAutomation.upState : null;
+    private _mouseup (): Promise<unknown> {
+        if (this.mouseDownAutomation && this.ensureElementResultArgs) {
+            this.mouseUpAutomation = new MouseUpAutomation(this.ensureElementResultArgs.element, this.options, this.mouseDownAutomation.eventState, this.ensureElementResultArgs);
+
+            return this.mouseUpAutomation.run(false);
+        }
+
+        return Promise.resolve();
     }
 
-    private _mousedown (eventArgs: EnsureElementResultArgs): Promise<unknown> {
-        this.mouseDownAutomation = new MouseDownAutomation(eventArgs.element, this.options, eventArgs);
-
-        return this.mouseDownAutomation.run(false);
-    }
-
-    private _mouseup (eventArgs: EnsureElementResultArgs): Promise<unknown> {
-        if (!this.mouseDownAutomation)
-            return Promise.resolve();
-
-        this.mouseUpAutomation = new MouseUpAutomation(eventArgs.element, this.options, this.mouseDownAutomation.eventState, eventArgs);
-
-        return this.mouseUpAutomation.run(false);
-    }
-
-    private run (useStrictElementCheck: boolean): Promise<unknown> {
-        return this
-            ._ensureElement(useStrictElementCheck, false, false)
-            .then(({ element, clientPoint, screenPoint, devicePoint }: EnsureElementResult) => {
-                const eventArgs: EnsureElementResultArgs = {
-                    point:       clientPoint,
-                    screenPoint: screenPoint,
-                    element:     element,
-                    options:     extend({
-                        clientX: clientPoint.x,
-                        clientY: clientPoint.y,
-                        screenX: devicePoint.x,
-                        screenY: devicePoint.y
-                    }, this.modifiers)
-                };
-
+    public run (useStrictElementCheck: boolean): Promise<unknown> {
+        return super.run(useStrictElementCheck)
+            .then(() => {
                 // NOTE: we should raise mouseup event with 'mouseActionStepDelay' after we trigger
                 // mousedown event regardless of how long mousedown event handlers were executing
-                return Promise.all([delay(this.automationSettings.mouseActionStepDelay), this._mousedown(eventArgs)])
-                    .then(() => eventArgs);
+                return Promise.all([delay(this.automationSettings.mouseActionStepDelay), this._mousedown()]);
             })
-            .then((eventArgs: EnsureElementResultArgs) => this._mouseup(eventArgs));
+            .then(() => this._mouseup());
     }
 }
