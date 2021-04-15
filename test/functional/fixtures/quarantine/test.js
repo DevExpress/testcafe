fb95e3a37f595b01ff9531f4b3ed564f9a94c5e5
Alex Kamaev <alexey.kamaev@devexpress.com>
2021-04-15 14:54:51 +0300

    only local tests

--- test/functional/fixtures/quarantine/test.js
+++ test/functional/fixtures/quarantine/test.js
@@ -1,5 +1,6 @@
 const chai               = require('chai');
 const { expect }         = chai;
+const config             = require('../../config');
 const { createReporter } = require('../../utils/reporter');
 
 chai.use(require('chai-string'));
@@ -25,53 +26,55 @@ const getReporter = function (scope) {
     });
 };
 
-describe('Using quarantineMode', function () {
-    it('Should attempt to run tests up to the failedThreshold amount (retryCount (10) - passCount (3) + 1 = 8)', function () {
-        const result   = {};
-        const reporter = getReporter(result);
+if (config.useLocalBrowsers) {
+    describe('Using quarantineMode', function () {
+        it('Should attempt to run tests up to the failedThreshold amount (retryCount (10) - passCount (3) + 1 = 8)', function () {
+            const result   = {};
+            const reporter = getReporter(result);
 
-        return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Failing test, in quarantine mode', {
-            quarantineMode: {
-                retryCount: 10
-            },
-            reporter: [ reporter ]
-        })
-            .then(function () {
-                expect(result.unstable).eql(false);
-                expect(result.failures).eql(8);
-            });
+            return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Failing test, in quarantine mode', {
+                quarantineMode: {
+                    retryCount: 10
+                },
+                reporter: [reporter]
+            })
+                .then(function () {
+                    expect(result.unstable).eql(false);
+                    expect(result.failures).eql(8);
+                });
+        });
+
+        it('Should pass the test if number of passes match passCount', function () {
+            const result   = {};
+            const reporter = getReporter(result);
+
+            return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Check for unstable test', {
+                quarantineMode: {
+                    passCount: 2
+                },
+                reporter: [reporter]
+            })
+                .then(function () {
+                    expect(result.unstable).eql(true);
+                    expect(result.passes).eql(2);
+                });
+        });
+
+        it('Should fail the test if number of failures match failureThreshold', function () {
+            const result   = {};
+            const reporter = getReporter(result);
+
+            return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Another unstable test', {
+                quarantineMode: {
+                    retryCount: 10
+                },
+                reporter: [reporter]
+            })
+                .then(function () {
+                    expect(result.unstable).eql(true);
+                    expect(result.passes).eql(2);
+                    expect(result.failures).eql(8);
+                });
+        });
     });
-
-    it('Should pass the test if number of passes match passCount', function () {
-        const result   = {};
-        const reporter = getReporter(result);
-
-        return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Check for unstable test', {
-            quarantineMode: {
-                passCount: 2
-            },
-            reporter: [ reporter ]
-        })
-            .then(function () {
-                expect(result.unstable).eql(true);
-                expect(result.passes).eql(2);
-            });
-    });
-
-    it('Should fail the test if number of failures match failureThreshold', function () {
-        const result   = {};
-        const reporter = getReporter(result);
-
-        return runTests('./testcafe-fixtures/test-quarantine-mode.js', 'Another unstable test', {
-            quarantineMode: {
-                retryCount: 10
-            },
-            reporter: [ reporter ]
-        })
-            .then(function () {
-                expect(result.unstable).eql(true);
-                expect(result.passes).eql(2);
-                expect(result.failures).eql(8);
-            });
-    });
-});
+}
