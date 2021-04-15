b1514137a91268036220c9c821c483359993a1dc
Alex Kamaev <alexey.kamaev@devexpress.com>
2021-04-15 13:12:11 +0300

    try fix tests for task with multiple browsers started once

--- test/functional/fixtures/quarantine/testcafe-fixtures/test-quarantine-mode.js
+++ test/functional/fixtures/quarantine/testcafe-fixtures/test-quarantine-mode.js
@@ -1,30 +1,34 @@
 import quarantineScope from './quarantineScope';
 
 
-// NOTE: to preserve callsites, add new tests AFTER the existing ones
 fixture `Test Quarantine Mode`
-    .page `../pages/index.html`
-    .after(() => {
-        quarantineScope['attempt'] = {};
-    });
+    .page `http://localhost:3000/fixtures/quarantine/pages/index.html`;
 
-test('Failing test, in quarantine mode', async t => {
-    await t.click('.notExist');
+test('Failing test, in quarantine mode', async () => {
+    throw new Error('Quarantine error');
 });
 
-test('Check for unstable test', async () => {
-    quarantineScope['attempt'] = quarantineScope['attempt'] || {};
-    const attemptNumber = quarantineScope['attempt'].attemptNumber || 0;
+test('Check for unstable test', async t => {
+    const key = t.testRun.test.name + t.browser.name;
 
-    quarantineScope['attempt'].attemptNumber = attemptNumber + 1;
-    if (attemptNumber % 2 === 0) throw new Error('Quarantine error');
+    quarantineScope[key] = quarantineScope[key] || {};
+
+    const attemptNumber = quarantineScope[key].attemptNumber || 0;
+
+    quarantineScope[key].attemptNumber = attemptNumber + 1;
+
+    if (attemptNumber < 2)
+        throw new Error('Quarantine error');
 });
 
-test('Another unstable test', async () => {
-    quarantineScope['attempt'] = quarantineScope['attempt'] || {};
-    const attemptNumber = quarantineScope['attempt'].attemptNumber || 0;
+test('Another unstable test', async t => {
+    const key = t.testRun.test.name + t.browser.name;
 
-    quarantineScope['attempt'].attemptNumber = attemptNumber + 1;
+    quarantineScope[key] = quarantineScope[key] || {};
+
+    const attemptNumber = quarantineScope[key].attemptNumber || 0;
+
+    quarantineScope[key].attemptNumber = attemptNumber + 1;
 
     if (attemptNumber === 0 || attemptNumber > 2)
         throw new Error('Quarantine error');
