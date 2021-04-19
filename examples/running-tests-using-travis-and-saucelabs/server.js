9d2c173a5966bdeac31e832543f6ada9d44cc9f5
Andrey <andrey.belym@devexpress.com>
2021-04-19 18:37:56 +0300

    Remove the Saucelabs example

--- examples/running-tests-using-travis-and-saucelabs/server.js
+++
@@ -1,6 +0,0 @@
-const connect     = require('connect');
-const serveStatic = require('serve-static');
-
-connect().use(serveStatic(__dirname)).listen(9090, () => {
-    process.stdout.write('Server running on 9090...\n');
-});
