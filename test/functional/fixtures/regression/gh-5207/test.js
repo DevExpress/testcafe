83b6287124e74589352d7d256d67490923d6d635
Alex Kamaev <alexey.kamaev@devexpress.com>
2021-04-26 17:32:29 +0300

    use `createReporter` function

--- test/functional/fixtures/regression/gh-5207/test.js
+++ test/functional/fixtures/regression/gh-5207/test.js
@@ -1,25 +1,15 @@
-function customReporter () {
-    return {
-        async reportTaskStart () {
-        },
-        async reportFixtureStart () {
-        },
-        async reportTestDone () {
-            return new Promise(resolve => {
-                setTimeout(resolve, 5000);
-            });
-        },
-        async reportTaskDone () {
-        }
-    };
-}
+const { createReporter } = require('../../../utils/reporter');
 
 describe('[Regression](GH-5207)', function () {
     it('Should not hand with `disablePageReloads` and async reporter', function () {
         return runTests('testcafe-fixtures/index.js', null, {
-            reporter: customReporter
+            reporter: createReporter({
+                async reportTestDone () {
+                    return new Promise(resolve => {
+                        setTimeout(resolve, 5000);
+                    });
+                }
+            })
         });
     });
 });
-
-
