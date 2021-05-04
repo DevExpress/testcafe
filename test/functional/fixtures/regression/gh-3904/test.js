0e3dc5176d4040391f01dd7b90405601b7761013
Alex <alexikam@gmail.com>
2021-05-04 14:39:20 +0300

    add a note about 6201

--- test/functional/fixtures/regression/gh-3904/test.js
+++ test/functional/fixtures/regression/gh-3904/test.js
@@ -1,3 +1,4 @@
+// See https://github.com/DevExpress/testcafe/issues/6201 for more information.
 describe.skip('GH-3904 - Should correctly switch to iframe loaded from form', function () {
     it('Should correctly switch to iframe loaded from form', function () {
         return runTests('testcafe-fixtures/index.js');
