778c13394b22abe088fda3396b8d1b01c2fb83a6
Farfurix <vladimir.airich@gmail.com>
2021-05-06 12:18:41 +0300

    Run the head script check in all browsers

--- test/functional/fixtures/hammerhead/gh-2622/test.js
+++ test/functional/fixtures/hammerhead/gh-2622/test.js
@@ -1,5 +1,5 @@
 describe('Page and iframe documents should not contain injected head scripts', () => {
     it('Page and iframe documents should not contain injected head scripts', () => {
-        return runTests('testcafe-fixtures/index.js', null, { only: 'chrome' });
+        return runTests('testcafe-fixtures/index.js', null);
     });
 });
