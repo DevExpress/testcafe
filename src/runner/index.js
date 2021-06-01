a528b9cdc9cc39fd5e72c75f90ec0d1a26ea0f5c
Aleksey28 <alekseypopow1995@yandex.ru>
2021-06-01 09:59:41 +0300

    fix: added condition before getting cdpPort

--- src/runner/index.js
+++ src/runner/index.js
@@ -259,7 +259,11 @@ export default class Runner extends EventEmitter {
         if (typeof concurrency !== 'number' || isNaN(concurrency) || concurrency < 1)
             throw new GeneralError(RUNTIME_ERRORS.invalidConcurrencyFactor);
 
-        if (concurrency > 1 && this.bootstrapper.browsers.some(browser => browser.browserConfig.cdpPort))
+        if (concurrency > 1 && this.bootstrapper.browsers.some(browser => {
+            return browser instanceof BrowserConnection
+                ? browser.browserInfo.browserConfig.cdpPort
+                : browser.browserConfig.cdpPort;
+        }))
             throw new GeneralError(RUNTIME_ERRORS.cannotSetConcurrencyWithCDPPort);
     }
 
