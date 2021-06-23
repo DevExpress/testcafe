85eb9b4dcc4a1beff1ad0fd819545bc1a615e033
Pavel <wabloxa@yandex.ru>
2021-06-23 21:23:21 +0300

    test: increase timeout for iframe

--- test/functional/fixtures/regression/gh-4232/testcafe-fixtures/index-test.js
+++ test/functional/fixtures/regression/gh-4232/testcafe-fixtures/index-test.js
@@ -4,7 +4,7 @@ fixture `gh-4232`
     .page `http://localhost:3000/fixtures/regression/gh-4232/pages/index.html`;
 
 test('Click on submit button in an iframe when the click redirecting to a page on a different domain', async t => {
-    const iframe = Selector('#same-domain-iframe');
+    const iframe = Selector('#same-domain-iframe', { timeout: 10000 });
 
     await t
         .switchToIframe(iframe)
