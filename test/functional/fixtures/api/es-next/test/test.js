1f9d6b6d3ba7182d4839c3b7fef22e30643a4802
Aleksey28 <alekseypopow1995@yandex.ru>
2021-06-24 11:23:59 +0300

    test: added test for import test

---
+++ test/functional/fixtures/api/es-next/test/test.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+const DEFAULT_SELECTOR_TIMEOUT   = 3000;
+const DEFAULT_RUN_OPTIONS        = {
+    selectorTimeout: DEFAULT_SELECTOR_TIMEOUT,
+};
+
+describe.only('[API] Test', function () {
+    it('Should work import Test', function () {
+        return runTests('./testcafe-fixtures/test-test.js', 'Test', DEFAULT_RUN_OPTIONS);
+    });
+});
