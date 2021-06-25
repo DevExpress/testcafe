b807e2eb696b6844cdff7fb6d6cd7ed34ee2e63d
Aleksey28 <alekseypopow1995@yandex.ru>
2021-06-25 11:50:31 +0300

    fix: deleted only in tests

--- test/functional/fixtures/api/es-next/fixture/test.js
+++ test/functional/fixtures/api/es-next/fixture/test.js
@@ -4,7 +4,7 @@ const DEFAULT_RUN_OPTIONS        = {
     selectorTimeout: DEFAULT_SELECTOR_TIMEOUT,
 };
 
-describe.only('[API] Fixture', function () {
+describe('[API] Fixture', function () {
     it('Should work import Fixture', function () {
         return runTests('./testcafe-fixtures/fixture-test.js', 'Fixture', DEFAULT_RUN_OPTIONS);
     });
--- test/functional/fixtures/api/es-next/test/test.js
+++ test/functional/fixtures/api/es-next/test/test.js
@@ -4,7 +4,7 @@ const DEFAULT_RUN_OPTIONS        = {
     selectorTimeout: DEFAULT_SELECTOR_TIMEOUT,
 };
 
-describe.only('[API] Test', function () {
+describe('[API] Test', function () {
     it('Should work import Test', function () {
         return runTests('./testcafe-fixtures/test-test.js', 'Test', DEFAULT_RUN_OPTIONS);
     });
