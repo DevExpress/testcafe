90807b40259e82d7f0e4c6196704fad882741523
Aleksey28 <alekseypopow1995@yandex.ru>
2021-07-01 13:07:36 +0300

    test: moved tests for import test and fixture, and added test for attached tests

--- test/functional/fixtures/api/es-next/fixture/testcafe-fixtures/fixture-test.js
+++
@@ -1,9 +0,0 @@
-import { fixture } from 'testcafe';
-
-fixture `Fixture`
-    .page `http://localhost:3000/fixtures/api/es-next/fixture/pages/index.html`;
-
-test('Fixture', async t => {
-    await t
-        .click('#button1');
-});
--- test/functional/fixtures/api/es-next/test/testcafe-fixtures/test-test.js
+++
@@ -1,9 +0,0 @@
-import { test } from 'testcafe';
-
-fixture `Test`
-    .page `http://localhost:3000/fixtures/api/es-next/test/pages/index.html`;
-
-test('Test', async t => {
-    await t
-        .click('#button1');
-});
