93568737502219fb159c3829bf611a88e4803948
Aleksey28 <alekseypopow1995@yandex.ru>
2021-07-06 15:54:22 +0300

    test: added test for global tests hooks

---
+++ test/functional/fixtures/api/es-next/hooks/testcafe-fixtures/test-hooks-global.js
@@ -0,0 +1,39 @@
+fixture`Fixture 1`
+    .page`http://localhost:3000/fixtures/api/es-next/hooks/pages/index.html`;
+
+test('Test1', async t => {
+    await t
+        .click('#test')
+        .expect(t.ctx.testBefore).eql(1)
+        .expect(t.ctx.testAfter).eql(0);
+});
+
+test
+    .before((t) => {
+        t.ctx.testBefore = t.ctx.testBefore ? t.ctx.testBefore + 1 : 1;
+    })
+    ('Test2', async t => {
+        await t
+            .click('#test')
+            .expect(t.ctx.testBefore).eql(2)
+            .expect(t.ctx.testAfter).eql(0);
+    })
+    .after(async (t) => {
+        t.ctx.testAfter++;
+    });
+
+fixture`Fixture2`
+    .page`http://localhost:3000/fixtures/api/es-next/hooks/pages/index.html`
+    .beforeEach(async (t) => {
+        t.ctx.testBefore = t.ctx.testBefore ? t.ctx.testBefore + 1 : 1;
+    })
+    .afterEach(async (t) => {
+        t.ctx.testAfter++;
+    });
+
+test('Test3', async t => {
+    await t
+        .click('#test')
+        .expect(t.ctx.testBefore).eql(2)
+        .expect(t.ctx.testAfter).eql(0);
+});
