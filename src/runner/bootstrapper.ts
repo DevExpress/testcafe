1f0ed8e995a6b7189674624b5f8f927780abdb33
Aleksey28 <alekseypopow1995@yandex.ru>
2021-07-20 17:39:10 +0300

    refactor: aligned

--- src/runner/bootstrapper.ts
+++ src/runner/bootstrapper.ts
@@ -209,12 +209,12 @@ export default class Bootstrapper {
         this._assertGlobalHooks();
 
         const fixtureBefore = this.hooks?.fixture?.before || null;
-        const fixtureAfter = this.hooks?.fixture?.after || null;
+        const fixtureAfter  = this.hooks?.fixture?.after || null;
 
         tests.forEach(item => {
             if (item.fixture) {
                 item.fixture.globalBeforeFn = item.fixture.globalBeforeFn || fixtureBefore;
-                item.fixture.globalAfterFn = item.fixture.globalAfterFn || fixtureAfter;
+                item.fixture.globalAfterFn  = item.fixture.globalAfterFn || fixtureAfter;
             }
         });
     }
