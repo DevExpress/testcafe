308c901babb1ddbcc5b93ee791a410e3e8accc31
Aleksey28 <alekseypopow1995@yandex.ru>
2021-08-05 19:01:28 +0300

    feat: added possibility to set the test execution timeout in the CLI

--- src/cli/argument-parser.ts
+++ src/cli/argument-parser.ts
@@ -63,6 +63,7 @@ interface CommandLineOptions {
     pageRequestTimeout?: string | number;
     ajaxRequestTimeout?: string | number;
     browserInitTimeout?: string | number;
+    testExecutionTimeout?: string | number;
     concurrency?: string | number;
     quarantineMode?: boolean | Dictionary<string | number>;
     ports?: string | number[];
@@ -143,6 +144,7 @@ export default class CLIArgumentParser {
             .option('--page-request-timeout <ms>', "specifies the timeout in milliseconds to complete the request for the page's HTML")
             .option('--ajax-request-timeout <ms>', 'specifies the timeout in milliseconds to complete the AJAX requests (XHR or fetch)')
             .option('--browser-init-timeout <ms>', 'specify the time (in milliseconds) TestCafe waits for the browser to start')
+            .option('--test-execution-timeout <ms>', 'specify the time (in milliseconds) TestCafe waits for the test executed')
             .option('--speed <factor>', 'set the speed of test execution (0.01 ... 1)')
             .option('--ports <port1,port2>', 'specify custom port numbers')
             .option('--hostname <name>', 'specify the hostname')
@@ -266,6 +268,14 @@ export default class CLIArgumentParser {
         assertType(is.nonNegativeNumberString, null, 'The browser initialization timeout', this.opts.browserInitTimeout);
 
         this.opts.browserInitTimeout = parseInt(this.opts.browserInitTimeout as string, 10);
+    }
+
+    private _parseTestExecutionTimeout (): void {
+        if (this.opts.testExecutionTimeout) {
+            assertType(is.nonNegativeNumberString, null, 'The test execution timeout', this.opts.testExecutionTimeout);
+
+            this.opts.testExecutionTimeout = parseInt(this.opts.testExecutionTimeout as string, 10);
+        }
     }
 
     private _parseSpeed (): void {
@@ -411,6 +421,7 @@ export default class CLIArgumentParser {
         this._parsePageRequestTimeout();
         this._parseAjaxRequestTimeout();
         this._parseBrowserInitTimeout();
+        this._parseTestExecutionTimeout();
         this._parseAppInitDelay();
         this._parseSpeed();
         this._parsePorts();
