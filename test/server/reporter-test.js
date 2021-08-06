a8038762aac2bb7620736a0afb84bd85c84576d1
Aleksey28 <alekseypopow1995@yandex.ru>
2021-08-06 11:09:03 +0300

    test: fix reporter test according the features

--- test/server/reporter-test.js
+++ test/server/reporter-test.js
@@ -442,6 +442,8 @@ describe('Reporter', () => {
                     'warning3',
                 ],
             };
+
+            this.startTime = new Date('1970-01-01T00:00:00.000Z');
         }
 
         _createBrowserJobs () {
