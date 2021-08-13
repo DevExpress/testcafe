23384c33899b75118c05aa0ae16fa982e383a2a0
miherlosev <miherlosev@mail.ru>
2021-08-13 12:17:16 +0300

    fix 2846 test

--- gulp/constants/functional-test-globs.js
+++ gulp/constants/functional-test-globs.js
@@ -13,7 +13,6 @@ const DEBUG_GLOB = [
     BASIC_TESTS_GLOB,
     COMPILER_SERVICE_TESTS_GLOB,
     '!test/functional/fixtures/multiple-windows/test.js',
-    '!test/functional/fixtures/regression/gh-2846/test.js',
 ];
 
 module.exports = {
