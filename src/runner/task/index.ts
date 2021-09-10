6bae39b44941e6118cfe4b87fd5348a4612a3578
Aleksey28 <alekseypopow1995@yandex.ru>
2021-09-10 13:43:06 +0300

    feat: added callback for warning in the Task

--- src/runner/task/index.ts
+++ src/runner/task/index.ts
@@ -58,7 +58,9 @@ export default class Task extends AsyncEventEmitter {
         this.tests                   = tests;
         this.opts                    = opts;
         this._proxy                  = proxy;
-        this.warningLog              = new WarningLog();
+        this.warningLog              = new WarningLog(null, async message => {
+            await messageBus.emit('warning-add', { message });
+        });
         this._compilerService        = compilerService;
         this._messageBus             = messageBus;
 
