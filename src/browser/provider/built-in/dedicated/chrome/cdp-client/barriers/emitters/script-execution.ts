daee98aeef32dd494662d0c5a629a412ef269820
Artem Lavrov <artjomlav@gmail.com>
2021-09-23 15:23:32 +0300

    Refactor the page unload barrier + add server's unload barrier

--- src/browser/provider/built-in/dedicated/chrome/cdp-client/barriers/emitters/script-execution.ts
+++ src/browser/provider/built-in/dedicated/chrome/cdp-client/barriers/emitters/script-execution.ts
@@ -1,5 +1,5 @@
-import EventEmitter from '../../../../../../../shared/utils/event-emitter';
-import { ScriptExecutionEmitter } from '../../../../../../../shared/types';
+import EventEmitter from '../../../../../../../../shared/utils/event-emitter';
+import { ScriptExecutionEmitter } from '../../../../../../../../shared/types';
 import ProtocolProxyApi from 'devtools-protocol/types/protocol-proxy-api';
 import NetworkApi = ProtocolProxyApi.NetworkApi;
 import Protocol from 'devtools-protocol';
