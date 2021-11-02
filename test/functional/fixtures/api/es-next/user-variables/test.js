1d70a86139f3a9a1c6fa6675fb4c51de11c6b14f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-02 14:47:09 +0400

    test: added comment to the test [API] UserVariables->Read

--- test/functional/fixtures/api/es-next/user-variables/test.js
+++ test/functional/fixtures/api/es-next/user-variables/test.js
@@ -7,6 +7,8 @@ const DEFAULT_RUN_OPTIONS        = {
 
 describe('[API] UserVariables', function () {
     it('Read', function () {
+        //It's a temporary hack since sending 'userVariables' in run isn't realized.
+        //https://github.com/DevExpress/testcafe/issues/6652
         Object.assign(exportableLib.userVariables, {
             url:             'localhost',
             port:            1337,
