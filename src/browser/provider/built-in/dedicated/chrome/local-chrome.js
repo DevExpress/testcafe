4ba5f32d228ff11ca3b7b26b40cca485f92d0b6d
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-11-25 10:07:13 +0300

    feat: added method startOnDocker

--- src/browser/provider/built-in/dedicated/chrome/local-chrome.js
+++ src/browser/provider/built-in/dedicated/chrome/local-chrome.js
@@ -2,6 +2,7 @@ import browserTools from 'testcafe-browser-tools';
 import { killBrowserProcess } from '../../../../../utils/process';
 import BrowserStarter from '../../../utils/browser-starter';
 import { buildChromeArgs } from './build-chrome-args';
+import remoteChrome from 'chrome-remote-interface';
 
 const browserStarter = new BrowserStarter();
 
@@ -12,6 +13,17 @@ export async function start (pageUrl, { browserName, config, cdpPort, tempProfil
     chromeOpenParameters.cmd = buildChromeArgs({ config, cdpPort, platformArgs: chromeOpenParameters.cmd, tempProfileDir, inDocker });
 
     await browserStarter.startBrowser(chromeOpenParameters, pageUrl);
+}
+
+export async function startOnDocker (pageUrl, { browserName, config, cdpPort, tempProfileDir, inDocker }) {
+    await start('', { browserName, config, cdpPort, tempProfileDir, inDocker });
+    await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+    const tabs       = await remoteChrome.List({ port: cdpPort });
+    const target     = tabs.filter(t => t.type === 'page')[0];
+    const { Target } = await remoteChrome({ target, port: cdpPort });
+
+    await Target.createTarget({ url: pageUrl });
 }
 
 export async function stop ({ browserId }) {
