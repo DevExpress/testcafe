f58b57f2ab01fdc384844568cdc80f503188b60d
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-12-14 10:30:56 +0300

    fix: fixed the error of start chromium under the docker (#6723)
    * feat: added method startOnDocker
    
    * feat: added calling method startOnDocker instead of start for docker
    
    * refactor: commented promise
    
    * feat: added repeating getting of the browser targets
    
    * feat: added deleting old tab
    
    * refactor: refactored getting tabs

--- src/browser/provider/built-in/dedicated/chrome/local-chrome.js
+++ src/browser/provider/built-in/dedicated/chrome/local-chrome.js
@@ -2,8 +2,14 @@ import browserTools from 'testcafe-browser-tools';
 import { killBrowserProcess } from '../../../../../utils/process';
 import BrowserStarter from '../../../utils/browser-starter';
 import { buildChromeArgs } from './build-chrome-args';
+import remoteChrome from 'chrome-remote-interface';
+import Timer from '../../../../../utils/timer';
+import delay from '../../../../../utils/delay';
 
 const browserStarter = new BrowserStarter();
+
+const LIST_TABS_TIMEOUT = 5000;
+const LIST_TABS_DELAY   = 500;
 
 export async function start (pageUrl, { browserName, config, cdpPort, tempProfileDir, inDocker }) {
     const chromeInfo           = await browserTools.getBrowserInfo(config.path || browserName);
@@ -12,6 +18,38 @@ export async function start (pageUrl, { browserName, config, cdpPort, tempProfil
     chromeOpenParameters.cmd = buildChromeArgs({ config, cdpPort, platformArgs: chromeOpenParameters.cmd, tempProfileDir, inDocker });
 
     await browserStarter.startBrowser(chromeOpenParameters, pageUrl);
+}
+
+async function tryListTabs (cdpPort) {
+    try {
+        return { tabs: await remoteChrome.List({ port: cdpPort }) };
+    }
+    catch (error) {
+        return { error };
+    }
+}
+
+export async function startOnDocker (pageUrl, { browserName, config, cdpPort, tempProfileDir, inDocker }) {
+    await start('', { browserName, config, cdpPort, tempProfileDir, inDocker });
+
+    let { tabs, error } = await tryListTabs(cdpPort);
+    const timer         = new Timer(LIST_TABS_TIMEOUT);
+
+    //NOTE: We should repeat getting 'List' after a while because we can get an error if the browser isn't ready.
+    while (error && !timer.expired) {
+        await delay(LIST_TABS_DELAY);
+
+        ({ tabs, error } = await tryListTabs(cdpPort));
+    }
+
+    if (error)
+        throw error;
+
+    const target     = tabs.filter(t => t.type === 'page')[0];
+    const { Target } = await remoteChrome({ target, port: cdpPort });
+
+    await Target.createTarget({ url: pageUrl });
+    await remoteChrome.Close({ id: target.id, port: cdpPort });
 }
 
 export async function stop ({ browserId }) {
