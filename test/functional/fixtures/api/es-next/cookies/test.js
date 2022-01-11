08f68f194682ecabcd1d95bd656cf4778bd0f2a9
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-01-11 18:43:08 +0300

    test: refactored tests for getting cookies

--- test/functional/fixtures/api/es-next/cookies/test.js
+++ test/functional/fixtures/api/es-next/cookies/test.js
@@ -1,6 +1,30 @@
 describe('[API] Cookies', function () {
-    it('Should get cookies (t.getCookies)', function () {
-        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies (t.getCookies)');
+    it('Should get all cookies', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get all cookies');
+    });
+    it('Should get cookies by name', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by name');
+    });
+    it('Should get cookies by names', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by names');
+    });
+    it('Should get cookies by name and url', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by name and url');
+    });
+    it('Should get cookies by names and url', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by names and url');
+    });
+    it('Should get cookies by name and urls', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by name and urls');
+    });
+    it('Should get cookies by names and urls', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by names and urls');
+    });
+    it('Should get cookies by object with domain', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by object with domain and path');
+    });
+    it('Should get cookies by objects', function () {
+        return runTests('./testcafe-fixtures/cookies-test.js', 'Should get cookies by objects');
     });
 
     it('Should set cookies (t.setCookies)', function () {
--- test/functional/fixtures/api/es-next/cookies/testcafe-fixtures/cookies-test.js
+++ test/functional/fixtures/api/es-next/cookies/testcafe-fixtures/cookies-test.js
@@ -1,114 +1,373 @@
 import { RequestLogger } from 'testcafe';
 
-fixture `Cookies API`;
 
-test
-    .before(async t => {
+fixture`[API] Get Cookies`
+    .page('http://localhost:3000/fixtures/api/es-next/cookies/pages/index.html')
+    .beforeEach(async t => {
         await t
-            .setCookies([{ apiCookie1: 'value1' }, { 'apiCookie2': 'value2' }], 'https://domain1.com/')
             .setCookies([
-                { name: 'apiCookie3', value: 'value3', domain: 'domain2.com', path: '/' },
-                { name: 'apiCookie4', value: 'value4', domain: 'domain2.com', path: '/path-1' },
-                { name: 'apiCookie5', value: 'value5', domain: 'domain1.com', path: '/path-2' },
-                { name: 'apiCookie6', value: 'value6', domain: 'domain1.com', path: '/path-2' },
-                { name: 'apiCookie3', value: 'value3', domain: 'domain1.com', path: '/' },
-                { name: 'apiCookie7', value: 'value7', domain: 'domain2.com', path: '/path-2' },
-                { name: 'apiCookie8', value: 'value8', domain: 'domain2.com', path: '/path-2/path-2-1' },
-                { name: 'apiCookie9', value: 'value9', domain: 'domain3.com', path: '/path-3' },
-                { name: 'apiCookie10', value: 'value10', domain: 'domain3.com', path: '/path-4/path-4-1' },
+                { name: 'apiCookie1', value: 'value1', domain: 'domain1.com', path: '/' },
+                { name: 'apiCookie1', value: 'value1', domain: 'domain2.com', path: '/' },
+                { name: 'apiCookie2', value: 'value2', domain: 'domain2.com', path: '/' },
+                { name: 'apiCookie3', value: 'value3', domain: 'domain2.com', path: '/path-1' },
+                { name: 'apiCookie4', value: 'value4', domain: 'domain1.com', path: '/path-2' },
+                { name: 'apiCookie5', value: 'value5', domain: 'domain2.com', path: '/path-1' },
             ]);
-    }).page('http://localhost:3000/fixtures/api/es-next/cookies/pages/index.html')('Should get cookies (t.getCookies)', async t => {
-        const allCookies = await t.getCookies();
-
-        await t
-            .expect(allCookies.length).eql(11)
-            .expect(allCookies.some(c => c.name === 'apiCookie1' && c.value === 'value1')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie2' && c.value === 'value2')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain2.com' && c.path === '/')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie4' && c.value === 'value4' && c.domain === 'domain2.com' && c.path === '/path-1')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie5' && c.value === 'value5' && c.domain === 'domain1.com' && c.path === '/path-2')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie6' && c.value === 'value6' && c.domain === 'domain1.com' && c.path === '/path-2')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain1.com' && c.path === '/')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie7' && c.value === 'value7' && c.domain === 'domain2.com' && c.path === '/path-2')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie8' && c.value === 'value8' && c.domain === 'domain2.com' && c.path === '/path-2/path-2-1')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie9' && c.value === 'value9' && c.domain === 'domain3.com' && c.path === '/path-3')).ok()
-            .expect(allCookies.some(c => c.name === 'apiCookie10' && c.value === 'value10' && c.domain === 'domain3.com' && c.path === '/path-4/path-4-1')).ok();
-
-
-        const cookiesByName = await t.getCookies('apiCookie3');
-
-        await t
-            .expect(cookiesByName.length).eql(2)
-            .expect(cookiesByName.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain2.com' && c.path === '/')).ok()
-            .expect(cookiesByName.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain1.com' && c.path === '/')).ok();
-
-
-        const cookiesByNames = await t.getCookies(['apiCookie1', 'apiCookie4']);
-
-        await t
-            .expect(cookiesByNames.length).eql(2)
-            .expect(cookiesByNames.some(c => c.name === 'apiCookie1' && c.value === 'value1')).ok()
-            .expect(cookiesByNames.some(c => c.name === 'apiCookie4' && c.value === 'value4' && c.domain === 'domain2.com' && c.path === '/path-1')).ok();
-
-
-        const cookieByNameAndUrl = await t.getCookies('apiCookie5', 'https://domain1.com/path-2');
-
-        await t
-            .expect(cookieByNameAndUrl.length).eql(1)
-            .expect(cookieByNameAndUrl.some(c => c.name === 'apiCookie5' && c.value === 'value5' && c.domain === 'domain1.com' && c.path === '/path-2')).ok();
-
-
-        const cookiesByNamesAndUrl = await t.getCookies(['apiCookie5', 'apiCookie6'], 'https://domain1.com/path-2');
-
-        await t
-            .expect(cookiesByNamesAndUrl.length).eql(2)
-            .expect(cookiesByNamesAndUrl.some(c => c.name === 'apiCookie5' && c.value === 'value5' && c.domain === 'domain1.com' && c.path === '/path-2')).ok()
-            .expect(cookiesByNamesAndUrl.some(c => c.name === 'apiCookie6' && c.value === 'value6' && c.domain === 'domain1.com' && c.path === '/path-2')).ok();
-
-
-        const cookiesByNameAndUrls = await t.getCookies('apiCookie3', ['https://domain1.com/', 'https://domain2.com/']);
-
-        await t
-            .expect(cookiesByNameAndUrls.length).eql(2)
-            .expect(cookiesByNameAndUrls.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain1.com' && c.path === '/')).ok()
-            .expect(cookiesByNameAndUrls.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain2.com' && c.path === '/')).ok();
-
-
-        const domainCookiesByCookieLikeObject = await t.getCookies({ domain: 'domain1.com' });
-
-        await t
-            .expect(domainCookiesByCookieLikeObject.length).eql(5)
-            .expect(domainCookiesByCookieLikeObject.some(c => c.name === 'apiCookie1' && c.value === 'value1' && c.path === '/')).ok()
-            .expect(domainCookiesByCookieLikeObject.some(c => c.name === 'apiCookie2' && c.value === 'value2' && c.path === '/')).ok()
-            .expect(domainCookiesByCookieLikeObject.some(c => c.name === 'apiCookie5' && c.value === 'value5' && c.path === '/path-2')).ok()
-            .expect(domainCookiesByCookieLikeObject.some(c => c.name === 'apiCookie6' && c.value === 'value6' && c.path === '/path-2')).ok()
-            .expect(domainCookiesByCookieLikeObject.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain1.com' && c.path === '/')).ok();
-
-
-        const domainPathCookiesByCookieLikeObject = await t.getCookies({ domain: 'domain2.com', path: '/path-2/path-2-1' });
-
-        await t
-            .expect(domainPathCookiesByCookieLikeObject.length).eql(3)
-            .expect(domainPathCookiesByCookieLikeObject.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain2.com' && c.path === '/')).ok()
-            .expect(domainPathCookiesByCookieLikeObject.some(c => c.name === 'apiCookie7' && c.value === 'value7' && c.domain === 'domain2.com' && c.path === '/path-2')).ok()
-            .expect(domainPathCookiesByCookieLikeObject.some(c => c.name === 'apiCookie8' && c.value === 'value8' && c.domain === 'domain2.com' && c.path === '/path-2/path-2-1')).ok();
-
-
-        const cookiesByCookieLikeObjects = await t.getCookies(
-            { name: 'apiCookie1' },
-            [{ domain: 'domain2.com' }],
-            [{ domain: 'domain3.com', path: '/path-3' }],
-        );
-
-        await t
-            .expect(cookiesByCookieLikeObjects.length).eql(6)
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie1' && c.value === 'value1')).ok()
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie3' && c.value === 'value3' && c.domain === 'domain2.com' && c.path === '/')).ok()
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie4' && c.value === 'value4' && c.domain === 'domain2.com' && c.path === '/path-1')).ok()
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie7' && c.value === 'value7' && c.domain === 'domain2.com' && c.path === '/path-2')).ok()
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie8' && c.value === 'value8' && c.domain === 'domain2.com' && c.path === '/path-2/path-2-1')).ok()
-            .expect(cookiesByCookieLikeObjects.some(c => c.name === 'apiCookie9' && c.value === 'value9' && c.domain === 'domain3.com' && c.path === '/path-3')).ok();
+    })
+    .afterEach(async t => {
+        await t.deleteCookies();
     });
+
+test('Should get all cookies', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie2',
+            'value':    'value2',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie3',
+            'value':    'value3',
+            'domain':   'domain2.com',
+            'path':     '/path-1',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie4',
+            'value':    'value4',
+            'domain':   'domain1.com',
+            'path':     '/path-2',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie5',
+            'value':    'value5',
+            'domain':   'domain2.com',
+            'path':     '/path-1',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies();
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by name', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies('apiCookie1');
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by names', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie4',
+            'value':    'value4',
+            'domain':   'domain1.com',
+            'path':     '/path-2',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies(['apiCookie1', 'apiCookie4']);
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by name and url', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie4',
+            'value':    'value4',
+            'domain':   'domain1.com',
+            'path':     '/path-2',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies('apiCookie4', 'https://domain1.com/path-2');
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by names and url', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie3',
+            'value':    'value3',
+            'domain':   'domain2.com',
+            'path':     '/path-1',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie5',
+            'value':    'value5',
+            'domain':   'domain2.com',
+            'path':     '/path-1',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies(['apiCookie3', 'apiCookie5'], 'https://domain2.com/path-1');
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by name and urls', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies('apiCookie1', ['https://domain1.com/', 'https://domain2.com/']);
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by names and urls', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie2',
+            'value':    'value2',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies(['apiCookie1', 'apiCookie2'], ['https://domain1.com/', 'https://domain2.com/']);
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by object with domain and path', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie4',
+            'value':    'value4',
+            'domain':   'domain1.com',
+            'path':     '/path-2',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies({ domain: 'domain1.com', path: '/path-2' });
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+test('Should get cookies by objects', async t => {
+    const expectedCookies = [
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain1.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie1',
+            'value':    'value1',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+        {
+            'name':     'apiCookie2',
+            'value':    'value2',
+            'domain':   'domain2.com',
+            'path':     '/',
+            'expires':  'Infinity',
+            'maxAge':   null,
+            'secure':   false,
+            'httpOnly': false,
+            'sameSite': 'none',
+        },
+    ];
+    const cookies         = await t.getCookies(
+        { name: 'apiCookie1' },
+        [{ domain: 'domain2.com', path: '/' }]);
+
+    await t.expect(expectedCookies).eql(cookies);
+});
+
+fixture `Cookies API`;
 
 test('Should set cookies (t.setCookies)', async t => {
     const logger = RequestLogger(/fixtures\/api\/es-next\/cookies\/pages/, { logRequestHeaders: true });
