ae5214dd50e677acae5a1e7e106d7b92319f2d4d
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-01-27 12:49:15 +0300

    test: fixed test Actions list

--- test/server/data/test-controller-reporter-expected/index.js
+++ test/server/data/test-controller-reporter-expected/index.js
@@ -823,45 +823,58 @@ module.exports = [
     },
     {
         testRunId: 'test-run-id',
-        name: 'getCookies',
-        command: {
-            type: 'get-cookies',
+        name:      'getCookies',
+        command:   {
+            type:     'get-cookies',
             actionId: 'GetCookiesCommand',
-            names: ['cookieName'],
-            urls: ['https://domain.com'],
-            cookies: undefined
+            urls:     ['https://domain.com'],
+            cookies:  [
+                {
+                    name: 'cookieName',
+                },
+            ],
         },
-        test: { id: 'test-id', name: 'test-name', phase: 'initial' },
+        test:    { id: 'test-id', name: 'test-name', phase: 'initial' },
         fixture: { name: 'fixture-name', id: 'fixture-id' },
-        browser: { alias: 'test-browser', headless: false }
+        browser: { alias: 'test-browser', headless: false },
     },
     {
         testRunId: 'test-run-id',
-        name: 'setCookies',
-        command: {
-            type: 'set-cookies',
+        name:      'setCookies',
+        command:   {
+            type:     'set-cookies',
             actionId: 'SetCookiesCommand',
-            nameValueObjects: [{ cookieName: 'cookieValue' }],
-            url: 'https://domain.com',
-            cookies: undefined
+            url:      'https://domain.com',
+            cookies:  [
+                {
+                    name:  'cookieName',
+                    value: 'cookieValue',
+                },
+            ],
         },
-        test: { id: 'test-id', name: 'test-name', phase: 'initial' },
+        test:    { id: 'test-id', name: 'test-name', phase: 'initial' },
         fixture: { name: 'fixture-name', id: 'fixture-id' },
-        browser: { alias: 'test-browser', headless: false }
+        browser: { alias: 'test-browser', headless: false },
     },
     {
         testRunId: 'test-run-id',
-        name: 'deleteCookies',
-        command: {
-            type: 'delete-cookies',
+        name:      'deleteCookies',
+        command:   {
+            type:     'delete-cookies',
             actionId: 'DeleteCookiesCommand',
-            names: ['cookieName1', 'cookieName2'],
-            urls: ['https://domain.com'],
-            cookies: undefined
+            urls:     ['https://domain.com'],
+            cookies:  [
+                {
+                    name: 'cookieName1',
+                },
+                {
+                    name: 'cookieName2',
+                },
+            ],
         },
-        test: { id: 'test-id', name: 'test-name', phase: 'initial' },
+        test:    { id: 'test-id', name: 'test-name', phase: 'initial' },
         fixture: { name: 'fixture-name', id: 'fixture-id' },
-        browser: { alias: 'test-browser', headless: false }
-    }
+        browser: { alias: 'test-browser', headless: false },
+    },
 
 ];
