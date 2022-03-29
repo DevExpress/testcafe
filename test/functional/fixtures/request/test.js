5eb6ba8d1464ebd0bbe84b414ac64954b53550de
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-03-29 17:40:17 +0400

    test: fixed tests Aleksey Popov Today 3:37 PM

--- test/functional/fixtures/request/test.js
+++ test/functional/fixtures/request/test.js
@@ -2,34 +2,34 @@ describe('Request', () => {
     it('Should execute GET request', function () {
         return runTests('testcafe-fixtures/request-test.js', 'Should execute a GET request');
     });
-    //
-    // it('Should execute POST request', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method get', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method post', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method delete', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method put', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method patch', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
-    //
-    // it('Should execute a request with method head', function () {
-    //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
-    // });
+
+    it('Should execute a POST request', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a POST request');
+    });
+
+    it('Should execute a request with method get', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method get');
+    });
+
+    it('Should execute a request with method post', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method post');
+    });
+
+    it('Should execute a request with method delete', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method delete');
+    });
+
+    it('Should execute a request with method put', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method put');
+    });
+
+    it('Should execute a request with method patch', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method patch');
+    });
+
+    it('Should execute a request with method head', function () {
+        return runTests('testcafe-fixtures/request-test.js', 'Should execute a request with method head');
+    });
     //
     // it('Should execute a request in an assertion', function () {
     //     return runTests('testcafe-fixtures/request-test.js', 'Should execute simple request');
