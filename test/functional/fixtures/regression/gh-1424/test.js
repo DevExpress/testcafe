8b300ec266391e9eccf73e29150e6091ed3a60d1
miherlosev <miherlosev@mail.ru>
2022-04-06 14:13:39 +0300

    turn on the skipped test in Firefox

--- test/functional/fixtures/regression/gh-1424/test.js
+++ test/functional/fixtures/regression/gh-1424/test.js
@@ -1,6 +1,5 @@
 describe('[Regression](GH-1424)', function () {
     it('Should raise click event on a button after "enter" key is pressed', function () {
-        // TODO: fix it for Firefox
-        return runTests('testcafe-fixtures/index-test.js', 'Press enter', { skip: ['firefox-osx', 'firefox'] });
+        return runTests('testcafe-fixtures/index-test.js', 'Press enter');
     });
 });
