b4c23475fb025f6cb11d2acd39e4629fc7d67e14
Mikhail Losev <miherlosev@mail.ru>
2022-04-06 14:03:15 +0300

    Update hammerhead (#6962)
    * update hammerhead
    
    * skip test

--- test/functional/fixtures/run-options/disable-page-caching/test.js
+++ test/functional/fixtures/run-options/disable-page-caching/test.js
@@ -1,17 +1,14 @@
 describe('Disable page caching', () => {
     it('Test run', () => {
-        // NOTE: Temporary disable running on Safari
-        return runTests('testcafe-fixtures/test-run.js', null, { disablePageCaching: true, disableMultipleWindows: true, skip: 'iphone,ipad,safari' });
+        return runTests('testcafe-fixtures/test-run.js', null, { disablePageCaching: true, disableMultipleWindows: true });
     });
 
     it('Fixture', () => {
-        // NOTE: Temporary disable running on Safari
-        return runTests('testcafe-fixtures/fixture.js', null, { disableMultipleWindows: true, skip: 'iphone,ipad,safari' });
+        return runTests('testcafe-fixtures/fixture.js', null, { disableMultipleWindows: true });
     });
 
     it('Single test', () => {
-        // NOTE: Temporary disable running on Safari
-        return runTests('testcafe-fixtures/single-test.js', null, { disableMultipleWindows: true, skip: 'iphone,ipad,safari' });
+        return runTests('testcafe-fixtures/single-test.js', null, { disableMultipleWindows: true });
     });
 });
 
