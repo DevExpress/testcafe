ae20950cf226b4650145f3492b58c65bdbfb2702
AlexKamaev <alexey.kamaev@devexpress.com>
2022-04-07 10:24:43 +0400

    fix after review

--- src/runner/dashboard-config-storage/index.ts
+++ src/runner/dashboard-config-storage/index.ts
@@ -2,7 +2,7 @@ import { DashboardConfigStorageBase, DashboardConfigOptions } from './base';
 
 export class DashboardConfigStorage extends DashboardConfigStorageBase {
     protected async readOptions (): Promise<DashboardConfigOptions> {
-        return Promise.resolve(null);
+        return null;
     }
 }
 
