e18e36473fd856278e56d95abe03ad60ed81ea31
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-05-20 17:49:28 +0400

    fix: deleted DEFAULT_STATUS and DEFAULT_STATUS_TEXT

--- src/request/send.ts
+++ src/request/send.ts
@@ -5,9 +5,6 @@ import { ExternalRequestOptions, ResponseOptions } from './interfaces';
 import { createRequestOptions } from './create-request-options';
 import { processResponseData } from './process-response-data';
 
-const DEFAULT_STATUS      = 404;
-const DEFAULT_STATUS_TEXT = 'Not found.';
-
 async function send (testRun: TestRun, options: ExternalRequestOptions, callsite: string): Promise<ResponseOptions> {
     const requestOptions = await createRequestOptions(testRun, options, callsite);
     const request        = new DestinationRequest(requestOptions);
@@ -21,8 +18,8 @@ async function send (testRun: TestRun, options: ExternalRequestOptions, callsite
     const body = await processResponseData(data, options.processResponse);
 
     return {
-        status:     data.statusCode || DEFAULT_STATUS,
-        statusText: data.statusMessage || DEFAULT_STATUS_TEXT,
+        status:     data.statusCode!,
+        statusText: data.statusMessage!,
         headers:    data.headers,
         body:       body,
     };
