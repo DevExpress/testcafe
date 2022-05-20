9ecfcc7505a88a1f0cf6dc9d9dc502244a566837
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-05-20 18:28:31 +0400

    fix: added StrictIncomingMessage type

--- src/request/send.ts
+++ src/request/send.ts
@@ -5,11 +5,13 @@ import { ExternalRequestOptions, ResponseOptions } from './interfaces';
 import { createRequestOptions } from './create-request-options';
 import { processResponseData } from './process-response-data';
 
+type StrictIncomingMessage = IncomingMessage & { statusCode: number, statusMessage: string };
+
 async function send (testRun: TestRun, options: ExternalRequestOptions, callsite: string): Promise<ResponseOptions> {
     const requestOptions = await createRequestOptions(testRun, options, callsite);
     const request        = new DestinationRequest(requestOptions);
-    const dataWaiter     = new Promise<IncomingMessage>((resolve, reject) => {
-        request.on('response', (res: IncomingMessage) => resolve(res));
+    const dataWaiter     = new Promise<StrictIncomingMessage>((resolve, reject) => {
+        request.on('response', (res: StrictIncomingMessage) => resolve(res));
         request.on('error', (err: Error) => reject(err));
         request.on('fatalError', (err: string) => reject(new Error(err)));
     });
@@ -18,8 +20,8 @@ async function send (testRun: TestRun, options: ExternalRequestOptions, callsite
     const body = await processResponseData(data, options.processResponse);
 
     return {
-        status:     data.statusCode!,
-        statusText: data.statusMessage!,
+        status:     data.statusCode,
+        statusText: data.statusMessage,
         headers:    data.headers,
         body:       body,
     };
