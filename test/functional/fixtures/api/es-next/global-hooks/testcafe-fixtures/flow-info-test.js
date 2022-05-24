3169fe68dc04cfaf918137bff71950f012e2e8ba
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-05-24 15:50:47 +0400

    test: added test 'Test with each hooks' and 'Test with all hooks'

--- test/functional/fixtures/api/es-next/global-hooks/testcafe-fixtures/flow-info-test.js
+++ test/functional/fixtures/api/es-next/global-hooks/testcafe-fixtures/flow-info-test.js
@@ -15,6 +15,49 @@ test
     .after(async function () {
         flowInfoStorage.safeAdd('localTestAfter');
     })
-    ('Test', async () => {
+    ('Test with local hooks', async () => {
+        flowInfoStorage.safeAdd('test body');
+    });
+
+fixture ('Fixture')
+    .before(async function () {
+        flowInfoStorage.safeAdd('localFixtureBefore');
+    })
+    .after(async function () {
+        flowInfoStorage.safeAdd('localFixtureAfter');
+    })
+    .beforeEach(async function () {
+        flowInfoStorage.safeAdd('eachTestBefore');
+    })
+    .afterEach(async function () {
+        flowInfoStorage.safeAdd('eachTestAfter');
+    });
+
+test('Test with each hooks', async () => {
+    flowInfoStorage.safeAdd('test body');
+});
+
+fixture ('Fixture')
+    .before(async function () {
+        flowInfoStorage.safeAdd('localFixtureBefore');
+    })
+    .after(async function () {
+        flowInfoStorage.safeAdd('localFixtureAfter');
+    })
+    .beforeEach(async function () {
+        flowInfoStorage.safeAdd('eachTestBefore');
+    })
+    .afterEach(async function () {
+        flowInfoStorage.safeAdd('eachTestAfter');
+    });
+
+test
+    .before(async function () {
+        flowInfoStorage.safeAdd('localTestBefore');
+    })
+    .after(async function () {
+        flowInfoStorage.safeAdd('localTestAfter');
+    })
+    ('Test with all hooks', async () => {
         flowInfoStorage.safeAdd('test body');
     });
