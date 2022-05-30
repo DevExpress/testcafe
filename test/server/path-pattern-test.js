6f7c155b0cb07c245e3692bd16eada42dae7bac4
artem-babich <artem.babich@devexpress.com>
2022-05-30 17:52:33 +0400

    fix parseUserAgent import

--- test/server/path-pattern-test.js
+++ test/server/path-pattern-test.js
@@ -1,7 +1,7 @@
 const path           = require('path');
 const expect         = require('chai').expect;
 const moment         = require('moment');
-const parseUserAgent = require('../../lib/utils/parse-user-agent');
+const { parseUserAgent } = require('../../lib/utils/parse-user-agent');
 const PathPattern    = require('../../lib/utils/path-pattern');
 
 
