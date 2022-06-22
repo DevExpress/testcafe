ecb47364e57cb51b51b3b6b63e9f79f8a11ae52d
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-06-22 12:08:45 +0400

    test: moved request tests

--- test/functional/fixtures/api/es-next/request/test.js
+++ test/functional/fixtures/api/es-next/request/test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const config     = require('../../config');
+const config     = require('../../../../config');
 
 describe('Request', () => {
     if (!config.experimentalDebug) {
