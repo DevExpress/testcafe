5d179bda305f23808d5c20de892f773a8c05c2c7
miherlosev <miherlosev@mail.ru>
2022-07-06 17:43:13 +0400

    1

--- src/browser/provider/built-in/dedicated/chrome/cdp-client/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import ProtocolProxyApi from 'devtools-protocol/types/protocol-proxy-api';
-import { ServerNode } from '../types';
-
-export async function describeNode (DOM: ProtocolProxyApi.DOMApi, objectId: string): Promise<ServerNode> {
-    const object   = { objectId };
-    const { node } = await DOM.describeNode(object);
-
-    return Object.assign(node, object);
-}
