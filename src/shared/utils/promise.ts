da26a023ea211f9a97848fec4a0c1a5406d37ed1
miherlosev <miherlosev@mail.ru>
2022-07-20 23:57:20 +0400

    fix wrong merge

--- src/shared/utils/promise.ts
+++
@@ -1,9 +0,0 @@
-export async function whilst (condition: () => boolean, iterator: () => Promise<unknown>): Promise<void> {
-    while (condition())
-        await iterator();
-}
-
-export async function times (n: number, iterator: (j: number) => Promise<unknown>): Promise<void> {
-    for (let i = 0; i < n; i++)
-        await iterator(i);
-}
