26b24937a2327d367acc3528d34d10658c636ce7
artem-babich <artem.babich@devexpress.com>
2022-07-27 12:03:21 +0400

    try fix tests for browserstack

--- test/functional/fixtures/regression/gh-2775/test.js
+++ test/functional/fixtures/regression/gh-2775/test.js
@@ -2,6 +2,17 @@ const { CLIENT_ERROR_MESSAGE, CLIENT_PAGE_URL, CALLBACK_FUNC_ERROR } = require('
 const config                                                         = require('../../../config');
 const expect                                                         = require('chai').expect;
 
+
+const expectFailAttempt = (errors, expectedMessage) => {
+    if (Array.isArray(errors))
+        expect(errors[0]).contains(expectedMessage);
+    else {
+        Object.values(errors).forEach(err => {
+            expect(err[0]).contains(expectedMessage);
+        });
+    }
+}
+
 // TODO: fix tests for Debug task
 describe.only('[Regression](GH-2775)', () => {
     describe('TestController command', () => {
@@ -24,41 +35,29 @@ describe.only('[Regression](GH-2775)', () => {
         it('Should fail due to incorrect message option', async () => {
             return runTests('./testcafe-fixtures/test-controller.js', 'Should fail due to incorrect message option', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
 
         it('Should fail if at least one option is incorrect', async () => {
             return runTests('./testcafe-fixtures/test-controller.js', 'Should fail if at least one option is incorrect', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
 
         it('Should fail with callback function', async () => {
             return runTests('./testcafe-fixtures/test-controller.js', 'Should fail with callback function', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
 
         it('Should fail due to error in callback function', async () => {
             return runTests('./testcafe-fixtures/test-controller.js', 'Should fail due to error in callback function', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CALLBACK_FUNC_ERROR);
-                    expect(errs[0]).to.contain(' > 62 |    await t.skipJsErrors(({ message }) => message === CLIENT_ERROR_MESSAGE);');
+                    expectFailAttempt(errs, CALLBACK_FUNC_ERROR);
+                    expectFailAttempt(errs, ' > 62 |    await t.skipJsErrors(({ message }) => message === CLIENT_ERROR_MESSAGE);');
                 });
         });
     });
@@ -79,37 +78,27 @@ describe.only('[Regression](GH-2775)', () => {
         it('Should fail due to errors in callback function', () => {
             return runTests('./testcafe-fixtures/fixture-and-test.js', 'Should fail due to errors in callback function', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CALLBACK_FUNC_ERROR);
-                    expect(errs[0]).to.contain(' > 32 |}).skipJsErrors(({ message }) => message === CLIENT_ERROR_MESSAGE);');
+                    expectFailAttempt(errs, CALLBACK_FUNC_ERROR);
+                    expectFailAttempt(errs, ' > 32 |}).skipJsErrors(({ message }) => message === CLIENT_ERROR_MESSAGE);');
                 });
         });
 
         it('Should fail due to TestController value override', () => {
             return runTests('./testcafe-fixtures/fixture-and-test.js', 'Should fail due to TestController value override', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
 
         it('Should fail with test callback function', () => {
             return runTests('./testcafe-fixtures/fixture-and-test.js', 'Should fail with test callback function', { shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
     });
 
     describe('Runner run option', () => {
-
         it('Should skip errors due to runner options', () => {
             const skipJsErrors = {
                 message: CLIENT_ERROR_MESSAGE,
@@ -135,10 +124,7 @@ describe.only('[Regression](GH-2775)', () => {
 
             return runTests('./testcafe-fixtures/runner.js', 'Throw client error', { skipJsErrors, shouldFail: true })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CLIENT_ERROR_MESSAGE);
+                    expectFailAttempt(errs, CLIENT_ERROR_MESSAGE);
                 });
         });
 
@@ -161,15 +147,9 @@ describe.only('[Regression](GH-2775)', () => {
                 shouldFail: true,
             })
                 .catch(errs => {
-                    if (!Array.isArray(errs))
-                        errs = [errs];
-
-                    expect(errs[0]).to.contain(CALLBACK_FUNC_ERROR);
-                    expect(errs[0]).to.contain(' > 289 |                    .run({');
+                    expectFailAttempt(errs, CALLBACK_FUNC_ERROR);
+                    expectFailAttempt(errs, ' > 289 |                    .run({');
                 });
         });
-
     });
-
-
 });
