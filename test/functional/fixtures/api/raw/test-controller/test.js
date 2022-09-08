c81a5751f23c49b27301a1000289431e984acf9b
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-09-08 14:01:30 +0400

    test: added test Should produce correct callsites for chained calls in hooks

--- test/functional/fixtures/api/raw/test-controller/test.js
+++ test/functional/fixtures/api/raw/test-controller/test.js
@@ -2,8 +2,9 @@ const { expect } = require('chai');
 
 // NOTE: we run tests in chrome only, because we mainly test server API functionality.
 // Actions functionality is tested in lower-level raw API.
-describe('[Raw API] TestController', () => {
-    it('Should produce correct callsites for chained calls in raw test', () => {
+/* eslint-disable */
+describe.only('[Raw API] TestController', () => {
+    it('Should produce correct callsites for chained calls', () => {
         return runTests('./testcafe-fixtures/test-controller-test.testcafe', 'Chaining callsites', {
             shouldFail: true,
             only:       'chrome',
@@ -17,7 +18,21 @@ describe('[Raw API] TestController', () => {
             });
     });
 
-    it('Should produce correct callsites with assertions for chained calls in raw test', () => {
+    it('Should produce correct callsites for chained calls in hooks', () => {
+        return runTests('./testcafe-fixtures/hooks-test.testcafe', 'Chaining callsites in hooks', {
+            shouldFail: true,
+            only:       'chrome',
+        })
+            .catch(errs => {
+                expect(errs[0]).to.contains(
+                    ' 2 |Click (Selector(\'#btn2\'))' +
+                    ' > 3 |Click (Selector(\'#errorrr\'))' +
+                    ' 4 |Click (Selector(\'#btn3\'))'
+                );
+            });
+    });
+
+    it('Should produce correct callsites with assertions for chained calls', () => {
         return runTests('./testcafe-fixtures/test-controller-test.testcafe', 'Chaining callsites with assertions', {
             shouldFail: true,
             only:       'chrome',
