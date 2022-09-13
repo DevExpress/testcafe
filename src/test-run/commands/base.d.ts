91176382ecb1a6ab750a88df2335c87fd4697e0c
artem-babich <artem.babich@devexpress.com>
2022-09-13 19:56:17 +0400

    fix CommandBase type definition

--- src/test-run/commands/base.d.ts
+++ src/test-run/commands/base.d.ts
@@ -6,6 +6,7 @@ export class CommandBase {
     public type: string;
     [key: string]: unknown;
     public _getAssignableProperties(): { name: string }[];
+    public _getAllAssignableProperties(): { name: string }[];
     public _getNonReportedProperties(): string[];
     public _getReportedProperties(): string[];
 }
