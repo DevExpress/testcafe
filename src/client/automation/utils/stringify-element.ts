9e918d893bebcdafb2f64ef0cc1c92a755c353c5
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-09-20 18:07:34 +0400

    fix: fixed truncate text for stringifying element

--- src/client/automation/utils/stringify-element.ts
+++ src/client/automation/utils/stringify-element.ts
@@ -5,20 +5,27 @@ const nativeMethods    = hammerhead.nativeMethods;
 
 const MAX_TEXT_CONTENT_LENGTH = 10;
 
+function truncateString (str: string, length: number, omission = '...'): string {
+    if (str.length < length)
+        return str;
+
+    return str.substring(0, length - omission.length) + omission;
+}
+
 export default function stringifyElement (element: HTMLElement | null): string {
     if (!element)
         return '';
 
     const emptyElement = nativeMethods.cloneNode.call(element);
     const outerHtml    = htmlUtils.cleanUpHtml(nativeMethods.elementOuterHTMLGetter.call(emptyElement));
-    const textContent  = nativeMethods.nodeTextContentGetter.call(element).substr(0, MAX_TEXT_CONTENT_LENGTH);
+    const text         = truncateString(nativeMethods.nodeTextContentGetter.call(element), MAX_TEXT_CONTENT_LENGTH);
     const children     = nativeMethods.elementChildrenGetter.call(element);
 
     if (nativeMethods.htmlCollectionLengthGetter.call(children) > 0)
         return outerHtml.replace('></', '>...</');
 
-    if (textContent)
-        return outerHtml.replace('></', `>${textContent}</`);
+    if (text)
+        return outerHtml.replace('></', `>${text}</`);
 
     return outerHtml;
 }
