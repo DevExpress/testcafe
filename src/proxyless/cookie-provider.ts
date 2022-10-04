8797ade300799d30f2caad9d0e14fb5db1b1dbc8
AlexKamaev <alexey.kamaev@devexpress.com>
2022-10-04 12:13:49 +0400

    refactoring

--- src/proxyless/cookie-provider.ts
+++ src/proxyless/cookie-provider.ts
@@ -3,36 +3,56 @@ import { ExternalCookies } from 'testcafe-hammerhead';
 import Protocol from 'devtools-protocol';
 import Cookie = Protocol.Network.Cookie;
 import { URL } from 'url';
-import { isMatch } from 'lodash';
 import { CookieOptions } from '../test-run/commands/options';
 import { CookieProvider, CookieProviderBase } from '../test-run/cookies/base';
+import CookieParam = Protocol.Network.CookieParam;
+import matchCollection from '../utils/match-collection';
 
 declare type CookieSameSite = 'Lax' | 'Strict' | 'None';
 
 const MAX_TIMESTAMP = 8640000000000000;
 
+function cdpCookieToExternalCookie (cookie: Cookie): ExternalCookies {
+    return {
+        name:     cookie.name,
+        value:    cookie.value,
+        domain:   cookie.domain,
+        maxAge:   void 0,
+        path:     cookie.path,
+        expires:  void 0,
+        secure:   cookie.secure,
+        httpOnly: cookie.httpOnly,
+        sameSite: cookie.sameSite ?? 'none',
+    } as unknown as ExternalCookies;
+}
+
+function cookieOptionToCdpCookieParam (cookie: CookieOptions, hostname: string, pathname: string): CookieParam {
+    return {
+        name:     cookie.name,
+        value:    cookie.value,
+        domain:   cookie.domain ?? hostname,
+        path:     cookie.path ?? pathname,
+        secure:   cookie.secure,
+        httpOnly: false,
+        sameSite: cookie.sameSite as CookieSameSite,
+        expires:  cookie.expires?.getTime() || MAX_TIMESTAMP,
+    };
+}
+
+function parseUrls (urls: string[]): { domain: string, path: string }[] {
+    return urls.map(url => {
+        const { hostname, pathname } = new URL(url);
+
+        return { domain: hostname, path: pathname };
+    });
+}
+
 export class CdpCookieProvider extends CookieProviderBase implements CookieProvider {
-    async getCdpClient (): Promise<remoteChrome.ProtocolApi> {
+    private async _getCdpClient (): Promise<remoteChrome.ProtocolApi> {
         const browserConnection = this.testRun.browserConnection;
         const browser           = browserConnection.provider.plugin.openedBrowsers[browserConnection.id];
 
         return browser.browserClient.getActiveClient();
-    }
-
-    filterCookies (cookies: Cookie[], cookieFilters: ExternalCookies[]): Cookie[] {
-        if (!cookieFilters.length)
-            return cookies;
-
-        const result = [];
-
-        for (const cookie of cookies) {
-            for (const cookieFilter of cookieFilters) {
-                if (isMatch(cookie, cookieFilter))
-                    result.push(cookie);
-            }
-        }
-
-        return result;
     }
 
     async initialize (): Promise<void> {
@@ -40,58 +60,28 @@ export class CdpCookieProvider extends CookieProviderBase implements CookieProvi
     }
 
     async getCookies (externalCookies: ExternalCookies[]): Promise<ExternalCookies[]> {
-        const client      = await this.getCdpClient();
+        const client      = await this._getCdpClient();
         const { cookies } = await client.Storage.getCookies({});
 
-        return this.filterCookies(cookies, externalCookies).map(cookie => {
-            const result: unknown = {
-                name:     cookie.name,
-                value:    cookie.value,
-                domain:   cookie.domain,
-                maxAge:   void 0,
-                path:     cookie.path,
-                expires:  void 0,
-                secure:   cookie.secure,
-                httpOnly: cookie.httpOnly,
-                sameSite: cookie.sameSite ?? 'none',
-            };
-
-            return result as ExternalCookies;
-        });
+        return (matchCollection(cookies, externalCookies) as Cookie[]).map(cdpCookieToExternalCookie);
     }
 
     async setCookies (cookies: CookieOptions[], url: string): Promise<void> {
-        const client = await this.getCdpClient();
+        const client = await this._getCdpClient();
         const { hostname = '', pathname = '/' } = url ? new URL(url) : {};
 
         await client.Network.setCookies({
-            cookies: cookies.map(cookie => {
-                return {
-                    name:     cookie.name,
-                    value:    cookie.value,
-                    domain:   cookie.domain ?? hostname,
-                    path:     cookie.path ?? pathname,
-                    secure:   cookie.secure,
-                    httpOnly: false,
-                    sameSite: cookie.sameSite as CookieSameSite,
-                    expires:  cookie.expires?.getTime() || MAX_TIMESTAMP,
-                };
-            }),
+            cookies: cookies.map(cookie => cookieOptionToCdpCookieParam(cookie, hostname, pathname)),
         });
     }
 
     async deleteCookies (cookies: CookieOptions[] = [], urls: string[] = []): Promise<void> {
-        const client = await this.getCdpClient();
+        const client = await this._getCdpClient();
 
         if (!cookies || !cookies.length)
             return client.Network.clearBrowserCookies();
 
-        const parsedUrls = urls.map(url => {
-            const { hostname, pathname } = new URL(url);
-
-            return { domain: hostname, path: pathname };
-        });
-
+        const parsedUrls    = parseUrls(urls);
         let existingCookies = await this.getCookies([]);
 
         if (parsedUrls.length) {
@@ -99,16 +89,14 @@ export class CdpCookieProvider extends CookieProviderBase implements CookieProvi
                 .find(url => url.domain === cookie.domain && url.path === cookie.path));
         }
 
-        for (const existingCookie of existingCookies) {
-            for (const cookieFilter of cookies) {
-                if (isMatch(existingCookie, cookieFilter)) {
-                    await client.Network.deleteCookies({
-                        name:   existingCookie.name,
-                        domain: existingCookie.domain,
-                        path:   existingCookie.path,
-                    });
-                }
-            }
+        existingCookies = matchCollection(existingCookies, cookies) as ExternalCookies[];
+
+        for (const cookie of existingCookies) {
+            await client.Network.deleteCookies({
+                name:   cookie.name,
+                domain: cookie.domain,
+                path:   cookie.path,
+            });
         }
 
         return void 0;
