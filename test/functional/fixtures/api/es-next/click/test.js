a104c99f4d3c499165a7f911bb20717dd66706bd
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-10-20 17:21:53 +0400

    test: fixed tests

--- test/functional/fixtures/api/es-next/click/test.js
+++ test/functional/fixtures/api/es-next/click/test.js
@@ -140,81 +140,84 @@ describe('[API] t.click()', function () {
     });
 
     describe('Hidden reasons', function () {
+        const runTestOptions = {
+            shouldFail: true,
+        };
+
         it('Should show that element has width: 0 and height: 0', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with width: 0 and height: 0', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with width: 0 and height: 0', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element width-height-0">Element...</div>' is not visible because it has an effective width and height of: '0 x 0' pixels.`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element width-height-0">Element...</div>) is too small to be visible: 0px x 0px.`);
                 });
         });
 
         it('Should show that element has width: 0', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with width: 0', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with width: 0', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element width-0">Element...</div>' is not visible because it has an effective width and height of: '0 x 100' pixels.`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element width-0">Element...</div>) is too small to be visible: 0px x 100px.`);
                 });
         });
 
         it('Should show that element has height: 0', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with height: 0', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with height: 0', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element height-0">Element...</div>' is not visible because it has an effective width and height of: '100 x 0' pixels.`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element height-0">Element...</div>) is too small to be visible: 100px x 0px.`);
                 });
         });
 
         it('Should show that element has display: none', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with display: none', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with display: none', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element display-none">Element...</div>' is not visible because it has CSS property: 'display: none'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element display-none">Element...</div>) is invisible. The value of its 'display' property is 'none'.`);
                 });
         });
 
         it('Should show that element has visibility: hidden', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with visibility: hidden', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with visibility: hidden', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element visibility-hidden">Element...</div>' is not visible because it has CSS property: 'visibility: hidden'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element visibility-hidden">Element...</div>) is invisible. The value of its 'visibility' property is 'hidden'.`);
                 });
         });
 
         it('Should show that element has visibility: collapse', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with visibility: collapse', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element with visibility: collapse', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element visibility-collapse">Element...</div>' is not visible because it has CSS property: 'visibility: collapse'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element visibility-collapse">Element...</div>) is invisible. The value of its 'visibility' property is 'collapse'.`);
                 });
         });
 
         it('Should show that ancestor has display: none', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with display: none', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with display: none', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element width-height-100"></div>' is not visible because its parent: '<div class="ancestor display-none">...</div>' has CSS property: 'display: none'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element width-height-100"></div>) is invisible. It descends from an element that has the 'display: none' property (<div class="ancestor display-none">...</div>).`);
                 });
         });
 
         it('Should show that ancestor has visibility: hidden', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with visibility: hidden', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with visibility: hidden', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element width-height-100"></div>' is not visible because its parent: '<div class="ancestor visibility-hidden">...</div>' has CSS property: 'visibility: hidden'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element width-height-100"></div>) is invisible. It descends from an element that has the 'visibility: hidden' property (<div class="ancestor visibility-hidden">...</div>).`);
                 });
         });
 
         it('Should show that ancestor has visibility: collapse', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with visibility: collapse', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an element in ancestor with visibility: collapse', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<div class="element width-height-100"></div>' is not visible because its parent: '<div class="ancestor visibility-collapse">...</div>' has CSS property: 'visibility: collapse'`);
+                    expect(errs[0]).to.contains(`The action target (<div class="element width-height-100"></div>) is invisible. It descends from an element that has the 'visibility: collapse' property (<div class="ancestor visibility-collapse">...</div>).`);
                 });
         });
 
         it('Should show that select has size less than 2 and is not expended', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an option in not expended select with size less than 2', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on an option in not expended select with size less than 2', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The option: '<option>Option</option>' is not visible because its parent: '<select class="select-not-expended">...</select>' is not expended and has size less than 2.`);
+                    expect(errs[0]).to.contains(`The action target (<option>Option</option>) is invisible. The parent element (<select class="select-not-expended">...</select>) is collapsed, and its length is shorter than 2.`);
                 });
         });
 
         it('Should show that map container is not visible', function () {
-            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on a map element with not visible container', { shouldFail: true })
+            return runTests('./testcafe-fixtures/click-on-hidden-elements-test.js', 'Click on a map element with not visible container', runTestOptions)
                 .catch(function (errs) {
-                    expect(errs[0]).to.contains(`The element: '<area class="map-area">' is not visible because container: '<img usemap="#map" src="" alt="">' is not visible'.`);
-                    expect(errs[0]).to.contains(`The element: '<img usemap="#map" src="" alt="">' is not visible because it has an effective width and height of: '0 x 0' pixels.`);
+                    expect(errs[0]).to.contains(`The action target (<area class="map-area">) is invisible because its container (<img usemap="#map" src="" alt="">) is too small to be visible: 0px x 0px.`);
                 });
         });
     });
