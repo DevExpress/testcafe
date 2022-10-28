a686cc05e9dcf41ed4b34488104e0d7e400fedda
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-10-28 13:46:48 +0400

    test: skipped test for debug mode

--- test/functional/fixtures/regression/gh-5961/test.js
+++ test/functional/fixtures/regression/gh-5961/test.js
@@ -1,10 +1,13 @@
-const assertionHelper = require('../../../assertion-helper.js');
-const { expect }      = require('chai');
-const config          = require('../../../config.js');
+const assertionHelper             = require('../../../assertion-helper.js');
+const { expect }                  = require('chai');
+const config                      = require('../../../config.js');
+const { skipInExperimentalDebug } = require('../../../utils/skip-in.js');
 
 if (config.useLocalBrowsers && config.useHeadlessBrowsers) {
     describe('[Regression](GH-5961)', () => {
-        it('Screenshot', () => {
+        afterEach(assertionHelper.removeScreenshotDir);
+
+        skipInExperimentalDebug('Screenshot', () => {
             return runTests('./testcafe-fixtures/index.js', 'Take a resized full page screenshot', { setScreenshotPath: true })
                 .then(function () {
                     return assertionHelper.checkScreenshotFileFullPage(false, 'custom');
