840532901031e1eb1b408ff4463474d2b702f6df
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-12-06 18:05:58 +0400

    feat: forced an --experimental-loader option to the node

--- bin/testcafe-with-v8-flag-filter.js
+++ bin/testcafe-with-v8-flag-filter.js
@@ -3,12 +3,25 @@
 'use strict';
 
 const path          = require('path');
+const url           = require('url');
 const v8FlagsFilter = require('bin-v8-flags-filter');
 
 const EXPERIMENTAL_DEBUG_OPTION = '--experimental-debug';
+const EXPERIMENTAL_ESM_OPTION   = '--experimental-esm';
 
 if (process.argv.slice(2).includes(EXPERIMENTAL_DEBUG_OPTION))
     require('../lib/cli');
 
-else
-    v8FlagsFilter(path.join(__dirname, '../lib/cli'), { useShutdownMessage: true });
+else {
+    const forcedNodeArgs = [];
+
+    if (process.argv.slice(2).includes(EXPERIMENTAL_ESM_OPTION)) {
+        forcedNodeArgs.push('--no-warnings');
+        forcedNodeArgs.push(`--experimental-loader=${url.pathToFileURL(path.join(__dirname, '../lib/compiler/esm-loader.js')).href}`);
+    }
+
+    v8FlagsFilter(path.join(__dirname, '../lib/cli'), {
+        useShutdownMessage: true,
+        forcedNodeArgs,
+    });
+}
