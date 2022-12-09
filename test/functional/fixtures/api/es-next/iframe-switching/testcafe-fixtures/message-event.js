a675927b6bf1f262209b2d16f537bd2a0aa0aba1
miherlosev <mlosev86@gmail.com>
2022-12-09 18:45:59 +0400

    fix test in safari

--- test/functional/fixtures/api/es-next/iframe-switching/testcafe-fixtures/message-event.js
+++ test/functional/fixtures/api/es-next/iframe-switching/testcafe-fixtures/message-event.js
@@ -4,12 +4,16 @@ fixture `Message Event`
     .page('http://localhost:3000/fixtures/api/es-next/iframe-switching/pages/message-event/index.html');
 
 test('test', async t => {
+    const receivedLog = Selector('#received-log').addCustomDOMProperties({
+        trimmedInnerText: el => el.innerText.replace('\n', ''),
+    });
+
     await t
         .click('#post-string-message')
         .click('#post-object-message')
-        .expect(Selector('#received-log').innerText).eql('"string message processed"\n{"object":"message","processed":true}')
+        .expect(receivedLog.trimmedInnerText).eql('"string message processed"{"object":"message","processed":true}')
         .switchToIframe('#cross-domain-iframe')
-        .expect(Selector('#received-log').innerText).eql('"string message"\n{"object":"message"}')
+        .expect(receivedLog.trimmedInnerText).eql('"string message"{"object":"message"}')
         .typeText('#input', 'Text')
         .expect(Selector('#input').value).eql('Text')
         .switchToMainWindow();
