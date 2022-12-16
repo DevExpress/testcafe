411d0797bed32474cc756c6a1536194880ed4e76
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-12-16 18:30:07 +0400

    test: rewrote to esm

--- test/functional/fixtures/api/es-next/request-hooks/common/mock-routes.js
+++ test/functional/fixtures/api/es-next/request-hooks/common/mock-routes.js
@@ -1,6 +1,8 @@
-module.exports = {
+const routes = {
     main:    'http://one-dummy-url.com',
     get:     'http://one-dummy-url.com/get',
     post:    'http://one-dummy-url.com/post',
     another: 'https://another-dummy-url.com',
 };
+
+export default routes;
