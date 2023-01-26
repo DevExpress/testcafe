31264da355b831e2928c0646da44c0e74d5a5d76
Ilya Afanasenko <fg1@mail.ru>
2023-01-26 16:58:22 +0400

    test: added required tests

--- test/functional/fixtures/ui/test.js
+++ test/functional/fixtures/ui/test.js
@@ -1,20 +1,67 @@
 const config = require('../../config');
 
-
 describe('TestCafe UI', () => {
-    it('Should display correct status', () => {
-        return runTests('./testcafe-fixtures/status-bar-test.js', 'Show status prefix', { assertionTimeout: 3000 });
+    describe('Status Bar', () => {
+        it('Should display correct status', () => {
+            return runTests('./testcafe-fixtures/status-bar-test.js', 'Show status prefix', { assertionTimeout: 3000 });
+        });
+
+        it('Hide elements when resizing the window', () => {
+            return runTests('./testcafe-fixtures/status-bar-test.js', 'Hide elements when resizing the window', { skip: ['android', 'ipad', 'iphone', 'edge', 'safari'] });
+        });
+
+        it('Should hide the status bar even if document was hidden during initialization (GH-7384)', function () {
+            // NOTE: the test needs direct access to the CDP client through the test controller
+            if (config.experimentalDebug)
+                this.skip();
+
+            return runTests('./testcafe-fixtures/status-bar-test.js', 'Hide status bar after mouse move', { only: ['chrome'] });
+        });
     });
 
-    it('Hide elements when resizing the window', () => {
-        return runTests('./testcafe-fixtures/status-bar-test.js', 'Hide elements when resizing the window', { skip: ['android', 'ipad', 'iphone', 'edge', 'safari'] });
-    });
+    describe('Selector Inspector', () => {
+        function runTest (testName) {
+            return runTests('./testcafe-fixtures/selector-inspector-test.js', testName);
+        }
 
-    it('Should hide the status bar even if document was hidden during initialization (GH-7384)', function () {
-        // NOTE: the test needs direct access to the CDP client through the test controller
-        if (config.experimentalDebug)
-            this.skip();
+        it('panel should be shown in debug mode', () => {
+            return runTest('Show panel');
+        });
 
-        return runTests('./testcafe-fixtures/status-bar-test.js', 'Hide status bar after mouse move', { only: ['chrome'] });
+        it('should hide TestCafe elements while piking', () => {
+            return runTest('Hide TestCafe element while picking');
+        });
+
+        it('should generate valid selector', () => {
+            return runTest('Generate selector');
+        });
+
+        it('should fill the selectors list with the generated selectors', () => {
+            return runTest('Fill the selectors list');
+        });
+
+        it('should indicate the correct number of elements matching the selector', () => {
+            return runTest('Indicate matching');
+        });
+
+        it('should indicate if the selector is invalid on input', () => {
+            return runTest('Indicate invalid');
+        });
+
+        it('should indicate that no matches on input', () => {
+            return runTest('Indicate no matching');
+        });
+
+        it('should highlight matches elements on input', () => {
+            return runTest('Highlight elements');
+        });
+
+        it('should place a selector selected from the list in the input field', () => {
+            return runTest('Select selector');
+        });
+
+        it('should copy selector', () => {
+            return runTest('Copy selector');
+        });
     });
 });
---
+++ test/functional/fixtures/ui/testcafe-fixtures/selector-inspector-test.js
@@ -0,0 +1,241 @@
+import { ClientFunction } from 'testcafe';
+
+fixture `Selector Inspector`
+    .clientScripts `../selector-inspector-utils.js`
+    .page `http://localhost:3000/fixtures/ui/pages/example.html`;
+
+test('Show panel', async t => {
+    await ClientFunction(() => {
+        window['%testCafeDriverInstance%'].selectorInspectorPanel.show = () => window.resumeTest();
+    })();
+
+    await t.debug();
+});
+
+test('Hide TestCafe element while picking', async t => {
+    await ClientFunction(() => {
+        const {
+            startPicking,
+            getShadowUIElements,
+            canBeShownInPicking,
+            isVisible,
+            resumeTest,
+        } = window;
+
+        startPicking().then(() => {
+            for (const child of getShadowUIElements()) {
+                if (!canBeShownInPicking(child) && isVisible(child))
+                    return;
+            }
+
+            resumeTest();
+        });
+    })();
+
+    await t.debug();
+});
+
+test('Generate selector', async t => {
+    await ClientFunction(() => {
+        const {
+            startPicking,
+            pickElement,
+            getSelectorInputValue,
+            resumeTest,
+        } = window;
+
+        startPicking()
+            .then(() => {
+                const target = document.querySelector('div.column:nth-child(2) > fieldset:nth-child(2) > legend:nth-child(1)');
+
+                pickElement(target);
+
+                return getSelectorInputValue();
+            })
+            .then(value => {
+                if (value === "Selector('#main-form legend').withText('Which TestCafe interface do you use:')")
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Fill the selectors list', async t => {
+    await ClientFunction(() => {
+        const {
+            startPicking,
+            pickElement,
+            expandSelectorsList,
+            getSelectorsListValues,
+            resumeTest,
+        } = window;
+
+        startPicking()
+            .then(() => {
+                const target = document.querySelector('div');
+
+                pickElement(target);
+
+                return expandSelectorsList();
+            })
+            .then(() => {
+                return getSelectorsListValues();
+            })
+            .then(selectorsValues => {
+                const isValidTestCafeSelectors = selectorsValues.every(value => value.startsWith('Selector('));
+
+                if (selectorsValues.length > 0 && isValidTestCafeSelectors)
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Indicate matching', async t => {
+    await ClientFunction(() => {
+        const { typeSelector, getMatchIndicatorInnerText, resumeTest } = window;
+
+        typeSelector('div')
+            .then(() => {
+                return getMatchIndicatorInnerText();
+            })
+            .then(text => {
+                if (text === 'Found: 19')
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Indicate invalid', async t => {
+    await ClientFunction(() => {
+        const { typeSelector, getMatchIndicatorInnerText, resumeTest } = window;
+
+        typeSelector('Selector(/.%')
+            .then(() => {
+                return getMatchIndicatorInnerText();
+            })
+            .then(text => {
+                if (text === 'Invalid Selector')
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Indicate no matching', async t => {
+    await ClientFunction(() => {
+        const { typeSelector, getMatchIndicatorInnerText, resumeTest } = window;
+
+        typeSelector('div > div > input[id="not-valid-id"]')
+            .then(() => {
+                return getMatchIndicatorInnerText();
+            })
+            .then(text => {
+                if (text === 'No Matching Elements')
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Highlight elements', async t => {
+    await ClientFunction(() => {
+        const {
+            typeSelector,
+            getElementFrames,
+            isElementsRectsEql,
+            resumeTest,
+        } = window;
+
+        const selector = 'input[id]';
+        const elements = document.querySelectorAll(selector);
+
+        if (!elements || elements.length < 2)
+            return;
+
+        typeSelector(selector)
+            .then(() => {
+                return getElementFrames();
+            })
+            .then(elementFrames => {
+                for (let i = 0; i < elements.length; i++) {
+                    if (!isElementsRectsEql(elements[i], elementFrames[i]))
+                        return;
+                }
+
+                resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Select selector', async t => {
+    await ClientFunction(() => {
+        const {
+            startPicking,
+            pickElement,
+            getGeneratedSelectors,
+            selectSelectorFromList,
+            getSelectorInputValue,
+            resumeTest,
+        } = window;
+
+        startPicking()
+            .then(() => {
+                const target = document.querySelector('#main-form > div > div.row > div.column.col-1 > fieldset:nth-child(2) > p:nth-child(3)');
+
+                pickElement(target);
+
+                const selectors = getGeneratedSelectors();
+
+                let promise = Promise.resolve();
+
+                for (let i = 0; i < selectors.length; i++) {
+                    promise = promise
+                        .then(() => {
+                            return selectSelectorFromList(i);
+                        })
+                        .then(() => {
+                            return getSelectorInputValue();
+                        })
+                        .then(selectorValue => {
+                            if (selectorValue !== selectors[i])
+                                throw 'Selector values do not match';
+                        });
+                }
+
+                return promise;
+            })
+            .then(() => {
+                resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
+
+test('Copy selector', async t => {
+    await ClientFunction(() => {
+        const { typeSelector, copySelector, resumeTest } = window;
+
+        const selector = "Selector('div').nth(2).find('input')";
+
+        typeSelector(selector)
+            .then(() => {
+                return copySelector();
+            })
+            .then(copiedSelector => {
+                if (copiedSelector === selector)
+                    resumeTest();
+            });
+    })();
+
+    await t.debug();
+});
