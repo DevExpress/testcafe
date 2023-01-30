f8ef0737ebe2dfd23774da983af00b4e3c62bcb5
Alex <alexikam@gmail.com>
2023-01-30 18:41:30 +0400

    basic http auth

--- src/test-run/request/send.ts
+++ src/test-run/request/send.ts
@@ -1,6 +1,5 @@
 import TestRun from '../index';
-import { DestinationRequest, sameOriginCheck } from 'testcafe-hammerhead';
-import { IncomingMessage } from 'http';
+import { IncomingHttpHeaders, IncomingMessage } from 'http';
 import { ExternalRequestOptions, ResponseOptions } from './interfaces';
 import { createRequestOptions } from './create-request-options';
 import { processResponseData } from './process-response-data';
@@ -10,13 +9,18 @@ import { CallsiteRecord } from 'callsite-record';
 import { castArray } from 'lodash';
 import { RUNTIME_ERRORS } from '../../errors/types';
 
+import {
+    DestinationRequest,
+    sameOriginCheck,
+    RequestOptions,
+} from 'testcafe-hammerhead';
+
 type StrictIncomingMessage = IncomingMessage & { statusCode: number; statusMessage: string };
 
-async function send (testRun: TestRun, options: ExternalRequestOptions, callsite: CallsiteRecord | null): Promise<ResponseOptions> {
-    const currentPageUrl = new URL(await testRun.getCurrentUrl());
-    const requestOptions = await createRequestOptions(currentPageUrl, testRun, options, callsite);
-    const request        = new DestinationRequest(requestOptions);
-    const dataWaiter     = new Promise<StrictIncomingMessage | string>(resolve => {
+export async function send (requestOptions: RequestOptions, rawResponse?: boolean): Promise<ResponseOptions | string> {
+    const request = new DestinationRequest(requestOptions);
+
+    const dataWaiter = new Promise<StrictIncomingMessage | string>(resolve => {
         request.on('response', (res: StrictIncomingMessage) => resolve(res));
         request.on('error', (err: Error) => resolve(err.message));
         request.on('fatalError', (message: string) => resolve(message));
@@ -25,15 +29,9 @@ async function send (testRun: TestRun, options: ExternalRequestOptions, callsite
     const data = await dataWaiter;
 
     if (typeof data === 'string')
-        throw new RequestRuntimeError(callsite, RUNTIME_ERRORS.requestRuntimeError, data.replace(/<.*?>/g, ''));
+        return data;
 
-    const setCookie  = data.headers[HTTP_HEADERS.setCookie];
-    const sameOrigin = !currentPageUrl.host || sameOriginCheck(currentPageUrl.href, requestOptions.url);
-
-    if (setCookie && (sameOrigin || options.withCredentials) )
-        testRun.session.cookies.copySyncCookies(castArray(setCookie).join(';'), currentPageUrl.href);
-
-    const body = await processResponseData(data, options.rawResponse);
+    const body = await processResponseData(data, rawResponse);
 
     return {
         status:     data.statusCode,
@@ -43,4 +41,21 @@ async function send (testRun: TestRun, options: ExternalRequestOptions, callsite
     };
 }
 
-export default send;
+export async function sendRequestThroughAPI (testRun: TestRun, options: ExternalRequestOptions, callsite: CallsiteRecord | null): Promise<ResponseOptions> {
+    const currentPageUrl = new URL(await testRun.getCurrentUrl());
+
+    const requestOptions = await createRequestOptions(currentPageUrl, testRun, options, callsite);
+
+    const data = await send(requestOptions, options.rawResponse);
+
+    if (typeof data === 'string')
+        throw new RequestRuntimeError(callsite, RUNTIME_ERRORS.requestRuntimeError, data.replace(/<.*?>/g, ''));
+
+    const setCookie  = (data.headers as IncomingHttpHeaders)[HTTP_HEADERS.setCookie];
+    const sameOrigin = !currentPageUrl.host || sameOriginCheck(currentPageUrl.href, requestOptions.url);
+
+    if (setCookie && (sameOrigin || options.withCredentials) )
+        testRun.session.cookies.copySyncCookies(castArray(setCookie).join(';'), currentPageUrl.href);
+
+    return data;
+}
