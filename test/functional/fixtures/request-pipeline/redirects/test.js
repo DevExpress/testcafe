782d1569d2e57b26b72a885ecc7c330093560a6c
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-03-02 00:14:22 +0400

    test

--- test/functional/fixtures/request-pipeline/redirects/test.js
+++ test/functional/fixtures/request-pipeline/redirects/test.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-describe.only('Redirects', () => {
+describe('Redirects', () => {
     it('Should correctly handle redirects', () => {
         return runTests('testcafe-fixtures/redirects.js', null, { only: 'chrome' });
     });
