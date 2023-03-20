6f9b2b3ba5f31ebcc9b1082b6cd62e475ba6ef6b
Alex <alexikam@gmail.com>
2023-03-20 13:10:34 +0400

    fix tests

--- test/functional/fixtures/regression/gh-7566/test.js
+++ test/functional/fixtures/regression/gh-7566/test.js
@@ -1,5 +1,7 @@
+const { onlyInProxyless } = require('../../../utils/skip-in');
+
 describe('[Regression](GH-7566)', function () {
-    it('Should has the "which" property equal to "0" for move events', function () {
+    onlyInProxyless('Should has the "which" property equal to "0" for move events', function () {
         return runTests('testcafe-fixtures/index.js');
     });
 });
