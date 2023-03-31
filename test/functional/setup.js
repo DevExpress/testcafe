3a3cbfb22d0c0ccd1f26fc048ba0ad4c88defcea
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-03-31 13:36:46 +0400

    test

--- test/functional/setup.js
+++ test/functional/setup.js
@@ -416,5 +416,14 @@ after(async function () {
 
     console.log(`file: setup.js -> line 393 -> browser closed;`); 
     global.wtf.dump();
+    
+    console.log('awaiting', new Date());
+
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    console.log('after awaiting', new Date());
+
+    global.wtf.dump();
+
 });
 
