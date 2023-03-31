ab3b94d66d9f79e29f7836c49fe59f7db66bf38d
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-03-31 18:26:50 +0400

    Revert "test"
    This reverts commit 5c4f41dacd6fabb047a4d3e908a292c560a8e539.

--- test/functional/setup.js
+++ test/functional/setup.js
@@ -409,8 +409,8 @@ after(async function () {
         console.log('after Promise.race');
         clearTimeout(timer);
     }
-        
-    await closeLocalBrowsers();
+    else
+        await closeLocalBrowsers();
         
     console.log(`file: setup.js:430 -> browsersInfo:`, browsersInfo);
     testRunTracker.clearTimers();
