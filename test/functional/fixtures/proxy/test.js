35c05241eb22c1b350b4d4d8bab9f168f36c2279
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-04-02 23:35:35 +0400

    test

--- test/functional/fixtures/proxy/test.js
+++ test/functional/fixtures/proxy/test.js
@@ -8,7 +8,7 @@ const TRUSTED_PROXY_URL     = os.hostname() + ':3004';
 const TRANSPARENT_PROXY_URL = os.hostname() + ':3005';
 const ERROR_PROXY_URL       = 'ERROR';
 
-describe.only('Using external proxy server', function () {
+describe('Using external proxy server', function () {
     it('Should open page via proxy server', function () {
         return runTests('testcafe-fixtures/index.test.js', null, { useProxy: TRANSPARENT_PROXY_URL });
     });
