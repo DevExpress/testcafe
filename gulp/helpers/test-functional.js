31104b6a09c76b8e09917ecd2d751066185f2d6f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-04-02 22:53:38 +0400

    test

--- gulp/helpers/test-functional.js
+++ gulp/helpers/test-functional.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
+const gulp           = require('gulp');
+const mocha          = require('gulp-mocha-simple');
 const { castArray }  = require('lodash');
 const getTimeout     = require('./get-timeout');
 const chai           = require('chai');
-const globby         = require('globby');
-const Mocha          = require('mocha');
 
 const {
     TESTS_GLOB,
@@ -20,15 +19,7 @@ function shouldAddTakeScreenshotTestGlob (glob) {
     return [TESTS_GLOB, DEBUG_GLOB_1].includes(glob);
 }
 
-function getGroupOfTests (tests, groupNumber, groupsCount) {
-    const testFragmentSize       = Math.ceil(tests.length / groupsCount);
-    const testFragmentStartIndex = testFragmentSize * (groupNumber - 1);
-    const testFragmentEndIndex   = testFragmentSize * groupNumber;
-
-    return tests.slice(testFragmentStartIndex, testFragmentEndIndex);
-}
-
-module.exports = async function testFunctional (src, testingEnvironmentName, { experimentalDebug, isProxyless } = {}) {
+module.exports = function testFunctional (src, testingEnvironmentName, { experimentalDebug, isProxyless } = {}) {
     process.env.TESTING_ENVIRONMENT       = testingEnvironmentName;
     process.env.BROWSERSTACK_USE_AUTOMATE = 1;
 
@@ -47,42 +38,17 @@ module.exports = async function testFunctional (src, testingEnvironmentName, { e
     if (shouldAddTakeScreenshotTestGlob(src))
         tests = SCREENSHOT_TESTS_GLOB.concat(tests);
 
-    tests = await globby(tests);
-
-    if (process.env.TEST_GROUPS_COUNT && process.env.TEST_GROUP_NUMBER)
-        tests = getGroupOfTests(tests, process.env.TEST_GROUP_NUMBER, process.env.TEST_GROUPS_COUNT);
-
     tests.unshift(SETUP_TESTS_GLOB);
 
     const opts = {
-        // reporter: 'mocha-reporter-spec-with-retries',
-        timeout:  getTimeout(2 * 60 * 1000),
+        reporter: 'mocha-reporter-spec-with-retries',
+        timeout:  getTimeout(3 * 60 * 1000),
     };
 
     if (process.env.RETRY_FAILED_TESTS === 'true')
         opts.retries = RETRY_TEST_RUN_COUNT;
 
-    const mocha = new Mocha(opts);
-
-    tests.forEach(file => {
-        mocha.addFile(file);
-    });
-
-    await new Promise((resolve, reject) => {
-        mocha.run((code) => {
-            console.log(`file: test-functional.js:72 -> mocha.run -> code:`, code);
-            if (code)
-                reject();
-
-            resolve();
-        });
-    });
-
-    console.log('Mocha was finished');
-    global.wtf.dump();
-    global.testRunTracker.clearTimers();
-    console.log('After clearing timeouts');
-    global.wtf.dump();
-
-    return new Promise((resolve) => resolve());
+    return gulp
+        .src(tests)
+        .pipe(mocha(opts));
 };
