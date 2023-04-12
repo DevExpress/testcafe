268005f04871cc22f654b7664e053e6a136fea90
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-04-12 10:10:33 +0400

    Revert "test"
    This reverts commit 39e908d2121d8072e155b60c6208543c637e592f.

--- test/functional/fixtures/app-command/test.js
+++ test/functional/fixtures/app-command/test.js
@@ -2,15 +2,14 @@ const { expect }     = require('chai');
 const { isFreePort } = require('endpoint-utils');
 const delay          = require('../../../../lib/utils/delay');
 
-/* eslint-disable */
-describe.only('App command', function () {
-    it.only('Should fail task if app fails', function () {
+describe('App command', function () {
+    it('Should fail task if app fails', function () {
         return runTests('./testcafe-fixtures/app-command-test.js', 'Wait', {
             shouldFail: true,
             appCommand: 'node test/functional/fixtures/app-command/failing-app.js',
         })
             .catch(function (err) {
-                expect(err.message).eql('fail');
+                expect(err.message).contains('The web application failed with the following error:\n\nError: Command failed');
             });
     });
 
