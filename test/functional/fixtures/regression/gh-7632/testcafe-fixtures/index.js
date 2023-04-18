a7832b14ac94e505d7cc7bc31c54c5f60bdd458c
Alex <alexikam@gmail.com>
2023-04-18 15:51:13 +0400

    hammerhead-isotope

---
+++ test/functional/fixtures/regression/gh-7632/testcafe-fixtures/index.js
@@ -0,0 +1,7 @@
+fixture `esotope`;
+
+test.page('http://localhost:3000/fixtures/regression/gh-7632/pages/test1.html')('PrivateIdentifier', () => {
+});
+
+test.page('http://localhost:3000/fixtures/regression/gh-7632/pages/test2.html')('PropertyDefinition', () => {
+});
