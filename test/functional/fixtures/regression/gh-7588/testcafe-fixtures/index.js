7716c4aa2c7327b708441f3d4b39f235a932184b
Alex <alexikam@gmail.com>
2023-04-19 15:26:01 +0400

    fix test in debug mode

--- test/functional/fixtures/regression/gh-7588/testcafe-fixtures/index.js
+++ test/functional/fixtures/regression/gh-7588/testcafe-fixtures/index.js
@@ -11,8 +11,7 @@ const logger = RequestLogger(
 fixture('Request body encoding in Native Automation in RequestLogger')
     .page('http://localhost:3000/fixtures/regression/gh-7588/pages/index.html');
 
-test('Request body encoding in Native Automation in RequestLogger', async t => {
-    await t.addRequestHooks(logger);
+test.requestHooks(logger)('Request body encoding in Native Automation in RequestLogger', async t => {
     await t.click('button');
 
     await t.expect(logger.requests[0].request.body).eql('{"key":"value"}');
