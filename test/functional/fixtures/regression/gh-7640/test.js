381eaa14734b7cb9c4bc92631b1b9309e62f55f9
Alex <alexikam@gmail.com>
2023-04-26 13:34:24 +0400

    only in native automation

--- test/functional/fixtures/regression/gh-7640/test.js
+++ test/functional/fixtures/regression/gh-7640/test.js
@@ -1,5 +1,7 @@
+const { onlyInNativeAutomation } = require('../../../utils/skip-in');
+
 describe('Redirect on the Request Hook', function () {
-    it('Redirect on the Request Hook', function () {
+    onlyInNativeAutomation('Redirect on the Request Hook', function () {
         return runTests('testcafe-fixtures/index.js');
     });
 });
