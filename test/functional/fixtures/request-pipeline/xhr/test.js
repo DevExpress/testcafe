cb24b6c43711e25c03585302b0369b5e84d0416f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-05-15 16:44:56 +0300

    test: added xhr tests

---
+++ test/functional/fixtures/request-pipeline/xhr/test.js
@@ -0,0 +1,18 @@
+const { errorInEachBrowserContains } = require('../../../assertion-helper.js');
+
+describe('XHR', () => {
+    it('Should keep header if request was reopened', () => {
+        return runTests('./testcafe-fixtures/index.js', 'Click test header button');
+    });
+
+    it('Should authorization prefix for authorization header', () => {
+        return runTests('./testcafe-fixtures/index.js', 'Click auth header button');
+    });
+
+    it('Should wait for xhr-requests after an action', () => {
+        return runTests('./testcafe-fixtures/index.js', 'Click delay button', { shouldFail: true })
+            .catch(errs => {
+                errorInEachBrowserContains(errs, 'Xhr requests are finished', 0);
+            });
+    });
+});
