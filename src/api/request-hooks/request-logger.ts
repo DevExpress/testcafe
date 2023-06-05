af616979bc18955402b74f954ab63d80fef36513
Alex Kamaev <alexikam@gmail.com>
2023-06-05 17:23:13 +0400

    try

--- src/api/request-hooks/request-logger.ts
+++ src/api/request-hooks/request-logger.ts
@@ -18,6 +18,7 @@ import {
 } from './interfaces';
 
 import { Dictionary } from '../../configuration/interfaces';
+import lowercaseObjectKeys from '../../utils/lowercase-object-keys';
 
 
 const DEFAULT_OPTIONS: RequestHookLogOptions = {
@@ -93,7 +94,7 @@ class RequestLoggerImplementation extends RequestHook {
         };
 
         if (this._options.logRequestHeaders)
-            loggedReq.request.headers = Object.assign({}, event._requestInfo.headers);
+            loggedReq.request.headers = Object.assign({}, lowercaseObjectKeys(event._requestInfo.headers as Dictionary<string>));
 
         if (this._options.logRequestBody)
             loggedReq.request.body = this._options.stringifyRequestBody ? event._requestInfo.body.toString() : event._requestInfo.body;
