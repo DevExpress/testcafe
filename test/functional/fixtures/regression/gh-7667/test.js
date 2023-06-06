610018808ca6b724544e252052b992cd0d96b83e
aleks-pro <prokhor.aleks@gmail.com>
2023-06-06 13:05:50 +0300

    lint fixed

--- test/functional/fixtures/regression/gh-7667/test.js
+++ test/functional/fixtures/regression/gh-7667/test.js
@@ -1,4 +1,4 @@
-describe.only('[Regression](GH-7667)', function () {
+describe('[Regression](GH-7667)', function () {
     it('Should select and remove text from input', function () {
         return runTests('testcafe-fixtures/index.js');
     });
