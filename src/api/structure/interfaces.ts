DevExpress/testcafe

commit 5f25d05fede7a3d39ac807e94d54601bfbf7e7d0

feat: add test and fixture info to the TestController object and fixture hooks(closes #2826) (#7736)
## Purpose
#2826 

Currently, you cannot access any test/fixture information inside test
and fixture hooks using public API. Fixture hooks cannot access this
info even with private API.

## Approach
Add corresponding members fixture: FixtureInfo, test:TestInfo to the
TestController instance. Add the fixture info as a second argument to
the fixture specific hooks.

## References
#2826 

## API
```ts
type Metadata = Record<string, string>;

interface TestInfo {
    name: string;
    meta: Metadata;
}

interface FixtureInfo {
    name: string;
    meta: Metadata;
    path: string;
}
```

```ts
fixture`testFixture`
    .before(async (ctx, info: FixtureInfo) => {})
    .after(async (ctx, info: FixtureInfo) => {})
    .beforeEach(async t => {
        const testInfo: TestInfo = t.test
        const fixtureInfo: FixtureInfo = t.fixture;
    })
    .afterEach(async t => {
        const testInfo: TestInfo = t.test
        const fixtureInfo: FixtureInfo = t.fixture;
    })

test('SomeTest', async t => {})
    .before(async t => {
        const testInfo: TestInfo = t.test
        const fixtureInfo: FixtureInfo = t.fixture;
    })
    .after(async t => {
        const testInfo: TestInfo = t.test
        const fixtureInfo: FixtureInfo = t.fixture;
    })
```

## Pre-Merge TODO
- [x] Write tests for your proposed changes
- [x] Add typings 
- [ ] Make sure that existing tests do not fail

ts-defs-src/test-api/structure.d.ts
@@ -3,6 +3,7 @@
 /// <reference path="client-script.d.ts" />
 /// <reference path="test-controller.d.ts" />
 /// <reference path="skip-js-errors-options.d.ts" />
+/// <reference path="test-info.d.ts" />
 // {{/allowReferences}}
 
 interface HTTPAuthCredentials {
@@ -65,16 +66,18 @@ interface FixtureFn {
      * Specifies the fixture hook that is executed before the start of the first test in the fixture.
      *
      * @param fn - An asynchronous hook function that contains initialization or clean-up code.
-     * @param fn `ctx` - A fixture context object used to share variables between fixture hooks and test code.
+     * @param fn `ctx` - An object that you can use to store variables. Accessible in both fixture hooks and test code.
+     * @param fn `info` - An object that contains information about the fixture: its name, metadata and path.
      */
-    before(fn: (ctx: {[key: string]: any}) => Promise<any>): this;
+    before(fn: (ctx: {[key: string]: any}, info: FixtureInfo) => Promise<any>): this;
     /**
      * Specifies the fixture hook that is executed after the end of the last test in the fixture.
      *
      * @param fn - An asynchronous hook function that contains initialization or clean-up code.
-     * @param fn `ctx` - A fixture context object used to share variables between fixture hooks and test code.
+     * @param fn `ctx` - An object that you can use to store variables. Accessible in both fixture hooks and test code.
+     * @param fn `info` - An object that contains information about the fixture: its name, metadata and path.
      */
-    after(fn: (ctx: {[key: string]: any}) => Promise<any>): this;
+    after(fn: (ctx: {[key: string]: any}, info: FixtureInfo) => Promise<any>): this;
     /**
      * Specifies the hook that is executed on the start of each test in the fixture.
      *
@@ -119,7 +122,7 @@ interface FixtureFn {
      *
      * @param data - Key-value pairs
      */
-    meta(data: object): this;
+    meta(data: Metadata): this;
     /**
      * Attaches hooks to all tests in the fixture
      *
@@ -207,7 +210,7 @@ interface TestFn {
      *
      * @param data - Key-value pairs
      */
-    meta(data: object): this;
+    meta(data: Metadata): this;
     /**
      * Attaches hooks to the test
      *

ts-defs-src/test-api/test-info.d.ts
@@ -1,0 +1,15 @@
+// Test Info
+//----------------------------------------------------------------------------------------------------------------------
+
+type Metadata = Record<string, string>;
+
+interface TestInfo {
+    name: string;
+    meta: Metadata;
+}
+
+interface FixtureInfo {
+    name: string;
+    meta: Metadata;
+    path: string;
+}

src/api/test-controller/index.js
@@ -85,6 +85,7 @@ import { RequestRuntimeError } from '../../errors/runtime';
 import { RUNTIME_ERRORS } from '../../errors/types';
 import CustomActions from './custom-actions';
 import delegatedAPI from './delegated-api';
+import { getFixtureInfo, getTestInfo } from '../../utils/get-test-and-fixture-info';
 
 const originalThen = Promise.resolve().then;
 
@@ -164,7 +165,7 @@ export default class TestController {
     enqueueCommand (CmdCtor, cmdArgs, validateCommandFn, callsite) {
         callsite = callsite || getCallsiteForMethod(CmdCtor.methodName);
 
-        const command  = this._createCommand(CmdCtor, cmdArgs, callsite);
+        const command = this._createCommand(CmdCtor, cmdArgs, callsite);
 
         if (typeof validateCommandFn === 'function')
             validateCommandFn(this, command, callsite);
@@ -227,6 +228,14 @@ export default class TestController {
         return this._customActions || new CustomActions(this, this.testRun.opts.customActions);
     }
 
+    _test$getter () {
+        return getTestInfo(this.testRun);
+    }
+
+    _fixture$getter () {
+        return getFixtureInfo(this.testRun);
+    }
+
     [delegatedAPI(DispatchEventCommand.methodName)] (selector, eventName, options = {}) {
         return this.enqueueCommand(DispatchEventCommand, { selector, eventName, options, relatedTarget: options.relatedTarget });
     }
@@ -275,7 +284,7 @@ export default class TestController {
 
     _createRequestFunction (bindOptions = {}) {
         const controller = this;
-        const callsite = getCallsiteForMethod(RequestCommand.methodName);
+        const callsite   = getCallsiteForMethod(RequestCommand.methodName);
 
         if (!controller.testRun)
             throw new RequestRuntimeError(callsite, RUNTIME_ERRORS.requestCannotResolveTestRun);
@@ -496,7 +505,7 @@ export default class TestController {
     }
 
     [delegatedAPI(CloseWindowCommand.methodName)] (window) {
-        const windowId      = window?.id || null;
+        const windowId = window?.id || null;
 
         this._validateMultipleWindowCommand(CloseWindowCommand.methodName);
 

src/runner/fixture-hook-controller.ts
@@ -4,6 +4,7 @@ import Test from '../api/structure/test';
 import Fixture from '../api/structure/fixture';
 import TestRun from '../test-run';
 import executeFnWithTimeout from '../utils/execute-fn-with-timeout';
+import { getFixtureInfo } from '../utils/get-test-and-fixture-info';
 
 interface FixtureState {
     started: boolean;
@@ -67,7 +68,7 @@ export default class FixtureHookController {
         item.runningFixtureBeforeHook = true;
 
         try {
-            await executeFnWithTimeout(fn, testRun.executionTimeout, item.fixtureCtx);
+            await executeFnWithTimeout(fn, testRun.executionTimeout, item.fixtureCtx, getFixtureInfo(testRun));
         }
         catch (err) {
             item.fixtureBeforeHookErr = processTestFnError(err);
@@ -85,7 +86,7 @@ export default class FixtureHookController {
         testRun.phase = TEST_RUN_PHASE.inFixtureAfterHook;
 
         try {
-            await executeFnWithTimeout(fn, testRun.executionTimeout, item.fixtureCtx);
+            await executeFnWithTimeout(fn, testRun.executionTimeout, item.fixtureCtx, getFixtureInfo(testRun));
         }
         catch (err) {
             testRun.addError(processTestFnError(err));

src/utils/get-test-and-fixture-info.ts
@@ -1,0 +1,16 @@
+import TestRun from '../test-run';
+
+export function getTestInfo (testRun: TestRun): object {
+    return {
+        name: testRun.test.name,
+        meta: { ...testRun.test.meta },
+    };
+}
+
+export function getFixtureInfo (testRun: TestRun): object {
+    return {
+        name: testRun.test.fixture?.name,
+        path: testRun.test.fixture?.path,
+        meta: { ...testRun.test.fixture?.meta },
+    };
+}

test/functional/fixtures/api/es-next/test-info/pages/index.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+</body>
+</html>

test/functional/fixtures/api/es-next/test-info/test.js
@@ -1,0 +1,38 @@
+const { expect } = require('chai');
+const path       = require('path');
+
+describe('[API] Test Info', () => {
+    it('Should pass correct test and fixture info to fixture hooks', () => {
+        return runTests('./testcafe-fixtures/fixture-hooks.js', 'Fixture hooks');
+    });
+    it('Should pass correct test and fixture info to test and its hooks', () => {
+        return runTests('./testcafe-fixtures/test-hooks.js', 'Test hooks');
+    });
+    it('Should pass correct test and fixture info to runner hooks', () => {
+        const fixtureInfo = {
+            name: 'FixtureName2',
+            meta: { fixtureMeta: 'v' },
+            path: path.resolve('./test/functional/fixtures/api/es-next/test-info/testcafe-fixtures/runner-hooks.js'),
+        };
+        const testInfo    = {
+            name: 'Runner hooks',
+            meta: { testMeta: 'v' },
+        };
+
+        const expectFixtureHookInfo = (_, info) => expect(info).eql(fixtureInfo);
+        const expectTestHookInfo    = async t => await t.expect(t.fixture).eql(fixtureInfo).expect(t.test).eql(testInfo);
+
+        return runTests('./testcafe-fixtures/runner-hooks.js', 'Runner hooks', {
+            hooks: {
+                fixture: {
+                    before: expectFixtureHookInfo,
+                    after:  expectFixtureHookInfo,
+                },
+                test: {
+                    before: expectTestHookInfo,
+                    after:  expectTestHookInfo,
+                },
+            },
+        });
+    });
+});

test/functional/fixtures/api/es-next/test-info/testcafe-fixtures/fixture-hooks.js
@@ -1,0 +1,25 @@
+import { expect } from 'chai';
+import path from 'path';
+
+const fixtureInfo = {
+    name: 'FixtureName',
+    meta: { fixtureMeta: 'v' },
+    path: path.resolve('./test/functional/fixtures/api/es-next/test-info/testcafe-fixtures/fixture-hooks.js'),
+};
+const testInfo    = {
+    name: 'Fixture hooks',
+    meta: { testMeta: 'v' },
+};
+
+const expectTestHookInfo    = async t => await t.expect(t.fixture).eql(fixtureInfo).expect(t.test).eql(testInfo);
+const expectFixtureHookInfo = (_, info) => expect(info).eql(fixtureInfo);
+
+fixture`FixtureName`
+    .meta({ fixtureMeta: 'v' })
+    .page`http://localhost:3000/fixtures/api/es-next/test-info/pages/index.html`
+    .before(expectFixtureHookInfo)
+    .after(expectFixtureHookInfo)
+    .beforeEach(expectTestHookInfo)
+    .afterEach(expectTestHookInfo);
+
+test.meta({ 'testMeta': 'v' })('Fixture hooks', expectTestHookInfo);

commit 21ffed757c20861ac796cf558f6312b9297cbaba

fix: meta typings regression (#7804)
## Purpose
Regression after #7736

## Approach
_Describe how your changes address the issue or implement the desired
functionality in as much detail as possible._

## References
_Provide a link to the existing issue(s), if any._

## Pre-Merge TODO
- [ ] Write tests for your proposed changes
- [ ] Make sure that existing tests do not fail

src/api/structure/interfaces.ts
@@ -1,7 +1,7 @@
 import { Dictionary } from '../../configuration/interfaces';
 import TestTimeout from './test-timeout';
 
-export type Metadata = Dictionary<string>;
+export type Metadata = Dictionary<unknown>;
 
 export interface AuthCredentials {
     username: string;

src/reporter/index.ts
@@ -76,7 +76,7 @@ interface FixtureInfo {
     id: string;
     name: string | null;
     path: string;
-    meta: Dictionary<string>;
+    meta: Metadata;
 }
 
 interface BrowserRunInfo extends Browser {

ts-defs-src/test-api/structure.d.ts
@@ -116,7 +116,7 @@ interface FixtureFn {
      * @param key - The name of the metadata entry
      * @param value - The value of the metadata entry
      */
-    meta(key: string, value: string): this;
+    meta(key: string, value: unknown): this;
     /**
      * Specifies the additional information for all tests in the fixture. This information can be used in reports.
      *
@@ -204,7 +204,7 @@ interface TestFn {
      * @param key - The name of the metadata entry
      * @param value - The value of the metadata entry
      */
-    meta(key: string, value: string): this;
+    meta(key: string, value: unknown): this;
     /**
      * Specifies the additional information for the test. This information can be used in reports.
      *

ts-defs-src/test-api/test-info.d.ts
@@ -1,7 +1,7 @@
 // Test Info
 //----------------------------------------------------------------------------------------------------------------------
 
-type Metadata = Record<string, string>;
+type Metadata = Record<string, unknown>;
 
 interface TestInfo {
     name: string;