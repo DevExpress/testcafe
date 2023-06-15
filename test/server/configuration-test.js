DevExpress/testcafe

commit 0e377575b841f030b7605c47c3d06ca064ab77c6

fix: throw error and provide original error details to read/parse config errors and attach reporter error(closes #7208, #6437)  (#7759)
## Purpose
#7208
#6437

When an error occurs during reading the config file you need to know
more details about this error. Especially in JS config file.
The same is for the require('testcafe-reporter-name'). Or even custom
reporter code.

## Approach
Provide original error message and formatted stack.

## Breaking Change
**Old behavior:** 
Config file reading or parsing error doesn't stop the test run and only
a warning is shown.

**New behavior:** 
Config file reading or parsing error stops the test run and a formatted
error message is shown.

## References
#7208
#6437

## Pre-Merge TODO
- [x] Write tests for your proposed changes
- [x] Make sure that existing tests do not fail

src/errors/runtime/index.js
@@ -1,6 +1,6 @@
 import TEMPLATES from './templates';
 import createStackFilter from '../create-stack-filter';
-import { getCallsiteForMethod } from '../get-callsite';
+import { getCallsiteForError, getCallsiteForMethod } from '../get-callsite';
 import renderTemplate from '../../utils/render-template';
 import renderCallsiteSync from '../../utils/render-callsite-sync';
 import { RUNTIME_ERRORS } from '../types';
@@ -8,8 +8,19 @@ import getRenderers from '../../utils/get-renderes';
 import util from 'util';
 import semver from 'semver';
 import { removePreventModuleCachingSuffix } from '../test-run/utils';
+import REPORTER_MODULE_PREFIX from '../../reporter/module-prefix';
 
-const ERROR_SEPARATOR = '\n\n';
+const ERROR_SEPARATOR        = '\n\n';
+const NO_STACK_AVAILABLE_MSG = 'No stack trace available for this error';
+const MODULE_NOT_FOUND_CODE  = 'MODULE_NOT_FOUND';
+
+function formatErrorWithCallsite (error) {
+    const callsite         = getCallsiteForError(error);
+    const stackFilter      = createStackFilter();
+    const formattedMessage = callsite?.renderSync({ stackFilter }) || NO_STACK_AVAILABLE_MSG;
+
+    return `${error.message}\n\n${formattedMessage}`;
+}
 
 class ProcessTemplateInstruction {
     constructor (processFn) {
@@ -69,7 +80,7 @@ export class APIError extends Error {
         if (typeof callsite === 'object')
             this.callsite = callsite;
         else
-            this.callsite   = getCallsiteForMethod(callsite);
+            this.callsite = getCallsiteForMethod(callsite);
 
         // NOTE: We need property getters here because callsite can be replaced by an external code.
         // See https://github.com/DevExpress/testcafe/blob/v1.0.0/src/compiler/test-file/formats/raw.js#L22
@@ -187,3 +198,53 @@ export class ImportESMInCommonJSError extends GeneralError {
         return esModule;
     }
 }
+
+export class ReadConfigFileError extends GeneralError {
+    constructor (code, originalError, filePath, renderCallsite) {
+        super(code, filePath, ReadConfigFileError._getFormattedMessage(originalError, renderCallsite));
+    }
+
+    static _getFormattedMessage (originalError, renderCallsite) {
+        if (!renderCallsite)
+            return originalError.message;
+
+        return formatErrorWithCallsite(originalError);
+    }
+}
+
+export class LoadReporterError extends GeneralError {
+    constructor (originalError, reporterFullName) {
+        const reporterShortName      = LoadReporterError._ensureShortName(reporterFullName);
+        const formattedOriginalError = LoadReporterError._getFormattedMessage(originalError, reporterFullName);
+
+        super(RUNTIME_ERRORS.cannotFindReporterForAlias, reporterShortName, formattedOriginalError);
+    }
+
+    static _ensureShortName (name) {
+        if (name && name.startsWith(REPORTER_MODULE_PREFIX))
+            return name.replace(REPORTER_MODULE_PREFIX, '');
+
+        return name;
+    }
+
+    static _getReporterModuleNotFoundMessage (reporterModuleName) {
+        return `Cannot find module "${reporterModuleName}"`;
+    }
+
+    static _getFormattedMessage (originalError, reporterFullName) {
+        const isModuleNotFoundError = originalError.code && originalError.code === MODULE_NOT_FOUND_CODE;
+
+        if (!isModuleNotFoundError)
+            return formatErrorWithCallsite(originalError);
+
+        // NOTE: ModuleNotFound error "message" property have the following pattern <message>\nRequire stack:\n<line1>\n<line2>
+        // We don't need to show require stack if error was produced by require('testcafe-reporter-<name>') call. If it wasn't, we need to show full require stack
+        const errorText = originalError.message.split('\n')[0];
+
+        if (errorText.includes(reporterFullName))
+            return LoadReporterError._getReporterModuleNotFoundMessage(reporterFullName);
+
+        return formatErrorWithCallsite(originalError);
+    }
+}
+

test/server/configuration-test.js
@@ -101,16 +101,22 @@ describe('TestCafeConfiguration', function () {
             describe('Exists', () => {
                 it('Config is not well-formed', async () => {
                     const filePath = testCafeConfiguration.defaultPaths[jsonConfigIndex];
+                    let message     = '';
 
                     fs.writeFileSync(filePath, '{');
-                    consoleWrapper.wrap();
 
-                    await testCafeConfiguration.init();
+                    try {
+                        await testCafeConfiguration.init();
+                    }
+                    catch (err) {
+                        message = err.message;
+                    }
 
-                    consoleWrapper.unwrap();
-
-                    expect(testCafeConfiguration.getOption('hostname')).eql(void 0);
-                    expect(consoleWrapper.messages.log).contains(`Failed to parse the '${testCafeConfiguration.defaultPaths[jsonConfigIndex]}' file.`);
+                    expect(message).eql(
+                        `Failed to parse the "${filePath}" configuration file. \n\n` +
+                        `This file is not a well-formed JSON file. Error details:\n\n` +
+                        `JSON5: invalid end of input at 1:2`
+                    );
                 });
 
                 it('Options', () => {
@@ -671,16 +677,24 @@ describe('TypeScriptConfiguration', function () {
     });
 
     it('Config is not well-formed', async () => {
+        let message     = '';
+
         fs.writeFileSync(tsConfigPath, '{');
-        consoleWrapper.wrap();
 
-        await typeScriptConfiguration.init();
+        try {
+            await typeScriptConfiguration.init();
+        }
+        catch (err) {
+            message = err.message;
+        }
 
-        consoleWrapper.unwrap();
         fs.unlinkSync(tsConfigPath);
 
-        expect(typeScriptConfiguration.getOption('hostname')).eql(void 0);
-        expect(consoleWrapper.messages.log).contains(`Failed to parse the '${typeScriptConfiguration.filePath}' file.`);
+        expect(message).eql(
+            `Failed to parse the "${typeScriptConfiguration.filePath}" configuration file. \n\n` +
+            `This file is not a well-formed JSON file. Error details:\n\n` +
+            `JSON5: invalid end of input at 1:2`
+        );
     });
 
     describe('With configuration file', () => {

test/server/runner-test.js
@@ -5,6 +5,7 @@ const fs                      = require('fs');
 const del                     = require('del');
 const request                 = require('request');
 const { times, uniqBy }       = require('lodash');
+const stripAnsi               = require('strip-ansi');
 const consoleWrapper          = require('./helpers/console-wrapper');
 const isAlpine                = require('./helpers/is-alpine');
 const createTestCafe          = require('../../lib/');
@@ -20,6 +21,7 @@ const { createReporter }      = require('../functional/utils/reporter');
 const proxyquire              = require('proxyquire');
 const BrowserConnectionStatus = require('../../lib/browser/connection/status');
 const TestCafeConfiguration   = require('../../lib/configuration/testcafe-configuration');
+const fakeReporterWithError   = require('./data/reporter/fake-reporter-with-error');
 
 const {
     browserConnectionGatewayMock,
@@ -154,8 +156,35 @@ describe('Runner', () => {
                     throw new Error('Promise rejection expected');
                 })
                 .catch(err => {
-                    expect(err.message).eql('The "reporter42" reporter does not exist. ' +
-                                            'Check the reporter parameter for errors.');
+                    expect(err.message).eql('An error occurred while loading the "reporter42" reporter. Please check the reporter parameter for errors. Error details:' +
+                                            '\n\n' +
+                                            'Cannot find module "testcafe-reporter-reporter42"');
+                });
+        });
+
+        it('Should raise an error if reporter code contains an error', () => {
+            return runner
+                .browsers(connection)
+                .reporter(fakeReporterWithError)
+                .src('test/server/data/test-suites/basic/testfile2.js')
+                .run()
+                .then(() => {
+                    throw new Error('Promise rejection expected');
+                })
+                .catch(err => {
+                    expect(stripAnsi(err.message)).eql(
+                        'An error occurred while loading the "fakeReporter" reporter. Please check the reporter parameter for errors. Error details:' +
+                             '\n\n' +
+                             'undefProp is not defined' +
+                             '\n\n' +
+                             '   1 |module.exports = function fakeReporter() {\n' +
+                             '   2 |    return {\n' +
+                             ' > 3 |        someProp: undefProp\n' +
+                             '   4 |    }\n' +
+                             '   5 |}\n' +
+                             '   6 |\n\n' +
+                             `   at fakeReporter (${path.resolve('./test/server/data/reporter/fake-reporter-with-error.js')}:3:19)`
+                    );
                 });
         });
 

src/configuration/configuration-base.ts
@@ -18,6 +18,8 @@ import WARNING_MESSAGES from '../notifications/warning-message';
 import log from '../cli/log';
 import { Dictionary } from './interfaces';
 import Extensions from './formats';
+import { ReadConfigFileError } from '../errors/runtime';
+import { RUNTIME_ERRORS } from '../errors/types';
 
 const DEBUG_LOGGER = debug('testcafe:configuration');
 
@@ -48,13 +50,13 @@ export default class Configuration {
         log.write(message);
     }
 
-    private static _showWarningForError (error: Error, warningTemplate: string, ...args: TemplateArguments): void {
-        const message = renderTemplate(warningTemplate, ...args);
+    private static _throwReadConfigError (code: string, error: Error, path: string, renderCallsite: boolean): void {
+        const readConfigError = new ReadConfigFileError(code, error, path, renderCallsite);
 
-        Configuration._showConsoleWarning(message);
-
-        DEBUG_LOGGER(message);
+        DEBUG_LOGGER(readConfigError.message);
         DEBUG_LOGGER(error);
+
+        throw readConfigError;
     }
 
     private static _resolveFilePath (path: string | null): string | null {
@@ -220,7 +222,7 @@ export default class Configuration {
                 return require(filePath);
             }
             catch (error: any) {
-                Configuration._showWarningForError(error, WARNING_MESSAGES.cannotReadConfigFile, filePath);
+                Configuration._throwReadConfigError(RUNTIME_ERRORS.cannotReadConfigFile, error, filePath, true);
             }
         }
 
@@ -232,7 +234,7 @@ export default class Configuration {
             return await readFile(filePath);
         }
         catch (error: any) {
-            Configuration._showWarningForError(error, WARNING_MESSAGES.cannotReadConfigFile, filePath);
+            Configuration._throwReadConfigError(RUNTIME_ERRORS.cannotReadConfigFile, error, filePath || '', false);
         }
 
         return null;
@@ -243,7 +245,7 @@ export default class Configuration {
             return JSON5.parse(configurationFileContent.toString());
         }
         catch (error: any) {
-            Configuration._showWarningForError(error, WARNING_MESSAGES.cannotParseConfigFile, filePath);
+            Configuration._throwReadConfigError(RUNTIME_ERRORS.cannotParseConfigFile, error, filePath || '', false);
         }
 
         return null;

src/errors/runtime/templates.js
@@ -35,8 +35,6 @@ export default {
 
     [RUNTIME_ERRORS.noTestsToRunDueFiltering]: 'No tests match your filter.\n' +
                                                `See ${DOCUMENTATION_LINKS.FILTER_SETTINGS}.`,
-
-    [RUNTIME_ERRORS.cannotFindReporterForAlias]:                         'The "{name}" reporter does not exist. Check the reporter parameter for errors.',
     [RUNTIME_ERRORS.multipleSameStreamReporters]:                        'Reporters cannot share output streams. The following reporters interfere with one another: "{reporters}".',
     [RUNTIME_ERRORS.optionValueIsNotValidRegExp]:                        'The "{optionName}" option does not contain a valid regular expression.',
     [RUNTIME_ERRORS.optionValueIsNotValidKeyValue]:                      'The "{optionName}" option does not contain a valid key-value pair.',
@@ -97,6 +95,18 @@ export default {
                                        '* add the path of the FFmpeg installation directory to the PATH environment variable,\n' +
                                        '* specify the path of the FFmpeg executable in the FFMPEG_PATH environment variable or the ffmpegPath option,\n' +
                                        '* install the @ffmpeg-installer/ffmpeg npm package.',
+
+    [RUNTIME_ERRORS.cannotReadConfigFile]: 'An error has occurred while reading the "{path}" configuration file. Error details:\n' +
+                                           '\n' +
+                                           '{err}',
+
+    [RUNTIME_ERRORS.cannotParseConfigFile]: 'Failed to parse the "{path}" configuration file. \n\nThis file is not a well-formed JSON file. Error details:\n' +
+                                           '\n' +
+                                           '{err}',
+
+    [RUNTIME_ERRORS.cannotFindReporterForAlias]: 'An error occurred while loading the "{name}" reporter. Please check the reporter parameter for errors. Error details:\n' +
+                                                 '\n' +
+                                                 '{err}',
 
     [RUNTIME_ERRORS.cannotFindTypescriptConfigurationFile]:            '"{filePath}" is not a valid TypeScript configuration file.',
     [RUNTIME_ERRORS.clientScriptInitializerIsNotSpecified]:            'Initialize your client script with one of the following: a JavaScript script, a JavaScript file path, or the name of a JavaScript module.',

src/errors/types.js
@@ -187,4 +187,6 @@ export const RUNTIME_ERRORS = {
     invalidCustomActionType:                            'E1080',
     cannotImportESMInCommonsJS:                         'E1081',
     setNativeAutomationForUnsupportedBrowsers:          'E1082',
+    cannotReadConfigFile:                               'E1083',
+    cannotParseConfigFile:                              'E1084',
 };

src/notifications/warning-message.ts
@@ -14,8 +14,6 @@ export default {
     maximizeError:                           'Was unable to maximize the window due to an error.\n\n{errMessage}',
     requestMockCORSValidationFailed:         '{RequestHook}: CORS validation failed for a request specified as {requestFilterRule}',
     debugInHeadlessError:                    'You cannot debug in headless mode.',
-    cannotReadConfigFile:                    'An error has occurred while reading the "{path}" configuration file.',
-    cannotParseConfigFile:                   "Failed to parse the '{path}' file.\n\nThis file is not a well-formed JSON file.",
     configOptionsWereOverridden:             'The {optionsString} option{suffix} from the configuration file will be ignored.',
     cannotOverrideTypeScriptConfigOptions:   'You cannot override the "{optionName}" compiler option in the TypeScript configuration file.\n',
 

src/reporter/index.ts
@@ -9,7 +9,7 @@ import { writable as isWritableStream } from 'is-stream';
 import ReporterPluginHost from './plugin-host';
 import ReporterPluginMethod from './plugin-methods';
 import formatCommand from './command/format-command';
-import { ReporterPluginError } from '../errors/runtime';
+import { ReporterPluginError, LoadReporterError } from '../errors/runtime';
 import Task from '../runner/task';
 import { Writable as WritableStream, Writable } from 'stream';
 import { WriteStream } from 'tty';
@@ -287,11 +287,16 @@ export default class Reporter {
             const processedName = processReporterName(name);
             const outStream     = output ? await Reporter._ensureOutStream(output) : void 0;
 
-            return {
-                plugin: pluginFactory(options),
-                name:   processedName,
-                outStream,
-            };
+            try {
+                return {
+                    plugin: pluginFactory(options),
+                    name:   processedName,
+                    outStream,
+                };
+            }
+            catch (err) {
+                throw new LoadReporterError(err, processedName);
+            }
         }));
     }
 

commit 305c003862bac881255a7624fcea9c3701aee3c7

fix: unexpected error is thrown if config file imports non-existing module (#7803)
## Purpose
Regression after #7759

## Approach
_Describe how your changes address the issue or implement the desired
functionality in as much detail as possible._

## References
_Provide a link to the existing issue(s), if any._

## Pre-Merge TODO
- [ ] Write tests for your proposed changes
- [ ] Make sure that existing tests do not fail

src/errors/runtime/index.js
@@ -15,11 +15,36 @@ const NO_STACK_AVAILABLE_MSG = 'No stack trace is available for this error';
 const MODULE_NOT_FOUND_CODE  = 'MODULE_NOT_FOUND';
 
 function formatErrorWithCallsite (error) {
-    const callsite         = getCallsiteForError(error);
-    const stackFilter      = createStackFilter();
-    const formattedMessage = callsite?.renderSync({ stackFilter }) || NO_STACK_AVAILABLE_MSG;
+    let formattedMessage = '';
+
+    try {
+        const callsite    = getCallsiteForError(error);
+        const stackFilter = createStackFilter();
+
+        // NOTE: If file from stack doesn't exist an error will be raised here:
+        formattedMessage = callsite?.renderSync({ stackFilter }) || NO_STACK_AVAILABLE_MSG;
+    }
+    catch {
+        formattedMessage = NO_STACK_AVAILABLE_MSG;
+    }
 
     return `${error.message}\n\n${formattedMessage}`;
+}
+
+function isModuleNotFoundError (error) {
+    return error.code && error.code === MODULE_NOT_FOUND_CODE;
+}
+
+// NOTE: The "message" property of the ModuleNotFound error has the following pattern: <message>\nRequire stack:\n<line1>\n<line2>.
+// This code removes lines bellow the targetPath. In case of "require('testcafe-reporter-<name>')" it removes all the Require stack.
+function formatModuleNotFoundErrorRequireStack (message, targetPath) {
+    const messageLines    = message.split('\n');
+    const targetLineIndex = messageLines.findIndex(line => line.includes(targetPath));
+
+    if (targetLineIndex === -1)
+        return message;
+
+    return messageLines.slice(0, targetLineIndex + 1).join('\n');
 }
 
 class ProcessTemplateInstruction {
@@ -201,12 +226,15 @@ export class ImportESMInCommonJSError extends GeneralError {
 
 export class ReadConfigFileError extends GeneralError {
     constructor (code, originalError, filePath, renderCallsite) {
-        super(code, filePath, ReadConfigFileError._getFormattedMessage(originalError, renderCallsite));
+        super(code, filePath, ReadConfigFileError._getFormattedMessage(originalError, renderCallsite, filePath));
     }
 
-    static _getFormattedMessage (originalError, renderCallsite) {
+    static _getFormattedMessage (originalError, renderCallsite, filePath) {
         if (!renderCallsite)
             return originalError.message;
+
+        if (isModuleNotFoundError(originalError))
+            return formatModuleNotFoundErrorRequireStack(originalError.message, filePath);
 
         return formatErrorWithCallsite(originalError);
     }
@@ -227,22 +255,9 @@ export class LoadReporterError extends GeneralError {
         return name;
     }
 
-    static _getReporterModuleNotFoundMessage (reporterModuleName) {
-        return `Cannot find module "${reporterModuleName}"`;
-    }
-
     static _getFormattedMessage (originalError, reporterFullName) {
-        const isModuleNotFoundError = originalError.code && originalError.code === MODULE_NOT_FOUND_CODE;
-
-        if (!isModuleNotFoundError)
-            return formatErrorWithCallsite(originalError);
-
-        // NOTE: The "message" property of the ModuleNotFound error has the following pattern: <message>\nRequire stack:\n<line1>\n<line2>.
-        // We need to output the full "require" stack, unless "require('testcafe-reporter-<name>')" caused the error.
-        const errorText = originalError.message.split('\n')[0];
-
-        if (errorText.includes(reporterFullName))
-            return LoadReporterError._getReporterModuleNotFoundMessage(reporterFullName);
+        if (isModuleNotFoundError(originalError))
+            return formatModuleNotFoundErrorRequireStack(originalError.message, reporterFullName);
 
         return formatErrorWithCallsite(originalError);
     }

test/server/configuration-test.js
@@ -3,6 +3,7 @@ const { cloneDeep, noop } = require('lodash');
 
 const { expect } = require('chai');
 const fs         = require('fs');
+const path       = require('path');
 const tmp        = require('tmp');
 const { nanoid } = require('nanoid');
 const del        = require('del');
@@ -25,14 +26,14 @@ const Extensions                              = require('../../lib/configuration
 const tsConfigPath           = 'tsconfig.json';
 const customTSConfigFilePath = 'custom-config.json';
 
-const createJSONConfig = (path, options) => {
+const createJSONConfig = (filePath, options) => {
     options = options || {};
-    fs.writeFileSync(path, JSON.stringify(options));
+    fs.writeFileSync(filePath, JSON.stringify(options));
 };
 
-const createJsConfig = (path, options) => {
+const createJsConfig = (filePath, options) => {
     options = options || {};
-    fs.writeFileSync(path, `module.exports = ${JSON.stringify(options)}`);
+    fs.writeFileSync(filePath, `module.exports = ${JSON.stringify(options)}`);
 };
 
 const jsConfigIndex = TestCafeConfiguration.FILENAMES.findIndex(file=>file.includes(Extensions.js));
@@ -115,6 +116,50 @@ describe('TestCafeConfiguration', function () {
                     expect(message).eql(
                         `Failed to parse the "${filePath}" configuration file. The file contains invalid JSON syntax. \n\nError details:\n\n` +
                         `JSON5: invalid end of input at 1:2`
+                    );
+                });
+
+                it('Non-existing module has been imported in config file', async () => {
+                    const filePath = testCafeConfiguration.defaultPaths[jsConfigIndex];
+                    let message     = '';
+
+                    fs.writeFileSync(filePath, 'const f=require("fake");module.exports={}');
+
+                    try {
+                        await testCafeConfiguration.init();
+                    }
+                    catch (err) {
+                        message = err.message;
+                    }
+
+                    expect(message).eql(
+                        `Failed to read the "${filePath}" configuration file from disk. Error details:\n\n` +
+                        `Cannot find module 'fake'\n` +
+                        `Require stack:\n` +
+                        `- ${filePath}`
+                    );
+                });
+
+                it('Non-existing module has been imported config file(depth 2)', async () => {
+                    const filePath   = path.resolve('./test/server/data/configuration/module-not-found/config.js');
+                    const modulePath = path.resolve('./test/server/data/configuration/module-not-found/module.js');
+                    let message      = '';
+
+                    const tcConfiguration = new TestCafeConfiguration(filePath);
+
+                    try {
+                        await tcConfiguration.init();
+                    }
+                    catch (err) {
+                        message = err.message;
+                    }
+
+                    expect(message).eql(
+                        `Failed to read the "${filePath}" configuration file from disk. Error details:\n\n` +
+                        `Cannot find module 'non-existing-module'\n` +
+                        `Require stack:\n` +
+                        `- ${modulePath}\n` +
+                        `- ${filePath}`
                     );
                 });
 

test/server/data/configuration/module-not-found/config.js
@@ -1,0 +1,5 @@
+const existingModule = require('./module.js');
+
+module.exports = {
+    reporters: [existingModule]
+}

test/server/data/configuration/module-not-found/module.js
@@ -1,0 +1,3 @@
+const nonExist = require('non-existing-module');
+
+module.exports = {}

test/server/runner-test.js
@@ -158,7 +158,30 @@ describe('Runner', () => {
                 .catch(err => {
                     expect(err.message).eql('Failed to load the "reporter42" reporter. Please check the parameter for errors. Error details:' +
                                             '\n\n' +
-                                            'Cannot find module "testcafe-reporter-reporter42"');
+                                            'Cannot find module \'testcafe-reporter-reporter42\'');
+                });
+        });
+
+        it('Should raise an ModuleNotFound error if reporter has non-existing import', () => {
+            const fakeReporter = function fake () {
+                this.nonExistingModule = require('non-existing-module');
+            };
+
+            return runner
+                .browsers(connection)
+                .reporter(fakeReporter)
+                .src('test/server/data/test-suites/basic/testfile2.js')
+                .run()
+                .then(() => {
+                    throw new Error('Promise rejection expected');
+                })
+                .catch(err => {
+                    expect(err.message.startsWith(
+                        'Failed to load the "fake" reporter. Please check the parameter for errors. Error details:' +
+                        '\n\n' +
+                        'Cannot find module \'non-existing-module\'\n' +
+                        'Require stack:\n'
+                    )).eql(true);
                 });
         });
 