fb75eb6690d50e1fd796644f1b839f8bb3059e46
Alex Kamaev <alexikam@gmail.com>
2023-06-21 18:57:02 +0400

    1

--- test/functional/fixtures/regression/gh-856/test.js
+++
@@ -1,9 +0,0 @@
-describe('[Regression](GH-856) Test should not hang when redirect occurs after the page is loaded', function () {
-    it('gh-856', function () {
-        return runTests('testcafe-fixtures/index-test.js', 'gh-856');
-    });
-
-    it('gh-856 (iframe)', function () {
-        return runTests('testcafe-fixtures/index-test.js', 'gh-856 (iframe)', { selectorTimeout: 10000 });
-    });
-});
