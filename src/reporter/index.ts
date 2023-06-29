2107bf907b6437ba1dd981929b6bf9f4d42159a6
artem-babich <artem.babich@devexpress.com>
2023-06-29 12:52:57 +0400

    refactor

--- src/reporter/index.ts
+++ src/reporter/index.ts
@@ -346,16 +346,19 @@ export default class Reporter {
         return task.tests.map(test => Reporter._createTestItem(test, runsPerTest));
     }
 
-    private static _createTestRunInfo (reportItem: TestInfo): TestRunInfo {
-        const durationMs = !reportItem.test.skip
-            ? +new Date() - (reportItem.startTime as number) //eslint-disable-line  @typescript-eslint/no-extra-parens
-            : 0;
+    private static _getTestDuration (reportItem: TestInfo): number {
+        if (reportItem.test.skip)
+            return 0;
 
+        return +new Date() - (reportItem.startTime as number);
+    }
+
+    private static _createTestRunInfo (reportItem: TestInfo): TestRunInfo {
         return {
-            durationMs,
             errs:           sortBy(reportItem.errs, [ 'userAgent', 'code' ]),
             warnings:       reportItem.warnings,
             reportData:     reportItem.reportData,
+            durationMs:     Reporter._getTestDuration(reportItem),
             unstable:       reportItem.unstable,
             screenshotPath: reportItem.screenshotPath as string,
             screenshots:    reportItem.screenshots,
