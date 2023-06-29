9754e77a35ea283e41089596eec3e232dea2afb5
Alex Kamaev <alexikam@gmail.com>
2023-06-29 11:50:30 +0400

    add a note

--- test/functional/fixtures/regression/gh-856/test.js
+++ test/functional/fixtures/regression/gh-856/test.js
@@ -1,5 +1,6 @@
 const { skipInNativeAutomation } = require('../../../utils/skip-in');
 
+// NOTE: details here: https://github.com/DevExpress/testcafe-private/issues/210
 describe('[Regression](GH-856) Test should not hang when redirect occurs after the page is loaded', function () {
     skipInNativeAutomation('gh-856', function () {
         return runTests('testcafe-fixtures/index-test.js', 'gh-856');
