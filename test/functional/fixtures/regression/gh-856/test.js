34a4f369eb19fba38627d058b1f11988fb7038ab
AlexKamaev <alexey.kamaev@devexpress.com>
2023-07-07 04:59:47 +0200

    skip flaky gh-856 test (#7830)
    Skip these flaky tests until we completely decide what should be done
    with them.

--- test/functional/fixtures/regression/gh-856/test.js
+++ test/functional/fixtures/regression/gh-856/test.js
@@ -1,9 +1,12 @@
+const { skipInNativeAutomation } = require('../../../utils/skip-in');
+
+// NOTE: details here: https://github.com/DevExpress/testcafe-private/issues/210
 describe('[Regression](GH-856) Test should not hang when redirect occurs after the page is loaded', function () {
-    it('gh-856', function () {
+    skipInNativeAutomation('gh-856', function () {
         return runTests('testcafe-fixtures/index-test.js', 'gh-856');
     });
 
-    it('gh-856 (iframe)', function () {
+    skipInNativeAutomation('gh-856 (iframe)', function () {
         return runTests('testcafe-fixtures/index-test.js', 'gh-856 (iframe)', { selectorTimeout: 10000 });
     });
 });
