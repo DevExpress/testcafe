f99d926afeefea6f50392c35c82f1bcd1b5d824d
miherlosev <mlosev86@gmail.com>
2023-08-08 06:29:33 +0400

    Implement t.browser.nativeAutomation

--- src/utils/get-browser.ts
+++ src/utils/get-browser.ts
@@ -5,13 +5,15 @@ import BrowserConnection from '../browser/connection';
 interface Browser extends ParsedUserAgent {
     alias: string;
     headless: boolean;
+    nativeAutomation: boolean;
 }
 
-export default function getBrowser (browserConnection: BrowserConnection): Browser {
+export default function getBrowser (browserConnection: BrowserConnection, nativeAutomation: boolean): Browser {
     const { browserInfo: { parsedUserAgent, alias } } = browserConnection;
 
     return assign({}, parsedUserAgent, {
         alias,
         headless: browserConnection.isHeadlessBrowser(),
+        nativeAutomation,
     });
 }
