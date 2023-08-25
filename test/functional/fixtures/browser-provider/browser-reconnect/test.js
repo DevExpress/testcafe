5e4dabd91fd0b766dcdac8b97fc98daea2962e42
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-08-25 14:05:20 +0300

    refactor: refactored test Should log error on browser disconnect

--- test/functional/fixtures/browser-provider/browser-reconnect/test.js
+++ test/functional/fixtures/browser-provider/browser-reconnect/test.js
@@ -94,7 +94,13 @@ if (config.useLocalBrowsers) {
             let errLog = '';
 
             return new Promise(resolve => {
-                const proc = spawn(`node ${path.join(__dirname, 'run-log-error-on-disconnect-test.js')}`, { shell: true, env: { ...process.env, DEBUG: 'testcafe:hammerhead:*' } });
+                const proc = spawn(`node ${path.join(__dirname, 'run-log-error-on-disconnect-test.js')}`, {
+                    shell: true,
+                    env:   {
+                        ...process.env,
+                        DEBUG: 'testcafe:hammerhead:*',
+                    },
+                });
 
                 proc.stderr.on('data', data => {
                     errLog += data.toString('utf-8');
