4ad3d6c58c3e9d81cfa5b8fd98efec9dbd69d264
miherlosev <mlosev86@gmail.com>
2023-08-25 09:41:00 +0400

    3

--- test/functional/site/basic-auth-server.js
+++ test/functional/site/basic-auth-server.js
@@ -2,34 +2,52 @@ const http            = require('http');
 const express         = require('express');
 const basicAuth       = require('basic-auth');
 const BasicHttpServer = require('./basic-http-server');
+const debug           = require('debug')('testcafe:basic-auth-server');
 
 class BasicAuthServer extends BasicHttpServer {
     start (port) {
         const app = express();
 
         app.all('/redirect', function (req, res) {
+            debug('/redirect');
             const credentials = basicAuth(req);
 
             if (!credentials || credentials.name !== 'username' || credentials.pass !== 'password') {
                 res.statusCode = 401;
                 res.setHeader('WWW-Authenticate', 'Basic realm="example"');
-                res.end('<html><body><div id="result">not authorized</div></body></html>');
+
+                setTimeout(() => {
+                    debug('end with not authorized');
+                    res.end('<html><body><div id="result">not authorized</div></body></html>');
+                }, 2000);
             }
-            else
-                res.redirect('/');
+            else {
+                setTimeout(() => {
+                    debug('redirect to /');
+                    res.redirect('/');
+                }, 3000);
+            }
+
         });
 
         app.all('*', function (req, res) {
+            debug('*');
             const credentials = basicAuth(req);
 
             if (!credentials || credentials.name !== 'username' || credentials.pass !== 'password') {
                 res.statusCode = 401;
                 res.setHeader('WWW-Authenticate', 'Basic realm="example"');
-                res.end('<html><body><div id="result">not authorized</div></body></html>');
+                setTimeout(() => {
+                    debug('end with not authorized');
+                    res.end('<html><body><div id="result">not authorized</div></body></html>');
+                }, 2000);
             }
             else {
                 res.statusCode = 200;
-                res.end('<html><body><div id="result">authorized</div></body></html>');
+                setTimeout(() => {
+                    debug('end with authorized');
+                    res.end('<html><body><div id="result">authorized</div></body></html>');
+                }, 3000);
             }
         });
 
