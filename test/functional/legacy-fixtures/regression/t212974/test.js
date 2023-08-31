20583223879219c33ad29a0056137b18abef6109
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-08-31 04:29:19 -0700

    refactor: removed IE leftovers from test\functional\legacy-fixtures (#7988)
    <!--
    Thank you for your contribution.
    
    Before making a PR, please read our contributing guidelines at
    
    https://github.com/DevExpress/testcafe/blob/master/CONTRIBUTING.md#code-contribution
    
    We recommend creating a *draft* PR, so that you can mark it as 'ready
    for review' when you are done.
    -->
    
    ## Purpose
    _Describe the problem you want to address or the feature you want to
    implement._
    
    ## Approach
    _Describe how your changes address the issue or implement the desired
    functionality in as much detail as possible._
    
    ## References
    _Provide a link to the existing issue(s), if any._
    
    ## Pre-Merge TODO
    - [ ] Write tests for your proposed changes
    - [ ] Make sure that existing tests do not fail

--- test/functional/legacy-fixtures/regression/t212974/test.js
+++
@@ -1,3 +0,0 @@
-it("[Regression](T212974) Page script can't get element under TestCafe cursor with document.elementFromPoint in IE", function () {
-    return runTests('testcafe-fixtures/index.test.js');
-});
