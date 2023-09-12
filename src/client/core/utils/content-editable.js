e242c84eb5b77644a0d23ad9f2b62057d5d18f2f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-09-12 14:16:04 +0300

    fix: rollback some changes

--- src/client/core/utils/content-editable.js
+++ src/client/core/utils/content-editable.js
@@ -289,11 +289,18 @@ function getSelectedPositionInParentByOffset (node, offset) {
     if (domUtils.isShadowUIElement(node))
         return { node, offset };
 
-    let currentNode          = childNodes[offset];
-    let currentOffset        = 0;
     const childNodes         = nativeMethods.nodeChildNodesGetter.call(node);
     const childCount         = domUtils.getChildNodesLength(childNodes);
     let isSearchForLastChild = offset >= childCount;
+    let currentNode          = null;
+    let currentOffset        = null;
+
+    if (isSearchForLastChild)
+        currentNode = childNodes[childCount - 1];
+    else {
+        currentNode   = childNodes[offset];
+        currentOffset = 0;
+    }
 
     // NOTE: skip shadowUI elements
     if (domUtils.isShadowUIElement(currentNode)) {
