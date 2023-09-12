87d42a3ae6ed6e72f9fdf15fa7fbcfd0863ae43e
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-09-12 04:41:05 -0700

    refactor: removed IE leftovers from src\client\core\utils (part 2) (#7999)
    <!--
    Thank you for your contribution.
    
    Before making a PR, please read our contributing guidelines at
    
    https://github.com/DevExpress/testcafe/blob/master/CONTRIBUTING.md#code-contribution
    
    We recommend creating a *draft* PR, so that you can mark it as 'ready
    for review' when you are done.
    -->
    
    ## Purpose
    _Describe the problem you want to address or the feature you want to
    implement._
    
    ## Approach
    _Describe how your changes address the issue or implement the desired
    functionality in as much detail as possible._
    
    ## References
    _Provide a link to the existing issue(s), if any._
    
    ## Pre-Merge TODO
    - [ ] Write tests for your proposed changes
    - [ ] Make sure that existing tests do not fail

--- src/client/core/utils/scroll.ts
+++ src/client/core/utils/scroll.ts
@@ -4,22 +4,13 @@ import { utils, nativeMethods } from '../deps/hammerhead';
 import * as domUtils from './dom';
 import * as styleUtils from './style';
 
-const SCROLLABLE_OVERFLOW_STYLE_RE               = /auto|scroll|hidden/i;
-const DEFAULT_IE_SCROLLABLE_OVERFLOW_STYLE_VALUE = 'visible';
+const SCROLLABLE_OVERFLOW_STYLE_RE = /auto|scroll|hidden/i;
 
 function getScrollable (el: Element): AxisValues<boolean> {
-    const overflowX            = styleUtils.get(el, 'overflowX') as string;
-    const overflowY            = styleUtils.get(el, 'overflowY') as string;
-    let scrollableHorizontally = SCROLLABLE_OVERFLOW_STYLE_RE.test(overflowX);
-    let scrollableVertically   = SCROLLABLE_OVERFLOW_STYLE_RE.test(overflowY);
-
-    // IE11 and MS Edge bug: There are two properties: overflow-x and overflow-y.
-    // If one property is set so that the browser may show scrollbars (`auto` or `scroll`) and the second one is set to 'visible',
-    // then the second one will work as if it had the 'auto' value.
-    if (utils.browser.isIE) {
-        scrollableHorizontally = scrollableHorizontally || scrollableVertically && overflowX === DEFAULT_IE_SCROLLABLE_OVERFLOW_STYLE_VALUE;
-        scrollableVertically   = scrollableVertically || scrollableHorizontally && overflowY === DEFAULT_IE_SCROLLABLE_OVERFLOW_STYLE_VALUE;
-    }
+    const overflowX              = styleUtils.get(el, 'overflowX') as string;
+    const overflowY              = styleUtils.get(el, 'overflowY') as string;
+    const scrollableHorizontally = SCROLLABLE_OVERFLOW_STYLE_RE.test(overflowX);
+    const scrollableVertically   = SCROLLABLE_OVERFLOW_STYLE_RE.test(overflowY);
 
     return new AxisValues(scrollableHorizontally, scrollableVertically);
 }
