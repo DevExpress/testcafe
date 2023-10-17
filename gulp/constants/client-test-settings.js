270d98371ada346676029e3184adf73b92f8da86
Pavel <palich_morozov@mail.ru>
2023-10-17 09:57:33 +0400

    test

--- gulp/constants/client-test-settings.js
+++ gulp/constants/client-test-settings.js
@@ -52,6 +52,7 @@ const CLIENT_TESTS_MOBILE_BROWSERS = [
         browserName:     'chrome',
         platformVersion: '8.0',
         platformName:    'Android',
+        platform:        'Linux'
     },
     // {
     //     platform:    'iOS',
