a2b7e41eb9b06fc04e02cd0a93b5fa39f2ce31fb
Pavel <palich_morozov@mail.ru>
2023-11-13 12:08:02 +0400

    fix lint

--- test/functional/fixtures/regression/gh-7097/test.js
+++ test/functional/fixtures/regression/gh-7097/test.js
@@ -1,7 +1,7 @@
 const config = require('../../../config');
 
 if (!config.nativeAutomation) {
-    describe('[Regression](GH-2969) - Should fail fetch on non exist path with disableNativeAutomation', function () {
+    describe('[Regression](GH-7097) - Should fail fetch on non exist path with disableNativeAutomation', function () {
         it('Fetch failed', function () {
             return runTests('testcafe-fixtures/fetch-to-non-exist-path.js');
         });
