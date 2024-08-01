46863bc243f8e36927c95227cd85618f37eb6e0e
Pavel <palich_morozov@mail.ru>
2024-08-01 10:58:17 +0400

    fix: add --disable-search-engine-choice-screen to default arguments

--- src/browser/provider/built-in/dedicated/chrome/build-chrome-args.js
+++ src/browser/provider/built-in/dedicated/chrome/build-chrome-args.js
@@ -2,6 +2,7 @@ export const CONTAINERIZED_CHROME_FLAGS = ['--no-sandbox', '--disable-dev-shm-us
 
 export function buildChromeArgs ({ config, cdpPort, platformArgs, tempProfileDir, isContainerized, isNativeAutomation, browserName }) {
     const headlessMode = ['chrome', 'chromium'].includes(browserName) ? '--headless=new' : '--headless';
+    const defaultArgs  = ['--disable-search-engine-choice-screen'];
 
     let chromeArgs = []
         .concat(
@@ -11,7 +12,8 @@ export function buildChromeArgs ({ config, cdpPort, platformArgs, tempProfileDir
             config.userArgs ? [config.userArgs] : [],
             // NOTE: we need to prevent new window blocking for multiple windows in Native Automation
             isNativeAutomation ? ['--disable-popup-blocking'] : [],
-            platformArgs ? [platformArgs] : []
+            platformArgs ? [platformArgs] : [],
+            defaultArgs
         )
         .join(' ');
 
