66cc73bc32fc8316c3d9023b07cbb3984eb02074
Bayheck <adil.rakhaliyev@devexpress.com>
2024-09-19 16:14:38 +0500

    update: websocket error moved to ignore list

--- src/native-automation/request-pipeline/safe-api.ts
+++ src/native-automation/request-pipeline/safe-api.ts
@@ -16,18 +16,20 @@ const IGNORED_ERROR_CODES = {
     // The "Session not found" error can occur in iframes for unclear reasons.
     SESSION_WITH_GIVEN_ID_NOT_FOUND: -32001,
 };
+//The "WebSocket connection closed" error occurs on closeWindow in multiple windows mode
+const WEBSOCKET_CLOSED_RESPONSE_ERROR_MSG  = 'WebSocket connection closed';
 
 export async function connectionResetGuard (handleRequestFn: () => Promise<void>, handleErrorFn: (err: any) => void): Promise<void> {
     try {
         await handleRequestFn();
     }
     catch (err: any) {
-        if (Object.values(IGNORED_ERROR_CODES).includes(err?.response?.code))
+        if (Object.values(IGNORED_ERROR_CODES).includes(err?.response?.code) || err.message === WEBSOCKET_CLOSED_RESPONSE_ERROR_MSG)
             return;
 
         handleErrorFn(err);
 
-        // throw err;
+        throw err;
     }
 }
 
