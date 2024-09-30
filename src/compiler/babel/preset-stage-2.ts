a99f1ccc75177249582778d52fa8759c99fbc841
Adil Rakhaliyev <67043367+Bayheck@users.noreply.github.com>
2024-09-30 12:05:49 +0500

    fix: updated babel packages (#8281)
    <!--
    Thank you for your contribution.
    
    Before making a PR, please read our contributing guidelines at
    
    https://github.com/DevExpress/testcafe/blob/master/CONTRIBUTING.md#code-contribution
    
    We recommend creating a *draft* PR, so that you can mark it as 'ready
    for review' when you are done.
    -->
    
    ## Purpose
    _Describe the problem you want to address or the feature you want to
    implement._
    
    ## Approach
    _Describe how your changes address the issue or implement the desired
    functionality in as much detail as possible._
    
    ## References
    closes https://github.com/DevExpress/testcafe/issues/8272
    
    ## Pre-Merge TODO
    - [ ] Write tests for your proposed changes
    - [ ] Make sure that existing tests do not fail
    
    ---------
    
    Co-authored-by: Bayheck <adil.rakhaliyev@devexpress.com>

--- src/compiler/babel/preset-stage-2.ts
+++ src/compiler/babel/preset-stage-2.ts
@@ -4,14 +4,14 @@ export default {
             require('babel-plugin-syntax-trailing-function-commas'),
             require('@babel/plugin-transform-async-to-generator'),
             require('@babel/plugin-transform-exponentiation-operator'),
-            require('@babel/plugin-proposal-async-generator-functions'),
-            require('@babel/plugin-proposal-object-rest-spread'),
+            require('@babel/plugin-transform-async-generator-functions'),
+            require('@babel/plugin-transform-object-rest-spread'),
         ],
     }],
     plugins: [
         require('@babel/plugin-syntax-dynamic-import'),
         [require('@babel/plugin-proposal-decorators'), { 'legacy': true }],
-        require('@babel/plugin-proposal-class-properties'),
-        require('@babel/plugin-proposal-async-generator-functions'),
+        require('@babel/plugin-transform-class-properties'),
+        require('@babel/plugin-transform-async-generator-functions'),
     ],
 };
