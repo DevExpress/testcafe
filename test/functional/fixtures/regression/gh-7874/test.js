342b0a1f9875176b0ef4789d4e780437d78d0601
Bayheck <adil.rakhaliyev@devexpress.com>
2024-10-17 21:13:27 +0500

    test: gh-7874 only run

--- test/functional/fixtures/regression/gh-7874/test.js
+++ test/functional/fixtures/regression/gh-7874/test.js
@@ -1,5 +1,6 @@
-describe('[Regression](GH-7874)', function () {
-    it.skip('Should clear cookies between roles if page have not been changed', function () {
+/* eslint-disable no-only-tests/no-only-tests */
+describe.only('[Regression](GH-7874)', function () {
+    it('Should clear cookies between roles if page have not been changed', function () {
         return runTests('testcafe-fixtures/index.js');
     });
 });
