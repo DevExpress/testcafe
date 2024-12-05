58731cd3b7a351a0f5e38eb4f71f704b7309a622
Bayheck <adil.rakhaliyev@devexpress.com>
2024-12-05 15:39:28 +0500

    fix: pr comments fixed

--- test/functional/fixtures/regression/gh-8321/test.js
+++ test/functional/fixtures/regression/gh-8321/test.js
@@ -1,5 +1,5 @@
 describe('[Regression](GH-8321)', function () {
-    it('Test should not fail with Uncaught object error on typeText', function () {
+    it('Should type text without errors when document selection is updated', function () {
         return runTests('testcafe-fixtures/index.js', 'Callsite Issue', { only: 'chrome' });
     });
 });
