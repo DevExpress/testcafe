97f234d80ab1fe0c601ddbaefc0d50ca2ee4695f
Bayheck <adil.rakhaliyev@devexpress.com>
2025-02-05 21:28:13 +0500

    tests: fix

--- test/functional/fixtures/hammerhead/gh-3012/test.js
+++ test/functional/fixtures/hammerhead/gh-3012/test.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-only-tests/no-only-tests */
 describe('Worker header', function () {
-    it.only('Should not break due to undefined optional chaining', function () {
+    it('Should not break due to undefined optional chaining', function () {
         return runTests('testcafe-fixtures/index.js', null);
     });
 });
