2f2bd23aefe7faf4080ce525b17c204246a9339d
Bayheck <adil.rakhaliyev@devexpress.com>
2025-03-04 17:29:29 +0500

    testing

--- test/functional/fixtures/regression/gh-8117/test.js
+++ test/functional/fixtures/regression/gh-8117/test.js
@@ -25,7 +25,7 @@ const run = (pathToTest) => {
             runner = testCafe.createRunner();
             return runner
                 .src(src)
-                .browsers(`chrome`)
+                .browsers(`chrome:headless`)
                 .reporter(reporter)
                 .run({ disableMultipleWindows: true });
         })
