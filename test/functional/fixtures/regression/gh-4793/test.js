0ce806342b9d56000775101958713d0e67e3daba
aleks-pro <prokhor.aleks@gmail.com>
2025-09-08 12:53:19 +0400

    skip iframe test for safari

--- test/functional/fixtures/regression/gh-4793/test.js
+++ test/functional/fixtures/regression/gh-4793/test.js
@@ -1,5 +1,6 @@
 describe('[Regression](GH-4793)', function () {
-    it('Elements inside cross-domain iframes should be focusable', function () {
-        return runTests('testcafe-fixtures/index-test.js', 'Type text into an input inside a cross-domain iframe');
+    ('Elements inside cross-domain iframes should be focusable', function () {
+        // TODO: skipped in Safari due to https://github.com/DevExpress/testcafe-private/issues/556
+        return runTests('testcafe-fixtures/index-test.js', 'Type text into an input inside a cross-domain iframe', { skip: ['safari'] });
     });
 });
